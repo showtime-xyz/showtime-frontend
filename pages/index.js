9346febfe40bd70f7b544b797d43e6be8ef82c99
Alex <alex.kilkka@gmail.com>
2021-01-12 17:06:25 -0800

    Test change

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Showtime</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
