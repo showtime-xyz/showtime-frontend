9dfa3af295a9e3e3622742c44efa7a4635e865c8
Alex <alex.kilkka@gmail.com>
2021-02-02 22:06:17 -0800

    Fix spacing on mobile

--- pages/p/[slug].js
+++ pages/p/[slug].js
@@ -468,7 +468,7 @@ const Profile = ({
             </div>
           </div>
           <div className="col-span-3 px-3">
-            <div className="flex flex-row border-b-2 border-gray-300 mr-8">
+            <div className="flex flex-row border-b-2 border-gray-300 lg:mr-8">
               <div className="flex-grow">
                 <div className="text-left flex flex-row items-center">
                   <div className="text-3xl md:text-5xl showtime-title">
