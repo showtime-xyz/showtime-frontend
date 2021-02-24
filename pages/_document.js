45d6dc0814f53f30f2595517093bf3159ebc6fd6
Nico Glennon <nicoglennon@icloud.com>
2021-02-23 20:19:44 -0600

    Add StyledComponents / Babel config

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,25 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import { ServerStyleSheet } from "styled-components";
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    // Step 1: Create an instance of ServerStyleSheet
+    const sheet = new ServerStyleSheet();
+
+    // Step 2: Retrieve styles from components in the page
+    const page = renderPage((App) => (props) =>
+      sheet.collectStyles(<App {...props} />)
+    );
+
+    // Step 3: Extract the styles as <style> tags
+    const styleTags = sheet.getStyleElement();
+
+    // Step 4: Pass styleTags as a prop
+    return { ...page, styleTags };
+  }
   render() {
     return (
       <Html>
-        <Head />
+        <Head>{this.props.styleTags}</Head>
         <body>
           <Main />
           {/* Here we will mount our modal portal */}
