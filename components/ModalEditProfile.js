0fe85561f4b1b0a55d3c5be269e7e076d3c148b6
Akhil <akhil.r.nadendla@gmail.com>
2021-03-03 16:32:39 -0800

    Add custom url input

--- components/ModalEditProfile.js
+++ components/ModalEditProfile.js
@@ -1,17 +1,22 @@
 import { useContext, useState, useEffect } from "react";
 import mixpanel from "mixpanel-browser";
 import ClientOnlyPortal from "./ClientOnlyPortal";
+import backend from "../lib/backend";
 import AppContext from "../context/app-context";
 import CloseButton from "./CloseButton";
 
 export default function Modal({ isOpen, setEditModalOpen }) {
+  const SHOWTIME_PROD_URL = "alpha.tryshowtime.com/";
   const context = useContext(AppContext);
   const [nameValue, setNameValue] = useState(null);
+  const [customURLValue, setCustomURLValue] = useState(SHOWTIME_PROD_URL);
+  const [customURLError, setCustomURLError] = useState("");
   const [bioValue, setBioValue] = useState(null);
   const [websiteValue, setWebsiteValue] = useState(null);
 
   useEffect(() => {
     if (context.myProfile) {
+      setCustomURLValue(SHOWTIME_PROD_URL + context.myProfile.username);
       setNameValue(context.myProfile.name);
       setBioValue(context.myProfile.bio);
       setWebsiteValue(context.myProfile.website_url);
@@ -22,12 +27,29 @@ export default function Modal({ isOpen, setEditModalOpen }) {
     mixpanel.track("Save profile edit");
     event.preventDefault();
 
+    const username = customURLValue
+      ? customURLValue.substring(SHOWTIME_PROD_URL.length, customURLValue.length).trim()
+      : null;
+    let validUsername;
+    try {
+      const result = await backend.get(`/v1/username_available?username=${username}`, {
+        method: "get",
+      });
+      validUsername = result?.data?.data;
+    } catch {
+      validUsername = false;
+    }
+    if (!validUsername) {
+      setCustomURLError("This username is not available");
+      return;
+    }
     // Post changes to the API
     await fetch(`/api/editname`, {
       method: "post",
       body: JSON.stringify({
         name: nameValue ? (nameValue.trim() ? nameValue.trim() : null) : null,
         bio: bioValue ? (bioValue.trim() ? bioValue.trim() : null) : null,
+        username,
         website_url: websiteValue
           ? websiteValue.trim()
             ? websiteValue.trim()
@@ -41,6 +63,7 @@ export default function Modal({ isOpen, setEditModalOpen }) {
       ...context.myProfile,
       name: nameValue ? (nameValue.trim() ? nameValue.trim() : null) : null, // handle names with all whitespaces
       bio: bioValue ? (bioValue.trim() ? bioValue.trim() : null) : null,
+      username,
       website_url: websiteValue
         ? websiteValue.trim()
           ? websiteValue.trim()
@@ -87,11 +110,52 @@ export default function Modal({ isOpen, setEditModalOpen }) {
                       color: "black",
                       padding: 10,
                       borderRadius: 7,
-
                       borderWidth: 2,
                       borderColor: "#999",
                     }}
                   />
+                  <label htmlFor="customURL" style={{ fontWeight: 600 }}>
+                    Custom URL
+                  </label>
+                  <input
+                    name="customURL"
+                    placeholder="Enter your custom URL"
+                    value={customURLValue}
+                    autoFocus
+                    onChange={(e) => {
+                      if (e.target.value === "") {
+                        setCustomURLError("");
+                        setCustomURLValue(SHOWTIME_PROD_URL)
+                      }
+                      const prefix = e.target.value.substring(0, SHOWTIME_PROD_URL.length);
+                      const suffix = e.target.value.substring(SHOWTIME_PROD_URL.length, e.target.value.length);
+                      const urlRegex = /^[a-zA-Z0-9_-]*$/;
+                      if (prefix === SHOWTIME_PROD_URL && urlRegex.test(suffix)) {
+                        setCustomURLError("");
+                        setCustomURLValue(e.target.value);
+                      }
+                    }}
+                    type="text"
+                    maxLength={30 + SHOWTIME_PROD_URL.length}
+                    className="w-full mt-1"
+                    style={{
+                      color: "black",
+                      padding: 10,
+                      borderRadius: 7,
+                      borderWidth: 2,
+                      borderColor: "#999",
+                      marginBottom: customURLError ? "4px" : "1.5rem",
+                    }}
+                  />
+                  {customURLError &&
+                    <div
+                      style={{
+                        color: "red",
+                        fontSize: 12,
+                        marginBottom: "1.5rem",
+                      }}>
+                      {customURLError}
+                    </div>}
                   <label htmlFor="bio" style={{ fontWeight: 600 }}>
                     About me{" "}
                     <span
@@ -158,7 +222,7 @@ export default function Modal({ isOpen, setEditModalOpen }) {
                     type="submit"
                     className="showtime-green-button px-5 py-3 float-right"
                     style={{ borderColor: "#35bb5b", borderWidth: 2 }}
-                    //onClick={() => setEditModalOpen(false)}
+                  //onClick={() => setEditModalOpen(false)}
                   >
                     Save changes
                   </button>
