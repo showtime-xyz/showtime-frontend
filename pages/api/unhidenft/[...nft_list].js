6f74be782f0c51340d2bb760e474dad7a2234f11
Alex <alex.kilkka@gmail.com>
2021-03-08 10:03:40 -0800

    Add endpoints

--- pages/api/unhidenft/[...nft_list].js
+++ pages/api/unhidenft/[...nft_list].js
@@ -2,7 +2,9 @@ import Iron from "@hapi/iron";
 import CookieService from "../../../lib/cookie";
 
 export default async (req, res) => {
-  const following_address = req.query.address;
+  const { nft_list } = context.query;
+  const nft_id = nft_list[0];
+  const list_id = nft_list[1];
 
   try {
     const user = await Iron.unseal(
@@ -12,17 +14,13 @@ export default async (req, res) => {
     );
 
     await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/profile?address=${following_address}`,
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/unhide_nft/${nft_id}/${list_id}`,
       {
         method: "POST",
         headers: {
           "X-Authenticated-User": user.publicAddress,
           "X-API-Key": process.env.SHOWTIME_FRONTEND_API_KEY,
-          "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          action: "unfollow",
-        }),
       }
     );
   } catch (error) {
