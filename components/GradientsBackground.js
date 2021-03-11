9fd0246953aee9c63690bf6bd43c94edd828fca3
Nico Glennon <nicoglennon@icloud.com>
2021-03-11 13:37:54 -0600

    New gradient colors

--- components/GradientsBackground.js
+++ components/GradientsBackground.js
@@ -7,46 +7,46 @@ export default function GradientsBackground() {
         #yellow-ellipse-background {
           position: absolute;
           width: 100%;
-          height: 200px;
+          height: 500px;
           left: 10%;
-          top: 0px;
+          top: -250px;
           background: radial-gradient(
             50% 50% at 50% 50%,
             #fffb93 0%,
             rgba(189, 255, 48, 0) 100%
           );
-          opacity: 0.3;
+          opacity: 0.6;
           //   filter: blur(121px);
           pointer-events: none;
         }
         #red-ellipse-background {
           position: absolute;
           width: 100%;
-          height: 800px;
+          height: 900px;
           right: -50%;
-          top: -400px;
+          top: -450px;
           background: radial-gradient(
             50% 50% at 50% 50%,
             #fc51ff 0%,
             rgba(255, 49, 185, 0) 100%
           );
-          opacity: 0.2;
+          opacity: 0.3;
           //   filter: blur(121px);
           pointer-events: none;
         }
         #blue-ellipse-background {
           position: absolute;
           width: 100%;
-          height: 800px;
+          height: 900px;
           left: -50%;
-          top: -400px;
+          top: -450px;
           pointer-events: none;
           background: radial-gradient(
             50% 50% at 50% 50%,
             #93ffeb 0%,
             rgba(49, 218, 255, 0) 100%
           );
-          opacity: 0.5;
+          opacity: 0.7;
           //   filter: blur(121px);
         }
       `}</style>
