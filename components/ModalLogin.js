c09a854bd68ccef667345aca38d8d54cf1ac118e
Alex <alex.kilkka@gmail.com>
2021-03-20 10:21:44 -0700

    Hide Coinbase Wallet on mobile

--- components/ModalLogin.js
+++ components/ModalLogin.js
@@ -49,36 +49,41 @@ export default function Modal({ isOpen }) {
   const handleSubmitWallet = async () => {
     mixpanel.track("Login - wallet button click");
 
-    const providerOptions = {
+    var providerOptions = {
       walletconnect: {
         package: WalletConnectProvider,
         options: {
           infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
         },
       },
-      "custom-walletlink": {
-        display: {
-          logo: "/coinbase.svg",
-          name: "Coinbase",
-          description: "Use Coinbase Wallet app on mobile device",
-        },
-        options: {
-          appName: "Showtime", // Your app name
-          networkUrl: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
-          chainId: process.env.NEXT_PUBLIC_CHAINID,
-        },
-        package: WalletLink,
-        connector: async (_, options) => {
-          const { appName, networkUrl, chainId } = options;
-          const walletLink = new WalletLink({
-            appName,
-          });
-          const provider = walletLink.makeWeb3Provider(networkUrl, chainId);
-          await provider.enable();
-          return provider;
-        },
-      },
     };
+    if (!context.isMobile) {
+      providerOptions = {
+        ...providerOptions,
+        "custom-walletlink": {
+          display: {
+            logo: "/coinbase.svg",
+            name: "Coinbase",
+            description: "Use Coinbase Wallet app on mobile device",
+          },
+          options: {
+            appName: "Showtime", // Your app name
+            networkUrl: `https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`,
+            chainId: process.env.NEXT_PUBLIC_CHAINID,
+          },
+          package: WalletLink,
+          connector: async (_, options) => {
+            const { appName, networkUrl, chainId } = options;
+            const walletLink = new WalletLink({
+              appName,
+            });
+            const provider = walletLink.makeWeb3Provider(networkUrl, chainId);
+            await provider.enable();
+            return provider;
+          },
+        },
+      };
+    }
 
     const web3Modal = new Web3Modal({
       network: "mainnet", // optional
