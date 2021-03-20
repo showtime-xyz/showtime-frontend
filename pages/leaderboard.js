251a8246c682ee4fdad41d93d30ad7d1b7cc3bda
Alex <alex.kilkka@gmail.com>
2021-03-19 18:51:12 -0700

    Fix URL typo

--- pages/leaderboard.js
+++ pages/leaderboard.js
@@ -73,7 +73,7 @@ const Leaderboard = () => {
         <meta name="twitter:description" content="Trending creators" />
         <meta
           name="twitter:image"
-          content="https://storage.googleapis.com/showtime-nft-thumbnails/trending_twitter_card_2.jpg"
+          content="https://storage.googleapis.com/showtime-nft-thumbnails/trending_twitter_card2.jpg"
         />
       </Head>
       {columns && (
