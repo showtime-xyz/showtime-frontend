71fd4f09f48d6a77ea073ffb2074dd063c59019f
Nico Glennon <nicoglennon@icloud.com>
2021-04-05 10:35:27 -0500

    Update brekadown on Like card

--- components/ActivityTypes/Like.js
+++ components/ActivityTypes/Like.js
@@ -8,17 +8,58 @@ export default function Like({ act }) {
   return (
     <div className="flex flex-col">
       <div className="mb-2 text-gray-500">
-        Liked{" "}
-        <span className="text-black">
-          {count === 1 ? (
+        {count === 1 && (
+          <>
+            Liked{" "}
             <Link href={`/t/${nfts[0].contract_address}/${nfts[0].token_id}`}>
-              <a className="hover:text-stpink">{nfts[0].title}</a>
+              <a className="text-black hover:text-stpink">{nfts[0].title}</a>
             </Link>
-          ) : (
-            `${count} items`
-          )}
-        </span>
-        .
+            .
+          </>
+        )}
+        {count === 2 && (
+          <>
+            Liked{" "}
+            <Link href={`/t/${nfts[0].contract_address}/${nfts[0].token_id}`}>
+              <a className="text-black hover:text-stpink">{nfts[0].title}</a>
+            </Link>{" "}
+            and{" "}
+            <Link href={`/t/${nfts[1].contract_address}/${nfts[1].token_id}`}>
+              <a className="text-black hover:text-stpink">{nfts[1].title}</a>
+            </Link>
+            .
+          </>
+        )}
+        {count === 3 && (
+          <>
+            Liked{" "}
+            <Link href={`/t/${nfts[0].contract_address}/${nfts[0].token_id}`}>
+              <a className="text-black hover:text-stpink">{nfts[0].title}</a>
+            </Link>
+            ,{" "}
+            <Link href={`/t/${nfts[1].contract_address}/${nfts[1].token_id}`}>
+              <a className="text-black hover:text-stpink">{nfts[1].title}</a>
+            </Link>{" "}
+            and{" "}
+            <Link href={`/t/${nfts[2].contract_address}/${nfts[2].token_id}`}>
+              <a className="text-black hover:text-stpink">{nfts[2].title}</a>
+            </Link>
+            .
+          </>
+        )}
+        {count > 3 && (
+          <>
+            Liked{" "}
+            <Link href={`/t/${nfts[0].contract_address}/${nfts[0].token_id}`}>
+              <a className="text-black hover:text-stpink">{nfts[0].title}</a>
+            </Link>
+            ,{" "}
+            <Link href={`/t/${nfts[1].contract_address}/${nfts[1].token_id}`}>
+              <a className="text-black hover:text-stpink">{nfts[1].title}</a>
+            </Link>{" "}
+            and {count - 2} others.
+          </>
+        )}
       </div>
       <ActivityImages nfts={nfts} />
     </div>
