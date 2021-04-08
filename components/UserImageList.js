28068ad1199a5c0323ee959b8800eebec5f868a8
Nico Glennon <nicoglennon@icloud.com>
2021-04-07 21:11:30 -0500

    Fallback image for followed user list

--- components/UserImageList.js
+++ components/UserImageList.js
@@ -11,7 +11,13 @@ export default function UserImageList({ users }) {
           key={user.wallet_address}
         >
           <a className="mr-1">
-            <img src={user.img_url} className="h-8 w-8 rounded-full" />
+            <img
+              src={
+                user.img_url ||
+                "https://storage.googleapis.com/opensea-static/opensea-profile/4.png"
+              }
+              className="h-8 w-8 rounded-full"
+            />
           </a>
         </Link>
       ))}
