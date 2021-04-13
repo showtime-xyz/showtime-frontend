4902f8d9cbb3249a3622d7796000b53a2ddbc4ce
Nico Glennon <nicoglennon@icloud.com>
2021-04-13 12:35:38 -0500

    Login Modal

--- components/ModalLogin.js
+++ components/ModalLogin.js
@@ -7,12 +7,12 @@ import WalletConnectProvider from "@walletconnect/web3-provider";
 //import ethProvider from "eth-provider";
 import { WalletLink } from "walletlink";
 import _ from "lodash";
-import ClientOnlyPortal from "./ClientOnlyPortal";
 import backend from "../lib/backend";
 import AppContext from "../context/app-context";
 import CloseButton from "./CloseButton";
 import Web3 from "web3";
 import Fortmatic from "fortmatic";
+import ScrollableModal from "./ScrollableModal";
 
 export default function Modal({ isOpen }) {
   const context = useContext(AppContext);
@@ -151,121 +151,88 @@ export default function Modal({ isOpen }) {
   return (
     <>
       {isOpen && (
-        <ClientOnlyPortal selector="#modal">
-          <div
-            className="backdrop"
-            onClick={() => context.setLoginModalOpen(false)}
-          >
-            <div
-              className="modal"
-              style={{ color: "black" }}
-              onClick={(e) => e.stopPropagation()}
-            >
-              <CloseButton setEditModalOpen={context.setLoginModalOpen} />
-              <div className="text-3xl border-b-2 pb-2 text-center">
-                Sign in
+        <ScrollableModal
+          closeModal={() => context.setLoginModalOpen(false)}
+          contentWidth="25rem"
+        >
+          <div className="p-4">
+            <CloseButton setEditModalOpen={context.setLoginModalOpen} />
+            <div className="text-3xl border-b-2 pb-2 text-center">Sign in</div>
+            {signaturePending ? (
+              <div className="text-center py-40">
+                Pushed a request to your wallet...
               </div>
-              {signaturePending ? (
-                <div className="text-center py-40">
-                  Pushed a request to your wallet...
-                </div>
-              ) : (
-                <>
-                  <form onSubmit={handleSubmitEmail}>
-                    <div className="text-center pt-8">
-                      <div>
-                        <label htmlFor="email" className="pb-4 ">
-                          Enter your email to receive a sign in link.
-                        </label>
-                        <div
-                          className="pt-1 pb-1"
-                          style={{ color: "#444", fontSize: 13 }}
-                        >
-                          If this is your first time, it will create a new
-                          account.
-                        </div>
-                      </div>
-                      <br />
-                      <input
-                        name="email"
-                        placeholder="Email"
-                        type="email"
-                        className="border-2 w-full"
-                        autoFocus
-                        style={{
-                          color: "black",
-                          padding: 10,
-                          borderRadius: 7,
-                        }}
-                      />
-
+            ) : (
+              <>
+                <form onSubmit={handleSubmitEmail}>
+                  <div className="text-center pt-8">
+                    <div>
+                      <label htmlFor="email" className="pb-4 ">
+                        Enter your email to receive a sign in link.
+                      </label>
                       <div
-                        className="pt-8 pb-8"
+                        className="pt-1 pb-1"
                         style={{ color: "#444", fontSize: 13 }}
                       >
-                        By signing in you agree to our{" "}
-                        <a
-                          href="https://www.notion.so/Showtime-Legal-c407e36eb7cd414ca190245ca8621e68"
-                          target="_blank"
-                        >
-                          Terms & Conditions
-                        </a>
-                        .
-                      </div>
-
-                      <button className="bg-stpink text-white rounded-full px-6 py-2 cursor-pointer border-2 hover:text-stpink hover:bg-white border-stpink transition-all">
-                        <span className="text-sm md:text-base">
-                          Sign in with Email
-                        </span>
-                      </button>
-                      <div className="py-6" style={{ color: "#444" }}>
-                        — or —
+                        If this is your first time, it will create a new
+                        account.
                       </div>
                     </div>
-                  </form>
-
-                  <div className="mb-4 text-center">
-                    <button
-                      className="bg-black text-white border-black rounded-full px-6 py-2 cursor-pointer border-2 hover:text-black hover:bg-white transition-all"
-                      onClick={() => {
-                        handleSubmitWallet();
+                    <br />
+                    <input
+                      name="email"
+                      placeholder="Email"
+                      type="email"
+                      className="border-2 w-full"
+                      autoFocus
+                      style={{
+                        color: "black",
+                        padding: 10,
+                        borderRadius: 7,
                       }}
+                    />
+
+                    <div
+                      className="pt-8 pb-8"
+                      style={{ color: "#444", fontSize: 13 }}
                     >
+                      By signing in you agree to our{" "}
+                      <a
+                        href="https://www.notion.so/Showtime-Legal-c407e36eb7cd414ca190245ca8621e68"
+                        target="_blank"
+                      >
+                        Terms & Conditions
+                      </a>
+                      .
+                    </div>
+
+                    <button className="bg-stpink text-white rounded-full px-6 py-2 cursor-pointer border-2 hover:text-stpink hover:bg-white border-stpink transition-all">
                       <span className="text-sm md:text-base">
-                        Sign in with Wallet
+                        Sign in with Email
                       </span>
                     </button>
+                    <div className="py-6" style={{ color: "#444" }}>
+                      — or —
+                    </div>
                   </div>
-                </>
-              )}
-            </div>
-            <style jsx>{`
-              :global(body) {
-                overflow: hidden;
-              }
-              .backdrop {
-                position: fixed;
-                background-color: rgba(0, 0, 0, 0.7);
-                top: 0;
-                right: 0;
-                bottom: 0;
-                left: 0;
-              }
-              .modal {
-                background-color: white;
-                position: absolute;
-                top: ${context.isMobile ? 5 : 10}%;
-                right: 3%;
-                left: 3%;
-                padding: 1em;
-                border-radius: 7px;
-                max-width: 400px;
-                margin-left: auto;
-                margin-right: auto;
-              }
-            `}</style>
+                </form>
+
+                <div className="mb-4 text-center">
+                  <button
+                    className="bg-black text-white border-black rounded-full px-6 py-2 cursor-pointer border-2 hover:text-black hover:bg-white transition-all"
+                    onClick={() => {
+                      handleSubmitWallet();
+                    }}
+                  >
+                    <span className="text-sm md:text-base">
+                      Sign in with Wallet
+                    </span>
+                  </button>
+                </div>
+              </>
+            )}
           </div>
-        </ClientOnlyPortal>
+        </ScrollableModal>
       )}
     </>
   );
