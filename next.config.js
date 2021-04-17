ff285ef11403c6653587b6903f338946a68f217d
Alex <alex.kilkka@gmail.com>
2021-04-16 18:02:34 -0700

    Add preact and webpack5

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = (phase, { defaultConfig }) => {
-  return {
-    env: {},
-  };
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  webpack: (config, { dev, isServer }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    return config;
+  },
 };
