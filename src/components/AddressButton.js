14e6ee1645fba508415dca3b5afc92a377c23bfd
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-05-05 21:01:15 +0200

    Show copied tooltip

--- src/components/AddressButton.js
+++ src/components/AddressButton.js
@@ -6,19 +6,15 @@ const AddressButton = ({ address }) => {
 	const [isCopied, setIsCopied] = useState(false)
 
 	return (
-		<Tippy content={isCopied ? 'Copied!' : 'Copy address'}>
+		<Tippy content={isCopied ? 'Copied!' : 'Copy address'} hideOnClick={false}>
 			<div
 				//className="py-1 px-3  rounded-full mr-1 md:mr-2 hover:bg-purple-200 bg-purple-100 text-purple-500 transition-all text-xs mt-1 "
 				className="py-1 px-3  rounded-full mr-1 md:mr-2 hover:bg-opacity-20 bg-black text-gray-800 transition-all text-xs mt-1 md:mt-0 bg-opacity-10 cursor-pointer"
 				key={address}
-				onMouseOut={() => {
-					setTimeout(function () {
-						setIsCopied(false)
-					}, 3000)
-				}}
 				onClick={() => {
 					setIsCopied(true)
 					copyToClipBoard(address)
+					setTimeout(() => setIsCopied(false), 1000)
 				}}
 			>
 				{formatAddressShort(address)}
