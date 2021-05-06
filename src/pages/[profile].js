25b87f2e0cda0dd6c03c547be6117114011f1e37
Nico Glennon <nicoglennon@icloud.com>
2021-05-06 10:55:42 -0500

    UI width issue on dropdown

--- src/pages/[profile].js
+++ src/pages/[profile].js
@@ -1145,7 +1145,7 @@ const Profile = ({ profile, slug_address, followers_list, followers_count, follo
 															</Menu.Button>
 														</div>
 														<Transition show={open} as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
-															<Menu.Items static className="z-1 absolute right-0 w-auto mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+															<Menu.Items static className="z-1 absolute right-0 mt-2 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" style={{ width: 200 }}>
 																<div className="px-1 py-1 ">
 																	<Menu.Item>
 																		{({ active }) => (
