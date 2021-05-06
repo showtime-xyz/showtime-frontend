c927ebec075781d225fb7ac8720b66ff928decf2
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-05-06 17:34:52 +0200

    Django is dumb

--- src/pages/api/feedback.js
+++ src/pages/api/feedback.js
@@ -7,18 +7,22 @@ export default handler().post(async ({ body: { description }, cookies }, res) =>
 	let user
 	try {
 		user = await Iron.unseal(CookieService.getAuthToken(cookies), process.env.ENCRYPTION_SECRET_V2, Iron.defaults)
-	} finally {
-		await backend.post(
-			'/v1/feedback',
-			{ description },
-			{
-				headers: {
-					'X-Authenticated-User': user?.publicAddress || null,
-					'X-API-Key': process.env.SHOWTIME_FRONTEND_API_KEY_V2,
-				},
-			}
-		)
+	} catch {
+		//user is logged out
 	}
+
+	console.log(user?.publicAddress || null)
+
+	await backend.post(
+		'/v1/feedback',
+		{ description },
+		{
+			headers: {
+				'X-Authenticated-User': user?.publicAddress || 'undefined', // Django is dumb ¯\_(ツ)_/¯
+				'X-API-Key': process.env.SHOWTIME_FRONTEND_API_KEY_V2,
+			},
+		}
+	)
 
 	res.status(200).end()
 })
