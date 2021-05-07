a63adc539a219c92873a44ae2baf4c60aaa4c00d
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-05-07 22:41:08 +0200

    rename styles file

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import '@/styles/globals.css'
+import '@/styles/styles.css'
 import AppContext from '@/context/app-context'
 import mixpanel from 'mixpanel-browser'
 import Router from 'next/router'
