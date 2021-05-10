dec03a39429a60ca8d1447b7ff0f41ddd7c18eeb
Rossi Meacham <rossi@tryshowtime.com>
2021-05-10 13:32:16 -0400

    handle error in the api-handler

--- src/lib/api-handler.js
+++ src/lib/api-handler.js
@@ -9,7 +9,9 @@ export default () =>
 		onError: (err, req, res) => {
 			if (process.env.NODE_ENV === 'development') {
 				console.error(err)
-
+				if (err.response.data.error.code === 429) {
+					return res.status(429).json(err.response.data.error)
+				}
 				return res.status(500).json(err)
 			}
 			// Log the exception on Sentry
@@ -23,6 +25,9 @@ export default () =>
 				captureException(err)
 			})
 
+			if (err.response.data.error.code === 429) {
+				return res.status(429).send(err.response.data.error.message)
+			}
 			res.status(500).send('Internal Server Error')
 		},
 	})
