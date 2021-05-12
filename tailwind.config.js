1299217e96880095c9f4ee9fe7291b519919f0d4
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-05-12 19:50:46 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
 	mode: 'jit',
 	purge: ['./src/**/*.{js,ts,jsx,tsx}'],
-	darkMode: false, // or 'media' or 'class'
+	darkMode: 'class', // or 'media' or 'class'
 	theme: {
 		extend: {
 			maxWidth: {
