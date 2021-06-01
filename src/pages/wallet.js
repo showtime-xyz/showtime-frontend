635d1068e90b4859ce4931294e8b763cb548cd6f
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-06-01 20:12:32 +0200

    wip

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -27,7 +27,7 @@ const Wallet = () => {
 
 		setHasEmailAddress(myProfile.wallet_addresses_v2.length !== myProfile.wallet_addresses_excluding_email_v2.length)
 		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [myProfile?.wallet_addresses_v2])
+	}, [myProfile?.wallet_addresses_v2, myProfile?.wallet_addresses_excluding_email_v2])
 
 	const loginWithTezos = async () => {
 		const activeAccount = await dAppClient.getActiveAccount()
@@ -42,6 +42,8 @@ const Wallet = () => {
 
 		const { signature } = await dAppClient.requestSignPayload({ signingType: SigningType.RAW, payload: process.env.NEXT_PUBLIC_SIGNING_MESSAGE_ADD_WALLET + nonce })
 		await axios.put('/api/auth/wallet/tz', { address: tezosAddr, signature, publicKey: tezosPk })
+
+		setWalletAddresses(walletAddresses => [...walletAddresses, { address: tezosAddr, ens_domain: null }])
 
 		dAppClient.clearActiveAccount()
 	}
@@ -72,77 +74,79 @@ const Wallet = () => {
 			<Layout>
 				<div className="flex-1 flex items-center justify-center">
 					{myProfile ? (
-						<div className="flex flex-col">
-							<div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-								<div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-									<div className="shadow overflow-hidden border-b dark:border-t dark:border-l dark:border-r border-gray-200 dark:border-gray-800 sm:rounded-lg">
-										<div className="bg-white dark:bg-gray-900 px-4 py-5 border-b border-gray-200 dark:border-gray-800 sm:px-6">
-											<div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
-												<div className="ml-4 mt-2">
-													<h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-400">Wallets</h3>
-												</div>
-												<div className="ml-4 mt-2 flex-shrink-0">
-													<Menu as="div" className="relative inline-block text-left">
-														{({ open }) => (
-															<>
-																<div>
-																	<Menu.Button className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white dark:text-gray-100 bg-indigo-600 dark:bg-indigo-800 hover:bg-indigo-700 dark:hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:ring-indigo-900 transition">Connect Wallet</Menu.Button>
-																</div>
+						<div className="w-full flex flex-col max-w-2xl mx-auto  shadow sm:rounded-lg border border-transparent dark:border-gray-800 bg-white dark:bg-gray-900">
+							<div className="px-4 py-5 border-b border-gray-200 dark:border-gray-800 sm:px-6">
+								<div className="-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap">
+									<div className="ml-4 mt-2">
+										<h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-400">Wallets</h3>
+									</div>
+									<div className="ml-4 mt-2 flex-shrink-0">
+										<Menu as="div" className="relative inline-block text-left">
+											{({ open }) => (
+												<>
+													<div>
+														<Menu.Button className="relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white dark:text-gray-100 bg-indigo-600 dark:bg-indigo-800 hover:bg-indigo-700 dark:hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:ring-indigo-900 transition">Connect Wallet</Menu.Button>
+													</div>
 
-																<Transition show={open} as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
-																	<Menu.Items static className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg border border-transparent dark:border-gray-800 bg-white dark:bg-gray-900 ring-1 ring-black ring-opacity-5 focus:outline-none">
-																		<div className="py-1">
-																			<Menu.Item>
-																				{({ active }) => (
-																					<button onClick={() => setWalletModalOpen(true)} className={classNames(active ? 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-400' : 'hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-700 dark:text-gray-500', 'block w-full text-left px-4 py-2 text-sm transition')}>
-																						Link Ethereum wallet
-																					</button>
-																				)}
-																			</Menu.Item>
-																			<Menu.Item>
-																				{({ active }) => (
-																					<button onClick={loginWithTezos} className={classNames(active ? 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-400' : 'hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-700 dark:text-gray-500', 'block w-full text-left px-4 py-2 text-sm transition')}>
-																						Link Tezos wallet
-																					</button>
-																				)}
-																			</Menu.Item>
-																			{!hasEmailAddress && (
-																				<Menu.Item>
-																					{({ active }) => (
-																						<button onClick={() => setEmailModalOpen(true)} className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full text-left px-4 py-2 text-sm')}>
-																							Link Email Address
-																						</button>
-																					)}
-																				</Menu.Item>
-																			)}
-																		</div>
-																	</Menu.Items>
-																</Transition>
-															</>
-														)}
-													</Menu>
-												</div>
-											</div>
+													<Transition show={open} as={Fragment} enter="transition ease-out duration-100" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
+														<Menu.Items static className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg border border-transparent dark:border-gray-800 bg-white dark:bg-gray-900 ring-1 ring-black ring-opacity-5 focus:outline-none">
+															<div className="py-1">
+																<Menu.Item>
+																	{({ active }) => (
+																		<button onClick={() => setWalletModalOpen(true)} className={classNames(active ? 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-400' : 'hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-700 dark:text-gray-500', 'block w-full text-left px-4 py-2 text-sm transition')}>
+																			Link Ethereum wallet
+																		</button>
+																	)}
+																</Menu.Item>
+																<Menu.Item>
+																	{({ active }) => (
+																		<button onClick={loginWithTezos} className={classNames(active ? 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-400' : 'hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-700 dark:text-gray-500', 'block w-full text-left px-4 py-2 text-sm transition')}>
+																			Link Tezos wallet
+																		</button>
+																	)}
+																</Menu.Item>
+																{!hasEmailAddress && (
+																	<Menu.Item>
+																		{({ active }) => (
+																			<button onClick={() => setEmailModalOpen(true)} className={classNames(active ? 'bg-gray-100 text-gray-900' : 'text-gray-700', 'block w-full text-left px-4 py-2 text-sm')}>
+																				Link Email Address
+																			</button>
+																		)}
+																	</Menu.Item>
+																)}
+															</div>
+														</Menu.Items>
+													</Transition>
+												</>
+											)}
+										</Menu>
+									</div>
+								</div>
+							</div>
+							<div className="overflow-hidden rounded-lg">
+								<div className="overflow-x-auto w-full">
+									<div className="align-middle inline-block min-w-full">
+										<div className="overflow-hidden">
+											<table className="min-w-full divide-y divide-gray-200">
+												<tbody>
+													{walletAddresses?.map(({ address, ens_domain }, i) => (
+														<tr key={address} className={i % 2 === 0 ? 'bg-white dark:bg-gray-800' : 'bg-gray-50 dark:bg-gray-900'}>
+															<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-400">{ens_domain || address}</td>
+															<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{address.startsWith('tz') ? 'Tezos' : 'Ethereum'}</td>
+															<td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+																{address.toLowerCase() === user.publicAddress ? (
+																	<span className="text-gray-600 dark:text-gray-500">Active</span>
+																) : (
+																	<button onClick={() => unlinkAddress(address)} className="text-red-600 hover:text-red-900 dark:hover:text-red-700 transition">
+																		Unlink
+																	</button>
+																)}
+															</td>
+														</tr>
+													))}
+												</tbody>
+											</table>
 										</div>
-										<table className="min-w-full divide-y divide-gray-200">
-											<tbody>
-												{walletAddresses?.map(({ address, ens_domain }, i) => (
-													<tr key={address} className={i % 2 === 0 ? 'bg-white dark:bg-gray-800' : 'bg-gray-50 dark:bg-gray-900'}>
-														<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-400">{ens_domain || address}</td>
-														<td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{address.startsWith('tz') ? 'Tezos' : 'Ethereum'}</td>
-														<td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-															{address.toLowerCase() === user.publicAddress ? (
-																<span className="text-gray-600 dark:text-gray-500">Active</span>
-															) : (
-																<button onClick={() => unlinkAddress(address)} className="text-red-600 hover:text-red-900 dark:hover:text-red-700 transition">
-																	Unlink
-																</button>
-															)}
-														</td>
-													</tr>
-												))}
-											</tbody>
-										</table>
 									</div>
 								</div>
 							</div>
