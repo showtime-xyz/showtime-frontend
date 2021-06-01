dd6963ada0667b46341bda6a4b0ffc9727be8cb8
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-06-01 20:14:15 +0200

    wip

--- src/pages/wallet.js
+++ src/pages/wallet.js
@@ -152,9 +152,9 @@ const Wallet = () => {
 							</div>
 						</div>
 					) : (
-						<div className="bg-white shadow sm:rounded-lg">
+						<div className="bg-white border border-transparent dark:border-gray-800 dark:bg-gray-900 shadow sm:rounded-lg">
 							<div className="px-4 py-5 sm:p-6">
-								<h3 className="text-lg leading-6 font-medium text-gray-900">Log in to manage your wallets</h3>
+								<h3 className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-300">Log in to manage your wallets</h3>
 								<div className="mt-2 max-w-xl text-sm text-gray-500">
 									<p>Once you're logged in, you'll be able to connect and disconnect your Ethereum and Tezos wallets.</p>
 								</div>
