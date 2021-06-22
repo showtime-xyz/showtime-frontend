5fefd1d2f0d0f68f81be6ced6c88b6617279596c
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-06-22 14:41:07 +0200

    wip

--- src/pages/[profile].js
+++ src/pages/[profile].js
@@ -81,7 +81,7 @@ const Profile = ({ profile, slug_address, followers_list, followers_count, follo
 	const [followersCount, setFollowersCount] = useState(followers_count)
 
 	// Using global context for logged in user, else server data for other pages
-	const { name, img_url, cover_url, wallet_addresses_v2, wallet_addresses_excluding_email_v2, bio, website_url, username, featured_nft_img_url, links } = isMyProfile ? context.myProfile : profile
+	const { name, img_url, cover_url, wallet_addresses_v2, wallet_addresses_excluding_email_v2, bio, website_url, username, featured_nft_img_url, links, verified } = isMyProfile ? context.myProfile : profile
 	const { profile_id } = profile
 
 	useEffect(() => {
@@ -699,7 +699,7 @@ const Profile = ({ profile, slug_address, followers_list, followers_count, follo
 									<div>
 										<div className="flex items-center space-x-2">
 											<h2 className="text-4xl font-tomato font-bold"> {name ? name : username ? username : wallet_addresses_excluding_email_v2 && wallet_addresses_excluding_email_v2.length > 0 ? (wallet_addresses_excluding_email_v2[0].ens_domain ? wallet_addresses_excluding_email_v2[0].ens_domain : formatAddressShort(wallet_addresses_excluding_email_v2[0].address)) : 'Unnamed'}</h2>
-											<BadgeIcon className="w-6 h-auto text-black dark:text-white" bgClass="text-white dark:text-black" />
+											{verified && <BadgeIcon className="w-6 h-auto text-black dark:text-white" bgClass="text-white dark:text-black" />}
 										</div>
 										<div className="mt-2">{(username || (wallet_addresses_excluding_email_v2 && wallet_addresses_excluding_email_v2.length > 0)) && <p className="flex flex-row items-center justify-start">{username && <div className="font-tomato font-bold tracking-wider dark:text-gray-300">@{username}</div>}</p>}</div>
 										<div>
