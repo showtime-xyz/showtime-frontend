d81eaa2894baee107c843cfa217beda8598e184f
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-06-23 20:31:17 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ const colors = require('tailwindcss/colors')
 module.exports = {
 	mode: 'jit',
 	purge: ['./src/**/*.{js,ts,jsx,tsx}'],
-	darkMode: 'class', // or 'media' or 'class'
+	darkMode: 'class',
 	theme: {
 		extend: {
 			maxWidth: {
