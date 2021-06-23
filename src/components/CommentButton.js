a2f2443fee455b44ab5b26ac6627233a568fd1a6
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-06-23 19:08:16 +0200

    wip

--- src/components/CommentButton.js
+++ src/components/CommentButton.js
@@ -3,10 +3,11 @@ import AppContext from '@/context/app-context'
 import mixpanel from 'mixpanel-browser'
 import CommentIcon from './Icons/CommentIcon'
 import Tippy from '@tippyjs/react'
+import useAuth from '@/hooks/useAuth'
 
 const CommentButton = ({ item, handleComment }) => {
+	const { isAuthenticated } = useAuth()
 	const context = useContext(AppContext)
-	const { isMobile } = context
 	const comment_count = (context.myCommentCounts && context.myCommentCounts[item?.nft_id]) || item.comment_count
 	const commented = context.myComments?.includes(item.nft_id)
 
@@ -16,10 +17,10 @@ const CommentButton = ({ item, handleComment }) => {
 	}
 
 	return (
-		<Tippy content="Sign in to comment" disabled={context.user || isMobile}>
-			<button className="focus:outline-none focus-visible:ring-1" disabled={context.disableComments} onClick={context.user ? handleComment : handleLoggedOutComment}>
-				<div className={`flex flex-row items-center rounded-lg py-1 dark:text-gray-300 hover:text-blue-500 ${context.disableComments ? 'hover:text-gray-500 text-gray-500' : ''}`}>
-					<div className={`flex ${commented ? 'text-blue-500 dark:text-blue-600' : 'hover:text-blue-400 dark:hover:text-blue-400'}`}>
+		<Tippy content="Sign in to comment" disabled={isAuthenticated || context.isMobile}>
+			<button className="focus:outline-none hover:bg-blue-50 dark:hover:bg-blue-900 focus:bg-blue-50 dark:focus:bg-blue-900 px-2 -mx-2 rounded-xl" disabled={context.disableComments} onClick={isAuthenticated ? handleComment : handleLoggedOutComment}>
+				<div className={`flex flex-row items-center rounded-lg py-1 dark:text-gray-300 ${commented ? 'text-blue-500 dark:text-blue-600' : 'hover:text-blue-400 dark:hover:text-blue-400'} ${context.disableComments ? 'hover:text-gray-500 text-gray-500' : ''}`}>
+					<div className={'flex'}>
 						<CommentIcon className="w-5 h-5" />
 					</div>
 					<div className="ml-1 whitespace-nowrap">{comment_count}</div>
--- src/components/LikeButton.js
+++ src/components/LikeButton.js
@@ -5,10 +5,11 @@ import _ from 'lodash'
 import Tippy from '@tippyjs/react'
 import axios from '@/lib/axios'
 import HeartIcon from './Icons/HeartIcon'
+import useAuth from '@/hooks/useAuth'
 
 const LikeButton = ({ item }) => {
+	const { isAuthenticated } = useAuth()
 	const context = useContext(AppContext)
-	const { isMobile } = context
 
 	const handleLike = async nft_id => {
 		// Change myLikes via setMyLikes
@@ -23,9 +24,7 @@ const LikeButton = ({ item }) => {
 		try {
 			await axios
 				.post(`/api/like_v3/${nft_id}`)
-				.then(() => {
-					mixpanel.track('Liked item')
-				})
+				.then(() => mixpanel.track('Liked item'))
 				.catch(err => {
 					if (err.response.data.code === 429) {
 						// Change myLikes via setMyLikes
@@ -67,10 +66,10 @@ const LikeButton = ({ item }) => {
 	}
 
 	return (
-		<Tippy content="Sign in to like" disabled={context.user || isMobile}>
-			<button className="focus:outline-none focus-visible:ring-1" disabled={context.disableLikes} onClick={() => (context.user ? (liked ? handleUnlike(item.nft_id) : handleLike(item.nft_id)) : handleLoggedOutLike())}>
-				<div className={`flex flex-row items-center rounded-md py-1 dark:text-gray-300 hover:text-red-500 ${context.disableLikes ? 'hover:text-gray-500 text-gray-500' : ''}`}>
-					<div className={`flex ${liked ? 'text-red-500 dark:text-red-600' : 'hover:text-red-400 dark:hover:text-red-500'}`}>
+		<Tippy content="Sign in to like" disabled={isAuthenticated || context.isMobile}>
+			<button className="focus:outline-none hover:bg-red-50 dark:hover:bg-red-900 focus:bg-red-50 dark:focus:bg-red-900 px-2 -mx-2 rounded-xl" disabled={context.disableLikes} onClick={() => (isAuthenticated ? (liked ? handleUnlike(item.nft_id) : handleLike(item.nft_id)) : handleLoggedOutLike())}>
+				<div className={`flex flex-row items-center rounded-md py-1 dark:text-gray-300 ${liked ? 'text-red-500 dark:text-red-600' : ''} ${context.disableLikes ? 'hover:text-gray-500 text-gray-500' : ''}`}>
+					<div className={'flex'}>
 						<HeartIcon className="w-5 h-5" />
 					</div>
 					<div className="ml-1 whitespace-nowrap">{Number(like_count < 0 ? 0 : like_count).toLocaleString()}</div>
