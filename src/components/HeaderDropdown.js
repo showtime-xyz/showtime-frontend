b21b49ce08b0bdac9ca1ea7c684569c774f11fe0
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-06-24 17:39:14 +0200

    wip

--- src/components/HeaderDropdown.js
+++ src/components/HeaderDropdown.js
@@ -47,8 +47,8 @@ const HeaderDropdown = () => {
 						<div>
 							<Menu.Button className={({ open }) => `max-w-xs ${open ? 'bg-black dark:bg-white bg-opacity-10 dark:bg-opacity-10 backdrop-filter backdrop-blur-lg backdrop-saturate-150' : 'hover:bg-black dark:hover:bg-white hover:bg-opacity-10 dark:hover:bg-opacity-10 hover:backdrop-filter hover:backdrop-blur-lg hover:backdrop-saturate-150'} p-1 -m-1 md:py-1 md:pl-2.5 md:pr-1 md:-my-1 md:-ml-2.5 md:-mr-1 flex items-center text-sm rounded-full focus:outline-none group`}>
 								<span className="sr-only">Open user menu</span>
-								<span className="hidden md:inline truncate dark:text-gray-200 transition font-medium">{myProfile?.name || myProfile?.username || myProfile.wallet_addresses_excluding_email_v2?.[0]?.ens_domain || formatAddressShort(myProfile.wallet_addresses_excluding_email_v2?.[0]?.address) || 'Profile'}</span>
-								<img className="h-8 w-8 rounded-full md:ml-2" src={myProfile?.img_url || DEFAULT_PROFILE_PIC} alt={myProfile?.name || myProfile?.username || myProfile.wallet_addresses_excluding_email_v2?.[0]?.ens_domain || formatAddressShort(myProfile.wallet_addresses_excluding_email_v2?.[0]?.address) || 'Profile'} />
+								<span className="hidden md:inline truncate dark:text-gray-200 transition font-medium">{myProfile?.name || myProfile?.username || myProfile?.wallet_addresses_excluding_email_v2?.[0]?.ens_domain || formatAddressShort(myProfile?.wallet_addresses_excluding_email_v2?.[0]?.address) || 'Profile'}</span>
+								<img className="h-8 w-8 rounded-full md:ml-2" src={myProfile?.img_url || DEFAULT_PROFILE_PIC} alt={myProfile?.name || myProfile?.username || myProfile?.wallet_addresses_excluding_email_v2?.[0]?.ens_domain || formatAddressShort(myProfile?.wallet_addresses_excluding_email_v2?.[0]?.address) || 'Profile'} />
 							</Menu.Button>
 						</div>
 						<Transition show={open} as={Fragment} enter="transition ease-out duration-200" enterFrom="transform opacity-0 scale-95" enterTo="transform opacity-100 scale-100" leave="transition ease-in duration-75" leaveFrom="transform opacity-100 scale-100" leaveTo="transform opacity-0 scale-95">
@@ -58,7 +58,7 @@ const HeaderDropdown = () => {
 								</Menu.Item>
 								<div className="mt-0.5 md:mt-0 rounded-2xl shadow-lg border border-transparent dark:border-gray-800 bg-white dark:bg-gray-900 space-y-4 divide-y divide-gray-100 dark:divide-gray-800 relative z-20 min-w-[10rem] w-full md:w-auto py-1 px-2">
 									<div className="py-1">
-										<Menu.Item as={NextLink} className={({ active }) => classNames(active ? 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-400' : 'hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-800 dark:text-gray-500', 'block rounded-lg w-full text-left py-2 pr-6 pl-2 text-sm font-semibold transition')} href={`/${myProfile?.username || myProfile.wallet_addresses_excluding_email_v2?.[0]?.ens_domain || myProfile.wallet_addresses_excluding_email_v2?.[0]?.address || user.publicAddress}`}>
+										<Menu.Item as={NextLink} className={({ active }) => classNames(active ? 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-400' : 'hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-800 dark:text-gray-500', 'block rounded-lg w-full text-left py-2 pr-6 pl-2 text-sm font-semibold transition')} href={`/${myProfile?.username || myProfile?.wallet_addresses_excluding_email_v2?.[0]?.ens_domain || myProfile?.wallet_addresses_excluding_email_v2?.[0]?.address || user.publicAddress}`}>
 											Your Profile
 										</Menu.Item>
 										<Menu.Item as={NextLink} className={({ active }) => classNames(active ? 'bg-gray-100 dark:bg-gray-800 text-gray-900 dark:text-gray-400' : 'hover:bg-gray-100 dark:hover:bg-gray-800 text-gray-800 dark:text-gray-500', 'block rounded-lg w-full text-left py-2 pr-6 pl-2 text-sm font-semibold transition')} href="/wallet">
