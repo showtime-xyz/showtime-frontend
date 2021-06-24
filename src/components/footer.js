b6b7def98daf6ca066e2f528ccc5c97ba2d09197
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-06-24 13:58:36 +0200

    wip

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,13 @@
 import { MailIcon } from '@heroicons/react/solid'
 import { useState } from 'react'
 import ModalFeedback from './ModalFeedback'
-import CappedWidth from './CappedWidth'
+import showtimeLogo from '../../public/img/logo.png'
+import mixpanel from 'mixpanel-browser'
+import Link from 'next/link'
+import TwitterIcon from './Icons/Social/TwitterIcon'
+import InstagramIcon from './Icons/Social/InstagramIcon'
+import DiscordIcon from './Icons/Social/DiscordIcon'
+import EmailIcon from './Icons/Social/EmailIcon'
 
 const Footer = () => {
 	const [isFeedbackModalOpen, setIsFeedbackModalOpen] = useState(false)
@@ -13,44 +19,44 @@ const Footer = () => {
 					<ModalFeedback isOpen={isFeedbackModalOpen} closeModal={() => setIsFeedbackModalOpen(false)} />
 				</>
 			) : null}
-			<footer className="text-center bg-black px-10">
-				<CappedWidth>
-					<div className="px-3 text-sm my-8 py-4 text-center text-gray-400 flex flex-col sm:flex-row items-center justify-between space-y-4 sm:space-y-0">
-						<div>&copy; {new Date().getFullYear()} Showtime Technologies, Inc.</div>
-						<div className="flex flex-row space-x-3">
-							<a href="mailto:help@tryshowtime.com" target="_blank" className="hover:text-gray-300 transition -mt-0.5" rel="noreferrer">
-								<MailIcon className="w-6 h-6" />
-							</a>
-							<a href="https://twitter.com/tryShowtime" target="_blank" className="hover:text-gray-300 transition" rel="noreferrer">
-								<svg aria-hidden="true" className="w-5 h-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-									<path fill="currentColor" d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" />
-								</svg>
-							</a>
-							<a href="https://www.instagram.com/tryshowtime/" target="_blank" className="hover:text-gray-300 transition" rel="noreferrer">
-								<svg aria-hidden="true" className="w-5 h-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-									<path fill="currentColor" d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z" />
-								</svg>
-							</a>
-							<a href="https://discord.gg/FBSxXrcnsm" target="_blank" className="hover:text-gray-300 transition" rel="noreferrer">
-								<svg aria-hidden="true" className="w-5 h-5" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-									<path
-										fill="currentColor"
-										d="M297.216 243.2c0 15.616-11.52 28.416-26.112 28.416-14.336 0-26.112-12.8-26.112-28.416s11.52-28.416 26.112-28.416c14.592 0 26.112 12.8 26.112 28.416zm-119.552-28.416c-14.592 0-26.112 12.8-26.112 28.416s11.776 28.416 26.112 28.416c14.592 0 26.112-12.8 26.112-28.416.256-15.616-11.52-28.416-26.112-28.416zM448 52.736V512c-64.494-56.994-43.868-38.128-118.784-107.776l13.568 47.36H52.48C23.552 451.584 0 428.032 0 398.848V52.736C0 23.552 23.552 0 52.48 0h343.04C424.448 0 448 23.552 448 52.736zm-72.96 242.688c0-82.432-36.864-149.248-36.864-149.248-36.864-27.648-71.936-26.88-71.936-26.88l-3.584 4.096c43.52 13.312 63.744 32.512 63.744 32.512-60.811-33.329-132.244-33.335-191.232-7.424-9.472 4.352-15.104 7.424-15.104 7.424s21.248-20.224 67.328-33.536l-2.56-3.072s-35.072-.768-71.936 26.88c0 0-36.864 66.816-36.864 149.248 0 0 21.504 37.12 78.08 38.912 0 0 9.472-11.52 17.152-21.248-32.512-9.728-44.8-30.208-44.8-30.208 3.766 2.636 9.976 6.053 10.496 6.4 43.21 24.198 104.588 32.126 159.744 8.96 8.96-3.328 18.944-8.192 29.44-15.104 0 0-12.8 20.992-46.336 30.464 7.68 9.728 16.896 20.736 16.896 20.736 56.576-1.792 78.336-38.912 78.336-38.912z"
-									/>
-								</svg>
-							</a>
-						</div>
-						<div>
-							<a href="https://www.notion.so/Showtime-Legal-c407e36eb7cd414ca190245ca8621e68" target="_blank" className="hover:text-gray-300 transition" rel="noreferrer">
-								Terms &amp; Conditions
-							</a>{' '}
-							·{' '}
-							<a className="cursor-pointer hover:text-gray-300 transition" onClick={() => setIsFeedbackModalOpen(true)}>
-								Feedback
-							</a>
-						</div>
+			<footer className="bg-gray-900 py-24 px-40 flex justify-between">
+				<div className="space-y-4">
+					<Link href="/">
+						<a className="flex items-center space-x-2" onClick={() => mixpanel.track('Footer logo click')}>
+							<img src={showtimeLogo.src} alt="Showtime logo" className="flex-shrink-0 rounded-lg overflow-hidden w-8 h-8" />
+							<span className="font-bold text-gray-100">Showtime</span>
+						</a>
+					</Link>
+					<p className="text-gray-400 font-medium text-sm">&copy; {new Date().getFullYear()} Showtime Technologies, Inc.</p>
+				</div>
+				<div className="flex space-x-12">
+					<div className="space-y-4">
+						<a className="text-gray-200 font-bold flex items-center space-x-2" href="https://twitter.com/tryShowtime" target="_blank" rel="noopener noreferrer">
+							<TwitterIcon className="w-4 h-4" />
+							<span>Twitter</span>
+						</a>
+						<a className="text-gray-200 font-bold flex items-center space-x-2" href="https://www.instagram.com/tryshowtime/" target="_blank" rel="noopener noreferrer">
+							<InstagramIcon className="w-4 h-4" />
+							<span>Instagram</span>
+						</a>
+						<a className="text-gray-200 font-bold flex items-center space-x-2" href="https://discord.gg/FBSxXrcnsm" target="_blank" rel="noopener noreferrer">
+							<DiscordIcon className="w-4 h-4" />
+							<span>Discord</span>
+						</a>
+						<a className="text-gray-200 font-bold flex items-center space-x-2" href="mailto:help@tryshowtime.com" target="_blank" rel="noopener noreferrer">
+							<EmailIcon className="w-4 h-auto" />
+							<span>Contact</span>
+						</a>
 					</div>
-				</CappedWidth>
+					<div className="space-y-4 flex flex-col">
+						<a className="text-gray-200 font-bold" href="https://www.notion.so/Showtime-Legal-c407e36eb7cd414ca190245ca8621e68" target="_blank" rel="noopener noreferrer">
+							Terms &amp; Conditions
+						</a>
+						<a className="text-gray-200 font-bold" href="https://showtime.nolt.io" target="_blank" rel="noopener noreferrer">
+							Feedback
+						</a>
+					</div>
+				</div>
 			</footer>
 		</>
 	)
