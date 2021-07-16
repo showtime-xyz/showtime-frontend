51e5e45bfa629996aee5c071edd9b1e713e388e5
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-07-16 13:43:46 +0200

    Different pages using the same component should still be rerendered to prevent data leakage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,7 +3,7 @@ import '@/styles/styles.css'
 import { DISABLE_ALL } from '@/lib/constants'
 import AppContext from '@/context/app-context'
 import mixpanel from 'mixpanel-browser'
-import Router from 'next/router'
+import Router, { useRouter } from 'next/router'
 import ProgressBar from '@badrap/bar-of-progress'
 import ModalThrottleUser from '@/components/ModalThrottleUser'
 import axios from '@/lib/axios'
@@ -26,6 +26,7 @@ Router.events.on('routeChangeComplete', progress.finish)
 Router.events.on('routeChangeError', progress.finish)
 
 const App = ({ Component, pageProps }) => {
+	const router = useRouter()
 	const { revalidate } = useAuth()
 	const [myProfile, setMyProfile] = useState(null)
 	const [user, setUser] = useState()
@@ -246,7 +247,7 @@ const App = ({ Component, pageProps }) => {
 		<ThemeProvider defaultTheme="system" disableTransitionOnChange={true} attribute="class">
 			<AppContext.Provider value={injectedGlobalContext}>
 				<ModalThrottleUser isOpen={throttleOpen} closeModal={() => setThrottleOpen(false)} modalContent={throttleContent} />
-				<Component {...pageProps} />
+				<Component {...pageProps} key={router.asPath} />
 			</AppContext.Provider>
 		</ThemeProvider>
 	)
