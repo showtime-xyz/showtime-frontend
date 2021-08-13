0e4e560e717884aeed579017c2163ef0705d2356
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-08-13 18:28:34 +0200

    Show 3D icon on feed & use placeholders

--- src/components/ActivityImage.js
+++ src/components/ActivityImage.js
@@ -4,6 +4,7 @@ import mixpanel from 'mixpanel-browser'
 import LikeButton from './LikeButton'
 import CommentButton from './CommentButton'
 import AppContext from '@/context/app-context'
+import OrbitIcon from './Icons/OrbitIcon'
 
 export default function ActivityImage({ nft, index, numberOfImages, openModal, spacingIndex, bottomRow, roundAllCorners, totalNumberOfImages, cardWidth }) {
 	useEffect(() => {
@@ -15,6 +16,7 @@ export default function ActivityImage({ nft, index, numberOfImages, openModal, s
 	const { isMobile } = useContext(AppContext)
 	const [imgWidth, setImgWidth] = useState(null)
 	const [isHovering, setIsHovering] = useState(false)
+	const [showModel, setShowModel] = useState(false)
 
 	useEffect(() => {
 		setImgWidth(aRef?.current?.clientWidth)
@@ -52,6 +54,14 @@ export default function ActivityImage({ nft, index, numberOfImages, openModal, s
 		return img_url
 	}
 
+	// Automatically load models that have no preview image. We don't account for video here because currently token_animation_url is a glb file.
+	useEffect(() => {
+		if (!nft || !nft.mime_type?.startsWith('model')) return
+		if (nft.token_img_url || nft.token_img_original_url) return
+
+		setShowModel(true)
+	}, [nft])
+
 	return (
 		<div
 			className={`flex-1 relative cursor-pointer overflow-hidden hover:opacity-90 transition-all ${spacingIndex !== numberOfImages - 1 ? 'mr-1' : ''}
@@ -72,7 +82,7 @@ export default function ActivityImage({ nft, index, numberOfImages, openModal, s
 			onMouseEnter={() => setIsHovering(true)}
 			onMouseLeave={() => setIsHovering(false)}
 		>
-			{nft.mime_type?.startsWith('model') && <model-viewer src={nft.source_url} class="object-cover w-full h-full" autoplay auto-rotate ar ar-modes="scene-viewer quick-look" interaction-prompt="none" />}
+			{nft.mime_type?.startsWith('model') && showModel && <model-viewer src={nft.source_url} class="object-cover w-full h-full" autoplay camera-controls auto-rotate ar ar-modes="scene-viewer quick-look" interaction-prompt="none" onClick={event => event.stopPropagation()} />}
 			{nft.token_img_url && !((nft.token_has_video || (nft.token_animation_url && !nft.token_img_url)) && numberOfImages === 1) && <img src={numberOfImages === 1 ? getImageUrlLarge(nft.token_img_url, nft.token_aspect_ratio) : getImageUrl(nft.token_img_url, nft.token_aspect_ratio)} className="object-cover w-full h-full" />}
 			{(nft.token_has_video || (nft.token_animation_url && !nft.token_img_url)) && (!nft.token_img_url || numberOfImages === 1) && (
 				<ReactPlayer
@@ -112,6 +122,22 @@ export default function ActivityImage({ nft, index, numberOfImages, openModal, s
 							openModal(index)
 						}}
 					/>
+				</div>
+			)}
+			{nft.mime_type?.startsWith('model') && (
+				<div
+					className={`p-2.5 ${showModel ? '' : 'opacity-80 hover:opacity-100 cursor-pointer'} absolute top-1 right-1`}
+					onClick={event => {
+						event.stopPropagation()
+						if (showModel) return
+						mixpanel.track('Load 3d model from feed')
+						setShowModel(true)
+					}}
+				>
+					<div className="flex items-center space-x-1 text-white rounded-full py-1 px-2 -my-1 -mx-1 bg-black bg-opacity-40">
+						<OrbitIcon className="w-4 h-4" />
+						<span className="font-semibold">3D</span>
+					</div>
 				</div>
 			)}
 		</div>
