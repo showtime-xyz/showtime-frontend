e3ba030b5d33f6944925a4599e8c430551fad352
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-08-13 17:02:04 +0200

    burn

--- src/components/UI/Buttons/Button.js
+++ src/components/UI/Buttons/Button.js
@@ -4,6 +4,8 @@ const Button = forwardRef(({ style, ...props }, ref) => {
 	switch (style) {
 		case 'primary':
 			return <PrimaryButton ref={ref} {...props} />
+		case 'danger':
+			return <DangerButton ref={ref} {...props} />
 
 		case 'tertiary':
 			return <TertiaryButton ref={ref} {...props} />
@@ -28,6 +30,10 @@ BaseButton.displayName = 'BaseButton'
 export const PrimaryButton = ({ className, iconOnly, ...props }) => <BaseButton {...props} className={`border border-transparent dark:border-white bg-black text-white font-medium flex items-center ${iconOnly ? 'p-2 rounded-xl' : 'px-4 py-2 rounded-2xl'} disabled:opacity-40 disabled:cursor-not-allowed ${className}`} />
 
 PrimaryButton.displayName = 'PrimaryButton'
+
+export const DangerButton = ({ className, iconOnly, ...props }) => <BaseButton {...props} className={`bg-red-500 dark:bg-red-700 text-white font-medium flex items-center ${iconOnly ? 'p-2 rounded-xl' : 'px-4 py-2 rounded-2xl'} disabled:opacity-40 disabled:cursor-not-allowed ${className}`} />
+
+DangerButton.displayName = 'DangerButton'
 
 export const TertiaryButton = ({ className, ...props }) => <BaseButton {...props} className={`relative bg-gray-100 text-gray-900 font-semibold dark:bg-gray-800 dark:text-gray-200 rounded-2xl flex items-center px-4 py-2 disabled:opacity-40 disabled:cursor-not-allowed ${className}`} />
 
