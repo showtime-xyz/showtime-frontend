c4abb92283bdc1b216ad372d46c25b8a28ea14ca
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-08-19 10:02:27 +0200

    Right-align number fields

--- src/components/UI/Modals/MintModal.jsx
+++ src/components/UI/Modals/MintModal.jsx
@@ -364,7 +364,7 @@ const OptionsPage = ({ editionCount, setEditionCount, royaltiesPercentage, setRo
 						<p className="font-semibold text-gray-900 dark:text-white">Number of Editions</p>
 						<p className="text-sm font-medium text-gray-700 dark:text-gray-300">1 by default</p>
 					</div>
-					<input type="number" min="1" max="10000" className="px-4 py-3 relative block rounded-2xl dark:text-gray-300 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 focus:outline-none focus-visible:ring" value={editionCount} onChange={event => setEditionCount(event.target.value)} />
+					<input type="number" min="1" max="10000" className="px-4 py-3 relative block rounded-2xl dark:text-gray-300 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 focus:outline-none focus-visible:ring text-right" value={editionCount} onChange={event => setEditionCount(event.target.value)} />
 				</div>
 			</div>
 			<div className="p-4 border-b border-gray-100 dark:border-gray-900">
@@ -374,7 +374,7 @@ const OptionsPage = ({ editionCount, setEditionCount, royaltiesPercentage, setRo
 						<p className="text-sm font-medium text-gray-700 dark:text-gray-300">10% by default</p>
 					</div>
 					<div className="flex items-center space-x-2">
-						<input type="number" max="69" step="10" className="px-4 max-w-[60px] py-3 relative block rounded-2xl dark:text-gray-300 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 focus:outline-none focus-visible:ring no-spinners font-medium" value={royaltiesPercentage} onChange={event => setRoyaltiesPercentage(event.target.value)} />
+						<input type="number" max="69" step="10" className="px-4 max-w-[60px] py-3 relative block rounded-2xl dark:text-gray-300 bg-gray-100 dark:bg-gray-900 border border-gray-300 dark:border-gray-700 focus:outline-none focus-visible:ring no-spinners font-medium text-right" value={royaltiesPercentage} onChange={event => setRoyaltiesPercentage(event.target.value)} />
 						<PercentageIcon className="w-4 h-4 text-gray-700 dark:text-gray-500" />
 					</div>
 				</div>
