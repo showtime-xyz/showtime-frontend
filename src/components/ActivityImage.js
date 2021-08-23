68e9b6e8cc3b17beb51760c012aa7dd3aaf7165b
Alex <alex.kilkka@gmail.com>
2021-08-22 17:30:11 -0700

    Utilize preview videos & source_url on news feed

--- src/components/ActivityImage.js
+++ src/components/ActivityImage.js
@@ -82,35 +82,76 @@ export default function ActivityImage({ nft, index, numberOfImages, openModal, s
 			onMouseEnter={() => setIsHovering(true)}
 			onMouseLeave={() => setIsHovering(false)}
 		>
-			{nft.mime_type?.startsWith('model') && showModel && <model-viewer src={nft.source_url} class="object-cover w-full h-full" autoplay camera-controls auto-rotate ar ar-modes="scene-viewer quick-look" interaction-prompt="none" onClick={event => event.stopPropagation()} />}
-			{nft.token_img_url && !(((nft.token_has_video && !nft.mime_type?.startsWith('model')) || (nft.token_animation_url && !nft.token_img_url)) && numberOfImages === 1) && <img src={numberOfImages === 1 ? getImageUrlLarge(nft.token_img_url, nft.token_aspect_ratio) : getImageUrl(nft.token_img_url, nft.token_aspect_ratio)} className="object-cover w-full h-full" />}
-			{!nft.mime_type?.startsWith('model') && (nft.token_has_video || (nft.token_animation_url && !nft.token_img_url)) && (!nft.token_img_url || numberOfImages === 1) && (
-				<ReactPlayer
-					url={nft?.token_animation_url}
-					playing={true}
-					loop
-					muted={true}
-					width={imgWidth > window.innerWidth ? window.innerWidth : imgWidth > cardWidth ? cardWidth : imgWidth}
-					height={imgWidth}
-					style={{
-						maxWidth: imgWidth > window.innerWidth ? window.innerWidth : imgWidth > cardWidth ? cardWidth : imgWidth,
-						justifyContent: 'center',
-						alignItems: 'center',
-						marginLeft: 'auto',
-						marginRight: 'auto',
-					}}
-					playsinline
-					// Disable downloading & right click
-					config={{
-						file: {
-							attributes: {
-								onContextMenu: e => e.preventDefault(),
-								controlsList: 'nodownload',
-							},
-						},
-					}}
-				/>
+			{nft.mime_type && (
+				<>
+					{nft.mime_type?.startsWith('model') && showModel && <model-viewer src={nft.source_url} class="object-cover w-full h-full" autoplay camera-controls auto-rotate ar ar-modes="scene-viewer quick-look" interaction-prompt="none" onClick={event => event.stopPropagation()} />}
+					{nft.mime_type?.startsWith('model') && !showModel && <img src={numberOfImages === 1 ? getImageUrlLarge(nft.still_preview_url ? nft.still_preview_url : nft.token_img_url, nft.token_aspect_ratio) : getImageUrl(nft.still_preview_url ? nft.still_preview_url : nft.token_img_url, nft.token_aspect_ratio)} className="object-cover w-full h-full" />}
+					{nft.mime_type?.startsWith('image') && <img src={numberOfImages === 1 ? getImageUrlLarge(nft.still_preview_url ? nft.still_preview_url : nft.token_img_url, nft.token_aspect_ratio) : getImageUrl(nft.still_preview_url ? nft.still_preview_url : nft.token_img_url, nft.token_aspect_ratio)} className="object-cover w-full h-full" />}
+					{nft.mime_type?.startsWith('video') && (
+						<ReactPlayer
+							url={nft.animation_preview_url ? nft.animation_preview_url : nft?.source_url ? nft?.source_url : nft?.token_animation_url}
+							playing={true}
+							loop
+							muted={true}
+							width={imgWidth > window.innerWidth ? window.innerWidth : imgWidth > cardWidth ? cardWidth : imgWidth}
+							height={imgWidth}
+							style={{
+								maxWidth: imgWidth > window.innerWidth ? window.innerWidth : imgWidth > cardWidth ? cardWidth : imgWidth,
+								justifyContent: 'center',
+								alignItems: 'center',
+								marginLeft: 'auto',
+								marginRight: 'auto',
+							}}
+							playsinline
+							// Disable downloading & right click
+							config={{
+								file: {
+									attributes: {
+										onContextMenu: e => e.preventDefault(),
+										controlsList: 'nodownload',
+										style: { objectFit: 'cover', width: '100%', height: '100%' },
+									},
+								},
+							}}
+						/>
+					)}
+				</>
 			)}
+
+			{!nft.mime_type && (
+				<>
+					{nft.token_img_url && !((nft.token_has_video && !nft.mime_type?.startsWith('model')) || (nft.token_animation_url && !nft.token_img_url)) && <img src={numberOfImages === 1 ? getImageUrlLarge(nft.token_img_url, nft.token_aspect_ratio) : getImageUrl(nft.token_img_url, nft.token_aspect_ratio)} className="object-cover w-full h-full" />}
+
+					{!nft.mime_type?.startsWith('model') && (nft.token_has_video || (nft.token_animation_url && !nft.token_img_url) || nft.animation_preview_url || nft.mime_type?.startsWith('video')) && (
+						<ReactPlayer
+							url={nft.animation_preview_url ? nft.animation_preview_url : nft?.source_url ? nft?.source_url : nft?.token_animation_url}
+							playing={true}
+							loop
+							muted={true}
+							width={imgWidth > window.innerWidth ? window.innerWidth : imgWidth > cardWidth ? cardWidth : imgWidth}
+							height={imgWidth}
+							style={{
+								maxWidth: imgWidth > window.innerWidth ? window.innerWidth : imgWidth > cardWidth ? cardWidth : imgWidth,
+								justifyContent: 'center',
+								alignItems: 'center',
+								marginLeft: 'auto',
+								marginRight: 'auto',
+							}}
+							playsinline
+							// Disable downloading & right click
+							config={{
+								file: {
+									attributes: {
+										onContextMenu: e => e.preventDefault(),
+										controlsList: 'nodownload',
+									},
+								},
+							}}
+						/>
+					)}
+				</>
+			)}
+
 			{totalNumberOfImages > 1 && !isMobile && (
 				<div className={`absolute flex bottom-1 right-0 py-1 px-2 bg-white dark:bg-black bg-opacity-95 dark:bg-opacity-50 backdrop-filter backdrop-blur-lg backdrop-saturate-150 shadow-md rounded-xl transform scale-90 ${isHovering ? 'visible opacity-100 translate-y-0' : 'invisible opacity-0 translate-y-1'} transition-all`} onClick={e => e.stopPropagation()}>
 					<LikeButton item={nft} />
