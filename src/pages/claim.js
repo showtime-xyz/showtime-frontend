02bef4929d1540b7c3265f39d75269ba5ac0b57d
Alex <alex.kilkka@gmail.com>
2021-08-26 06:34:48 -0700

    Add claim redirect

---
+++ src/pages/claim.js
@@ -0,0 +1,22 @@
+/*
+export async function getServerSideProps({ query: { slug } }) {
+	return {
+		redirect: {
+			destination: `/${slug}`,
+			permanent: true,
+		},
+	}
+}
+
+const RedirectURL = () => null
+
+export default RedirectURL
+*/
+
+import { useEffect } from 'react'
+export default function redirect() {
+	useEffect(() => {
+		window.location.assign('https://showtime-airdrop.vercel.app/')
+	})
+	return <></>
+}
