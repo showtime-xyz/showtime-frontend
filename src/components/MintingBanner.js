02f1a33e105f1cf2ab0dfa4308d12627599b228e
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-10-04 12:49:41 +0100

    Update alert font size

--- src/components/MintingBanner.js
+++ src/components/MintingBanner.js
@@ -7,7 +7,7 @@ const MintingBanner = ({ openMintModal }) => {
 
 	return (
 		<Transition as="span" show={show} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-			<div className="relative bg-gray-100 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800">
+			<div className="relative bg-gray-100 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800 text-sm">
 				<div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
 					<div className="pr-16 sm:text-center sm:px-16">
 						<p className="font-medium text-gray-900 dark:text-gray-100">
