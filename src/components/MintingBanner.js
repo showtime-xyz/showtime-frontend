020488169a6b5d35f2f1624f733baec9b0e7980c
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-10-04 11:19:17 +0100

    Update banner styles

--- src/components/MintingBanner.js
+++ src/components/MintingBanner.js
@@ -7,7 +7,7 @@ const MintingBanner = ({ openMintModal }) => {
 
 	return (
 		<Transition as="span" show={show} enter="ease-out duration-300" enterFrom="opacity-0" enterTo="opacity-100" leave="ease-in duration-200" leaveFrom="opacity-100" leaveTo="opacity-0">
-			<div className="relative bg-gray-100 dark:bg-gray-900">
+			<div className="relative bg-gray-100 dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800">
 				<div className="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
 					<div className="pr-16 sm:text-center sm:px-16">
 						<p className="font-medium text-gray-900 dark:text-gray-100">
@@ -15,20 +15,20 @@ const MintingBanner = ({ openMintModal }) => {
 								We're rolling out free minting! <span aria-hidden="true">&rarr;</span>
 							</a>
 							<span className="hidden md:inline">
-								You can now create NFTs for free (no gas costs)!{' '}
-								<button className="text-gray-900 dark:text-gray-100 font-bold underline" onClick={openMintModal}>
+								<span className="text-xs mr-1 -mt-1 inline-block">🎉</span> You can now create NFTs for free (no gas costs)!{' '}
+								<button className="text-gray-900 dark:text-gray-100 font-bold" onClick={openMintModal}>
 									Get started
 								</button>{' '}
 								and{' '}
-								<a className="text-gray-900 dark:text-gray-100 font-bold underline" href="/claim" target="_blank">
+								<a className="text-gray-900 dark:text-gray-100 font-bold" href="/claim" target="_blank">
 									claim our genesis drop NFT
 								</a>{' '}
-								to celebrate. 🎉
+								to celebrate.
 							</span>
 						</p>
 					</div>
-					<div className="absolute inset-y-0 right-0 pt-1 pr-1 flex items-start sm:pt-1 sm:pr-2 sm:items-start">
-						<button onClick={() => setShow(false)} type="button" className="flex p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-800 focus:outline-none">
+					<div className="absolute inset-y-0 right-2 flex items-center">
+						<button onClick={() => setShow(false)} type="button" className="flex p-1 rounded-full hover:bg-gray-200 dark:hover:bg-gray-800 focus:outline-none transition">
 							<span className="sr-only">Dismiss</span>
 							<XIcon className="h-6 w-6 text-gray-900 dark:text-gray-100" aria-hidden="true" />
 						</button>
