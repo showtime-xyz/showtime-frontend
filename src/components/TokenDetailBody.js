58e48985a9dc3eb476754157c09081e09001c246
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-10-05 16:31:18 +0100

    eslint

--- src/components/TokenDetailBody.js
+++ src/components/TokenDetailBody.js
@@ -28,9 +28,7 @@ import { CHAIN_IDENTIFIERS } from '@/lib/constants'
 import PolygonIcon from './Icons/PolygonIcon'
 import Tippy from '@tippyjs/react'
 import TezosIcon from './Icons/TezosIcon'
-import Image from 'next/image'
 import ShowtimeIcon from './Icons/ShowtimeIcon'
-import showtimeLogo from '@/../public/img/logo_sm.png'
 
 // how tall the media will be
 const TOKEN_MEDIA_HEIGHT = 500
