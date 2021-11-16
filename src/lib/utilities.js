7a82a8d746f920dde85bf9199bc3166d1490d8b3
Alex <alex.kilkka@gmail.com>
2021-11-16 07:07:47 -0800

    Update HEN links to community-maintained fork

--- src/lib/utilities.js
+++ src/lib/utilities.js
@@ -85,7 +85,7 @@ export const getBidLink = item => {
 			}
 
 		case CONTRACTS.HICETNUNC:
-			return `https://www.hicetnunc.xyz/objkt/${item.token_id}`
+			return `https://www.hicetnunc.art/objkt/${item.token_id}`
 		default:
 			return `https://opensea.io/assets/${item.chain_identifier == 137 ? 'matic/' : ''}${item.contract_address}/${item.token_id}?ref=0xe3fac288a27fbdf947c234f39d6e45fb12807192`
 	}
