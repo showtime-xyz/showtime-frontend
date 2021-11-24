60b7f10620aa7db103c4980847937b852e926b7b
Nishan <nishanbende@gmail.com>
2021-11-24 18:49:25 +0530

    fix: fonts warning in storybook RN

--- apps/storybook-react-native/App.tsx
+++ apps/storybook-react-native/App.tsx
@@ -1,2 +1,25 @@
-import StorybookUIRoot from './.storybook/Storybook';
-export { StorybookUIRoot as default };
+import StorybookUIRoot from './.storybook/Storybook'
+import { useFonts } from 'expo-font'
+
+export default function App() {
+	const [fontsLoaded, error] = useFonts({
+		'Inter-Black': require('./assets/fonts/Inter-Black.otf'),
+		'Inter-Bold': require('./assets/fonts/Inter-Bold.otf'),
+		'Inter-ExtraBold': require('./assets/fonts/Inter-ExtraBold.otf'),
+		'Inter-ExtraLight': require('./assets/fonts/Inter-ExtraLight.otf'),
+		'Inter-Light': require('./assets/fonts/Inter-Light.otf'),
+		'Inter-Medium': require('./assets/fonts/Inter-Medium.otf'),
+		Inter: require('./assets/fonts/Inter-Regular.otf'),
+		'Inter-Regular': require('./assets/fonts/Inter-Regular.otf'),
+		'Inter-SemiBold': require('./assets/fonts/Inter-SemiBold.otf'),
+		'Inter-Thin': require('./assets/fonts/Inter-Thin.otf'),
+	})
+
+	if (!fontsLoaded) return null
+
+	if (error) {
+		console.error(error)
+	}
+
+	return <StorybookUIRoot />
+}
