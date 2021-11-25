70ebcbe82b955d20938c0e839fa0fc2cab836393
Axel Delafosse <axeldelafosse@gmail.com>
2021-11-25 19:41:15 +0100

    use chromatic for storybook react

--- apps/storybook-react/pages/index.tsx
+++ apps/storybook-react/pages/index.tsx
@@ -21,31 +21,6 @@ const Home: NextPage = () => {
 				<p className={styles.description}>
 					Get started by editing <code className={styles.code}>pages/index.tsx</code>
 				</p>
-
-				<div className={styles.grid}>
-					<a href="https://nextjs.org/docs" className={styles.card}>
-						<h2>Documentation &rarr;</h2>
-						<p>Find in-depth information about Next.js features and API.</p>
-					</a>
-
-					<a href="https://nextjs.org/learn" className={styles.card}>
-						<h2>Learn &rarr;</h2>
-						<p>Learn about Next.js in an interactive course with quizzes!</p>
-					</a>
-
-					<a href="https://github.com/vercel/next.js/tree/master/examples" className={styles.card}>
-						<h2>Examples &rarr;</h2>
-						<p>Discover and deploy boilerplate example Next.js projects.</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-					</a>
-				</div>
 			</main>
 
 			<footer className={styles.footer}>
