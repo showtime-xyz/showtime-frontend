4039a4da07260e3a15d11e28b93e2c18251a2abd
Miguel Piedrafita <github@miguelpiedrafita.com>
2021-12-10 15:50:23 -0800

    Update ABI

--- apps/next-react/src/pages/api/marketplace/permit.js
+++ apps/next-react/src/pages/api/marketplace/permit.js
@@ -1,5 +1,6 @@
 import handler, { middleware } from '@/lib/api-handler'
 import ierc20PermitAbi from '@/data/IERC20Permit.json'
+import ierc20MetaTxAbi from '@/data/IERC20MetaTx.json'
 import { ethers } from 'ethers'
 import { SOL_MAX_INT } from '@/lib/constants'
 
@@ -54,7 +55,7 @@ const submitErc20Permit = (wallet, permit) => {
 
 const executeMetaTx = (wallet, metatx) => {
 	const { r, s, v } = ethers.utils.splitSignature(metatx.signature)
-	const tokenContract = new ethers.Contract(metatx.tokenAddr, ierc20PermitAbi, wallet)
+	const tokenContract = new ethers.Contract(metatx.tokenAddr, ierc20MetaTxAbi, wallet)
 
 	try {
 		return tokenContract.executeMetaTransaction(metatx.owner, metatx.fnSig, r, s, v)
