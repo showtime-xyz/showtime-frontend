4a2bfcdd4180a382c7ce57bbdf86c2a6157d9fc4
Axel Delafosse <axeldelafosse@gmail.com>
2021-12-14 12:00:03 +0100

    use custom dev client for storybook expo app

--- apps/storybook-react-native/app.config.js
+++ apps/storybook-react-native/app.config.js
@@ -18,15 +18,24 @@ export default {
 	},
 	assetBundlePatterns: ['**/*'],
 	ios: {
+		bundleIdentifier: 'io.showtime.storybook',
+		buildNumber: '1.0.0',
 		supportsTablet: true,
 		jsEngine: 'hermes',
+		backgroundColor: '#FFFFFF',
 	},
 	android: {
+		package: 'io.showtime.storybook',
+		versionCode: 1,
 		adaptiveIcon: {
 			foregroundImage: './assets/adaptive-icon.png',
 			backgroundImage: './assets/adaptive-icon-background.storybook.png',
 		},
 		jsEngine: 'hermes',
+	},
+	androidNavigationBar: {
+		barStyle: 'dark-content',
+		backgroundColor: '#FFFFFF',
 	},
 	web: {
 		favicon: './assets/favicon.png',
