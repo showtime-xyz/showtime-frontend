056ea2502d8c2da44a86fb4b9c774f45bff1fc8f
Axel Delafosse <axeldelafosse@gmail.com>
2022-01-06 12:07:49 +0100

    create video component

--- packages/design-system/card/media.tsx
+++ packages/design-system/card/media.tsx
@@ -1,11 +1,12 @@
 import { useCallback } from "react";
-import { FlatList, useWindowDimensions } from "react-native";
-import { Video } from "expo-av";
+import { Pressable, FlatList, useWindowDimensions } from "react-native";
 import Router from "next/router";
 
-import { View, Image } from "design-system";
-import { Pressable } from "react-native";
 import { useRouter } from "app/navigation/use-router";
+import { mixpanel } from "app/lib/mixpanel";
+
+import { View, Image } from "design-system";
+import { Video } from "design-system/video";
 
 const getImageUrl = (imgUrl: string, tokenAspectRatio: number) => {
   if (imgUrl && imgUrl.includes("https://lh3.googleusercontent.com")) {
@@ -59,51 +60,66 @@ function Media({ nfts }: Props) {
     ({ item }) => {
       if (!item) return null;
 
+      // TODO: how to handle `image/svg+xml`?
       return (
-        <Pressable onPress={() => openNFT(item.nft_id?.toString())}>
-          {item.mime_type?.startsWith("image") && (
-            <Image
-              source={{
-                uri:
-                  count === 1
-                    ? getImageUrlLarge(
-                        item.still_preview_url
-                          ? item.still_preview_url
-                          : item.token_img_url,
-                        item.token_aspect_ratio
-                      )
-                    : getImageUrl(
-                        item.still_preview_url
-                          ? item.still_preview_url
-                          : item.token_img_url,
-                        item.token_aspect_ratio
-                      ),
-              }}
-              tw={count > 1 ? "w-[50vw] h-[50vw]" : "w-[100vw] h-[100vw]"}
-              blurhash={item.blurhash}
-              resizeMode="cover"
-            />
-          )}
+        <View
+          tw={[
+            count >= 2 ? "m-[2px]" : "",
+            item.token_background_color
+              ? `bg-[#${item.token_background_color}]`
+              : "bg-black",
+          ]}
+        >
+          <Pressable
+            onPress={() => {
+              openNFT(item.nft_id?.toString());
+              mixpanel.track("Activity - Click on NFT image, open modal");
+            }}
+          >
+            {(item.mime_type?.startsWith("image") ||
+              item.mime_type?.startsWith("model")) && (
+              <Image
+                source={{
+                  uri:
+                    count === 1
+                      ? getImageUrlLarge(
+                          item.still_preview_url
+                            ? item.still_preview_url
+                            : item.token_img_url,
+                          item.token_aspect_ratio
+                        )
+                      : getImageUrl(
+                          item.still_preview_url
+                            ? item.still_preview_url
+                            : item.token_img_url,
+                          item.token_aspect_ratio
+                        ),
+                }}
+                tw={count > 1 ? "w-[50vw] h-[50vw]" : "w-[100vw] h-[100vw]"}
+                blurhash={item.blurhash}
+                resizeMode="cover"
+              />
+            )}
 
-          {item.mime_type?.startsWith("video") && (
-            <Video
-              // ref={video}
-              // style={styles.video}
-              source={{
-                uri: item.animation_preview_url
-                  ? item.animation_preview_url
-                  : item?.source_url
-                  ? item?.source_url
-                  : item?.token_animation_url,
-              }}
-              useNativeControls
-              resizeMode="contain"
-              isLooping
-              isMuted
-              // onPlaybackStatusUpdate={status => setStatus(() => status)}
-            />
-          )}
-        </Pressable>
+            {item.mime_type?.startsWith("video") && (
+              <Video
+                source={{
+                  uri: item.animation_preview_url
+                    ? item.animation_preview_url
+                    : item?.source_url
+                    ? item?.source_url
+                    : item?.token_animation_url,
+                }}
+                posterSource={{
+                  uri: item.still_preview_url,
+                }}
+                tw={count > 1 ? "w-[50vw] h-[50vw]" : "w-[100vw] h-[100vw]"}
+                resizeMode="cover"
+                useNativeControls={count === 1}
+              />
+            )}
+          </Pressable>
+        </View>
       );
     },
     [count]
@@ -122,20 +138,18 @@ function Media({ nfts }: Props) {
     [width]
   );
 
-  const ItemSeparatorComponent = useCallback(() => <View />, []);
-
   if (count === 1) {
     return renderItem({ item: nfts[0] });
   }
 
   return (
     <FlatList
+      style={count >= 2 ? { marginHorizontal: -2 } : {}}
       data={nfts}
       keyExtractor={keyExtractor}
       numColumns={2}
       renderItem={renderItem}
       getItemLayout={getItemLayout}
-      ItemSeparatorComponent={ItemSeparatorComponent}
       initialNumToRender={count}
       // @ts-ignore
       listKey={keyExtractor}
