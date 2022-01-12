0f4f65b101668b9c1c46b4bc19288992e251413a
Nishan <nishanbende@gmail.com>
2022-01-12 12:02:35 +0530

    add react navigation to storybook RN

--- apps/storybook-react-native/App.tsx
+++ apps/storybook-react-native/App.tsx
@@ -1,5 +1,25 @@
-import { LogBox } from "react-native";
-import StorybookUIRoot from "./.storybook/Storybook";
-export { StorybookUIRoot as default };
+import "../expo/shim";
 
-LogBox.ignoreLogs(["You tried to add a duplicate"]);
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import StorybookUIRoot from "./.storybook/Storybook";
+import { enableScreens } from "react-native-screens";
+import { linking } from "app/navigation/linking";
+
+enableScreens(true);
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{ headerShown: false }}
+      >
+        <Stack.Screen name="Storybook" component={StorybookUIRoot} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
