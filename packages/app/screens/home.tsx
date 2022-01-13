26cc711ddfd8c9636e88314b22ee457af027ebe1
Nishan <nishanbende@gmail.com>
2022-01-14 05:17:12 +0530

    fix: color mode re-renders

--- packages/app/screens/home.tsx
+++ packages/app/screens/home.tsx
@@ -2,13 +2,13 @@ import { useEffect } from "react";
 
 import { mixpanel } from "app/lib/mixpanel";
 import { Feed } from "app/components/feed";
+import { withColorScheme } from "../components/memoWithTheme";
 
-const HomeScreen = () => {
+const HomeScreen = withColorScheme(() => {
   useEffect(() => {
     mixpanel.track("Home page view");
   }, []);
-
   return <Feed />;
-};
+});
 
 export { HomeScreen };
