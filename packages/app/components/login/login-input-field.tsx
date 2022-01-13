67ef45da76e771c81c2f6be0640703620fc141c7
gorhom <gorhom.dev@gmail.com>
2022-01-13 21:48:26 +0000

    refactor: added a generic input field

--- packages/app/components/login/login-input-field.tsx
+++ packages/app/components/login/login-input-field.tsx
@@ -3,27 +3,30 @@ import { Button, ButtonLabel, Text, TextInput, View } from "design-system";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { yup } from "app/lib/yup";
-
-const validationSchema = yup
-  .object({
-    // @ts-ignore
-    contact: yup.string().or([yup.string().email(), yup.string().phone()]),
-  })
-  .required();
+import { TextInputProps } from "react-native";
 
 type FormData = {
-  contact?: string;
+  data?: string;
 };
 
-interface LoginEmailFieldProps {
-  onSubmitEmail: (value: string) => void;
-  onSubmitPhoneNumber: (value: string) => void;
+interface LoginInputFieldProps extends Pick<TextInputProps, "keyboardType"> {
+  label?: string;
+  placeholder?: string;
+  signInButtonLabel?: string;
+  validationSchema: yup.AnyObjectSchema;
+  // eslint-disable-next-line no-unused-vars
+  onSubmit: (value: string) => void;
 }
 
-export function LoginContactDetailsField({
-  onSubmitEmail,
-  onSubmitPhoneNumber,
-}: LoginEmailFieldProps) {
+export function LoginInputField({
+  label = "Input Field",
+  placeholder = "Enter here",
+  signInButtonLabel = "Sign in",
+  keyboardType = "default",
+  validationSchema,
+  onSubmit,
+}: LoginInputFieldProps) {
+  //#region hooks
   const {
     control,
     handleSubmit,
@@ -33,23 +36,21 @@ export function LoginContactDetailsField({
     mode: "onBlur",
     reValidateMode: "onChange",
   });
+  //#endregion
 
-  const handleSubmitContact = useCallback(
-    ({ contact }: FormData) => {
-      console.log("contact", contact);
-      if (contact.includes("@")) {
-        onSubmitEmail(contact);
-      } else {
-        onSubmitPhoneNumber(contact);
-      }
+  //#region callbacks
+  const handleSubmitData = useCallback(
+    ({ data }: FormData) => {
+      onSubmit(data ?? "");
     },
-    [onSubmitEmail, onSubmitPhoneNumber]
+    [onSubmit]
   );
+  //#endregion
   return (
     <>
       <View tw="p-[16px] mb-[16px] rounded-[16px] bg-gray-100 dark:bg-gray-900">
         <Text tw="mb-[8px] font-bold text-sm text-gray-900 dark:text-white">
-          Contact details
+          {label}
         </Text>
         <Controller
           control={control}
@@ -59,28 +60,31 @@ export function LoginContactDetailsField({
               onBlur={onBlur}
               onChangeText={(value) => onChange(value)}
               value={value}
-              placeholder="Enter your email or phone number"
+              placeholder={placeholder}
               autoCapitalize="none"
               autoCorrect={false}
+              keyboardType={keyboardType}
               returnKeyType="go"
             />
           )}
-          name="contact"
+          name="data"
         />
-        {errors.contact && (
+        {errors.data && (
           <Text tw="text-xs text-red-500 font-semibold mt-[8px]">
-            Please enter a valid email address or phone number.
+            {errors.data.message}
           </Text>
         )}
       </View>
 
       <Button
-        onPress={handleSubmit(handleSubmitContact)}
+        onPress={handleSubmit(handleSubmitData)}
         variant="tertiary"
         size="regular"
         tw="mb-[16px]"
       >
-        <ButtonLabel tw="text-black dark:text-white">Sign in</ButtonLabel>
+        <ButtonLabel tw="text-black dark:text-white">
+          {signInButtonLabel}
+        </ButtonLabel>
       </Button>
     </>
   );
