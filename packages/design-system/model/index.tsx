005030cdf9a7337303cdc9ea72a7b6f29b80978d
Axel Delafosse <axeldelafosse@gmail.com>
2022-01-19 13:25:05 +0100

    install and use react-native-blob-jsi-helper

--- packages/design-system/model/index.tsx
+++ packages/design-system/model/index.tsx
@@ -4,6 +4,8 @@ import { Canvas, useFrame } from "./react-three-fiber";
 import { useGLTF } from "./use-gltf";
 import { Image } from "design-system/image";
 
+// import iphoneModelPath from "./iphone.glb";
+
 type Props = {
   url: string;
   fallbackUrl: string;
@@ -13,7 +15,7 @@ type Props = {
 
 function Model({ url }: { url: string }) {
   const { scene } = useGLTF(url);
-  // useFrame(() => (scene.rotation.y += 0.01));
+  useFrame(() => (scene.rotation.y += 0.01));
 
   return <primitive object={scene} />;
 }
@@ -28,21 +30,30 @@ function Model({ url }: { url: string }) {
 // onPointerMove	called when press moves	onPressMove
 
 function ModelViewer({ url, fallbackUrl, blurhash, count }: Props) {
-  // return (
-  //   <Image
-  //     source={{
-  //       uri: fallbackUrl,
-  //     }}
-  //     tw={count > 1 ? "w-[50vw] h-[50vw]" : "w-[100vw] h-[100vw]"}
-  //     blurhash={blurhash}
-  //     resizeMode="cover"
-  //   />
-  // );
+  if (url.endsWith(".gltf")) {
+    return (
+      <Image
+        source={{
+          uri: fallbackUrl,
+        }}
+        tw={count > 1 ? "w-[50vw] h-[50vw]" : "w-[100vw] h-[100vw]"}
+        blurhash={blurhash}
+        resizeMode="cover"
+      />
+    );
+  }
+
+  console.log(url);
 
   return (
     <Canvas>
+      <color attach="background" args={[0xe2f4df]} />
+      <ambientLight />
+      <directionalLight intensity={1.1} position={[0.5, 0, 0.866]} />
+      <directionalLight intensity={0.8} position={[-6, 2, 2]} />
       <Suspense fallback={null}>
         <Model url={url} />
+        {/* <Model url={iphoneModelPath} /> */}
       </Suspense>
     </Canvas>
   );
