3d14318041f23d3864da46ab548e813691b80775
Nishan <nishanbende@gmail.com>
2022-01-21 17:04:48 +0530

    add faster base64 shims

--- apps/expo/index.js
+++ apps/expo/index.js
@@ -1,12 +1,9 @@
-import 'expo-dev-client';
+import "expo-dev-client";
 import "expo-dev-launcher";
 import "expo/build/Expo.fx";
 import { activateKeepAwake } from "expo-keep-awake";
 import { registerRootComponent } from "expo";
-import "react-native-get-random-values";
 // Import the required shims
-import "@ethersproject/shims";
-
 import "./shim";
 import App from "./App";
 
--- packages/app/components/create.tsx
+++ packages/app/components/create.tsx
@@ -75,8 +75,8 @@ function Create({ uri }: { uri: string }) {
   return (
     <View tw="flex-1">
       <ScrollView contentContainerStyle={{ paddingBottom: tabBarHeight + 100 }}>
-        <View tw="p-4">
-          <View tw="mt-11 flex-row">
+        <View tw="px-3 py-4">
+          <View tw="flex-row">
             <Preview
               source={{
                 uri,
