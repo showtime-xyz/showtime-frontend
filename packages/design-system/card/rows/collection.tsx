27a9ada3f59ec124c9f9cc55738b38ebefd6c576
Axel Delafosse <axeldelafosse@gmail.com>
2022-01-22 14:36:52 +0100

    remove link from collection

--- packages/design-system/card/rows/collection.tsx
+++ packages/design-system/card/rows/collection.tsx
@@ -12,29 +12,29 @@ function Collection({ nft }: Props) {
   if (!nft) return null;
 
   return (
-    <Link href={`/c/${nft.collection_slug}`}>
-      <View tw="px-4 py-2 flex-row items-center justify-between">
-        <View tw="flex-row items-center">
-          {nft.collection_img_url && (
-            <Image
-              source={{ uri: nft.collection_img_url }}
-              tw="w-5 h-5 rounded-full"
-            />
-          )}
-          <Text
-            tw="ml-2 text-xs font-bold text-gray-600 dark:text-gray-400 w-[65vw]"
-            numberOfLines={1}
-          >
-            {nft.collection_name}
-          </Text>
-        </View>
-        <Text tw="text-xs font-bold text-gray-600 dark:text-gray-400">
-          {!nft.token_count || nft.token_count === 1
-            ? "1 Edition"
-            : `${nft.token_count} Editions`}
+    // <Link href={`/c/${nft.collection_slug}`}>
+    <View tw="px-4 py-2 flex-row items-center justify-between">
+      <View tw="flex-row items-center">
+        {nft.collection_img_url && (
+          <Image
+            source={{ uri: nft.collection_img_url }}
+            tw="w-5 h-5 rounded-full"
+          />
+        )}
+        <Text
+          tw="ml-2 text-xs font-bold text-gray-600 dark:text-gray-400 w-[65vw]"
+          numberOfLines={1}
+        >
+          {nft.collection_name}
         </Text>
       </View>
-    </Link>
+      <Text tw="text-xs font-bold text-gray-600 dark:text-gray-400">
+        {!nft.token_count || nft.token_count === 1
+          ? "1 Edition"
+          : `${nft.token_count} Editions`}
+      </Text>
+    </View>
+    // </Link>
   );
 }
 
