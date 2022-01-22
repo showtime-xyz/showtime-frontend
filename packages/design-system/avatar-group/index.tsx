9137c7c49fa3d7986f79d37b66cbe5c08c75db35
gorhom <gorhom.dev@gmail.com>
2022-01-22 09:06:58 +0000

    feat: added avatar group component

---
+++ packages/design-system/avatar-group/index.tsx
@@ -0,0 +1,45 @@
+import { View } from "design-system/view";
+import { Image } from "design-system/image";
+import { DEFAULT_PROFILE_PIC } from "app/lib/constants";
+import { useIsDarkMode } from "../hooks";
+
+const getProfileImageUrl = (imgUrl: string) => {
+  if (imgUrl && imgUrl.includes("https://lh3.googleusercontent.com")) {
+    imgUrl = imgUrl.split("=")[0] + "=s112";
+  }
+  return imgUrl;
+};
+
+interface Profile {
+  profile_id: number;
+  img_url: string;
+}
+
+interface AvatarGroupProps {
+  count: number;
+  profiles?: Profile[];
+}
+
+export function AvatarGroup({ count, profiles }: AvatarGroupProps) {
+  //#region hooks
+  const isDarkMode = useIsDarkMode();
+  //#endregion
+
+  return (
+    <View tw={["flex-row min-h-[28px]", `w-[${count * 20}px]`]}>
+      {profiles?.slice(0, 3).map((profile, index) => (
+        <Image
+          key={`avatar-${profile.profile_id}`}
+          tw={[
+            "w-[28px] h-[28px] rounded-full border-2 ml-[-2px]",
+            `left-[${-(index * 8)}px]`,
+            isDarkMode ? "border-black" : "border-white",
+          ]}
+          source={{
+            uri: getProfileImageUrl(profile.img_url ?? DEFAULT_PROFILE_PIC),
+          }}
+        />
+      ))}
+    </View>
+  );
+}
