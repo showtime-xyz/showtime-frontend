2022d90915cc9291318f1735d34ca08600ddcfea
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-01-27 02:43:19 +0100

    Ensure we don't pick a Tezos address when minting

--- packages/app/hooks/use-mint-nft.ts
+++ packages/app/hooks/use-mint-nft.ts
@@ -129,10 +129,14 @@ export const useMintNFT = () => {
   useEffect(() => {
     if (
       user?.data &&
-      user?.data.profile.wallet_addresses_excluding_email_v2[0]
+      user?.data.profile.wallet_addresses_excluding_email_v2.filter((addr) =>
+        addr.address.startsWith("0x")
+      )[0]
     ) {
       setUserAddress(
-        user.data.profile.wallet_addresses_excluding_email_v2[0].address
+        user.data.profile.wallet_addresses_excluding_email_v2.filter((addr) =>
+          addr.address.startsWith("0x")
+        )[0].address
       );
     }
     // Web3 is initialised for magic users
