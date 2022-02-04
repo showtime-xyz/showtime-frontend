4cc2f7973fa78ab8efa550a753b5f1bf9d1c2b28
Axel Delafosse <axeldelafosse@gmail.com>
2022-02-04 09:51:50 +0100

    update feed

--- packages/app/components/feed.tsx
+++ packages/app/components/feed.tsx
@@ -1,14 +1,10 @@
-import { Suspense, useCallback, useState } from "react";
-import { Platform } from "react-native";
+import { Suspense, useCallback } from "react";
+import { FlatList, Platform } from "react-native";
 import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
 import { useActivity } from "app/hooks/api-hooks";
 
 import { View, Spinner, Text } from "design-system";
 import { Card } from "design-system/card";
-import { Tabs, TabItem, SelectedTabIndicator } from "design-system/tabs";
-import { tw } from "design-system/tailwind";
-
-const TAB_LIST_HEIGHT = 64;
 
 const Footer = ({ isLoading }: { isLoading: boolean }) => {
   const tabBarHeight = useBottomTabBarHeight();
@@ -28,249 +24,12 @@ const Footer = ({ isLoading }: { isLoading: boolean }) => {
 };
 
 const Feed = () => {
-  const [selected, setSelected] = useState(0);
-
   return (
     <View tw="bg-white dark:bg-black flex-1">
-      <Tabs.Root
-        onIndexChange={setSelected}
-        initialIndex={selected}
-        tabListHeight={TAB_LIST_HEIGHT}
-        lazy
-      >
-        <Tabs.Header>
-          <View tw="bg-white dark:bg-black pt-4 pl-4 pb-[3px]">
-            <Text
-              variant="text-2xl"
-              tw="text-gray-900 dark:text-white font-extrabold"
-            >
-              Home
-            </Text>
-          </View>
-        </Tabs.Header>
-        <Tabs.List
-          style={[
-            {
-              height: TAB_LIST_HEIGHT,
-              ...tw.style(
-                "dark:bg-black bg-white border-b border-b-gray-100 dark:border-b-gray-900"
-              ),
-            },
-          ]}
-        >
-          <Tabs.Trigger>
-            <TabItem name="All Activity" selected={selected === 0} />
-          </Tabs.Trigger>
-
-          <Tabs.Trigger>
-            <TabItem name="Creations" selected={selected === 1} />
-          </Tabs.Trigger>
-
-          <Tabs.Trigger>
-            <TabItem name="Likes" selected={selected === 2} />
-          </Tabs.Trigger>
-
-          <Tabs.Trigger>
-            <TabItem name="Comments" selected={selected === 3} />
-          </Tabs.Trigger>
-
-          <Tabs.Trigger>
-            <TabItem name="Follows" selected={selected === 4} />
-          </Tabs.Trigger>
-
-          <SelectedTabIndicator />
-        </Tabs.List>
-        <Tabs.Pager>
-          <Suspense fallback={<Spinner size="small" />}>
-            <AllActivityList />
-          </Suspense>
-          <Suspense fallback={<Spinner size="small" />}>
-            <CreationList />
-          </Suspense>
-          <Suspense fallback={<Spinner size="small" />}>
-            <LikesList />
-          </Suspense>
-          <Suspense fallback={<Spinner size="small" />}>
-            <CommentsList />
-          </Suspense>
-          <Suspense fallback={<Spinner size="small" />}>
-            <FollowsList />
-          </Suspense>
-        </Tabs.Pager>
-      </Tabs.Root>
+      <Suspense fallback={<Spinner size="small" />}>
+        <AllActivityList />
+      </Suspense>
     </View>
-  );
-};
-
-const CreationList = () => {
-  const { isLoading, data, fetchMore, isRefreshing, refresh, isLoadingMore } =
-    useActivity({ typeId: 3 });
-
-  const keyExtractor = useCallback((item) => item.id, []);
-
-  const renderItem = useCallback(
-    ({ item }) => <Card act={item} variant="activity" />,
-    []
-  );
-
-  const ListFooterComponent = useCallback(
-    () => <Footer isLoading={isLoadingMore} />,
-    [isLoadingMore]
-  );
-
-  if (isLoading) {
-    return (
-      <View tw="items-center justify-center flex-1">
-        <Spinner />
-      </View>
-    );
-  }
-
-  return (
-    <Tabs.FlatList
-      data={data}
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
-      refreshing={isRefreshing}
-      onRefresh={refresh}
-      onEndReached={fetchMore}
-      onEndReachedThreshold={0.6}
-      removeClippedSubviews={Platform.OS !== "web"}
-      numColumns={1}
-      windowSize={4}
-      initialNumToRender={2}
-      alwaysBounceVertical={false}
-      ListFooterComponent={ListFooterComponent}
-    />
-  );
-};
-
-const LikesList = () => {
-  const { isLoading, data, fetchMore, isRefreshing, refresh, isLoadingMore } =
-    useActivity({ typeId: 1 });
-
-  const keyExtractor = useCallback((item) => item.id, []);
-
-  const renderItem = useCallback(
-    ({ item }) => <Card act={item} variant="activity" />,
-    []
-  );
-
-  const ListFooterComponent = useCallback(
-    () => <Footer isLoading={isLoadingMore} />,
-    [isLoadingMore]
-  );
-
-  if (isLoading) {
-    return (
-      <View tw="items-center justify-center flex-1">
-        <Spinner />
-      </View>
-    );
-  }
-
-  return (
-    <Tabs.FlatList
-      data={data}
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
-      refreshing={isRefreshing}
-      onRefresh={refresh}
-      onEndReached={fetchMore}
-      onEndReachedThreshold={0.6}
-      removeClippedSubviews={Platform.OS !== "web"}
-      numColumns={1}
-      windowSize={4}
-      initialNumToRender={2}
-      alwaysBounceVertical={false}
-      ListFooterComponent={ListFooterComponent}
-    />
-  );
-};
-
-const CommentsList = () => {
-  const { isLoading, data, fetchMore, isRefreshing, refresh, isLoadingMore } =
-    useActivity({ typeId: 2 });
-
-  const keyExtractor = useCallback((item) => item.id, []);
-
-  const renderItem = useCallback(
-    ({ item }) => <Card act={item} variant="activity" />,
-    []
-  );
-
-  const ListFooterComponent = useCallback(
-    () => <Footer isLoading={isLoadingMore} />,
-    [isLoadingMore]
-  );
-
-  if (isLoading) {
-    return (
-      <View tw="items-center justify-center flex-1">
-        <Spinner />
-      </View>
-    );
-  }
-
-  return (
-    <Tabs.FlatList
-      data={data}
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
-      refreshing={isRefreshing}
-      onRefresh={refresh}
-      onEndReached={fetchMore}
-      onEndReachedThreshold={0.6}
-      removeClippedSubviews={Platform.OS !== "web"}
-      numColumns={1}
-      windowSize={4}
-      initialNumToRender={2}
-      alwaysBounceVertical={false}
-      ListFooterComponent={ListFooterComponent}
-    />
-  );
-};
-
-const FollowsList = () => {
-  const { isLoading, data, fetchMore, isRefreshing, refresh, isLoadingMore } =
-    useActivity({ typeId: 4, limit: 10 });
-
-  const keyExtractor = useCallback((item) => item.id, []);
-
-  const renderItem = useCallback(
-    ({ item }) => <Card act={item} variant="activity" />,
-    []
-  );
-
-  const ListFooterComponent = useCallback(
-    () => <Footer isLoading={isLoadingMore} />,
-    [isLoadingMore]
-  );
-
-  if (isLoading) {
-    return (
-      <View tw="items-center justify-center flex-1">
-        <Spinner />
-      </View>
-    );
-  }
-
-  return (
-    <Tabs.FlatList
-      data={data}
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
-      refreshing={isRefreshing}
-      onRefresh={refresh}
-      onEndReached={fetchMore}
-      onEndReachedThreshold={0.6}
-      removeClippedSubviews={Platform.OS !== "web"}
-      numColumns={1}
-      windowSize={4}
-      initialNumToRender={10}
-      alwaysBounceVertical={false}
-      ListFooterComponent={ListFooterComponent}
-    />
   );
 };
 
@@ -285,6 +44,20 @@ const AllActivityList = () => {
     []
   );
 
+  const ListHeaderComponent = useCallback(
+    () => (
+      <View tw="bg-white dark:bg-black pt-4 pl-4 pb-[3px]">
+        <Text
+          variant="text-2xl"
+          tw="text-gray-900 dark:text-white font-extrabold"
+        >
+          Home
+        </Text>
+      </View>
+    ),
+    []
+  );
+
   const ListFooterComponent = useCallback(
     () => <Footer isLoading={isLoadingMore} />,
     [isLoadingMore]
@@ -299,7 +72,7 @@ const AllActivityList = () => {
   }
 
   return (
-    <Tabs.FlatList
+    <FlatList
       data={data}
       keyExtractor={keyExtractor}
       renderItem={renderItem}
@@ -312,6 +85,7 @@ const AllActivityList = () => {
       windowSize={4}
       initialNumToRender={2}
       alwaysBounceVertical={false}
+      ListHeaderComponent={ListHeaderComponent}
       ListFooterComponent={ListFooterComponent}
     />
   );
