276dd9d04429c21b1046e26e33199bad274164e4
Nishan <nishanbende@gmail.com>
2022-03-12 07:26:40 +0530

    add header offset in other screens

--- packages/app/screens/edit-profile.tsx
+++ packages/app/screens/edit-profile.tsx
@@ -1,5 +1,16 @@
+import { useHeaderHeight } from "@react-navigation/elements";
+
 import { EditProfile } from "app/components/edit-profile";
 
+import { View } from "design-system";
+
 export const EditProfileScreen = () => {
-  return <EditProfile />;
+  const headerHeight = useHeaderHeight();
+
+  return (
+    <>
+      <View tw={`h-[${headerHeight}px]`} />
+      <EditProfile />
+    </>
+  );
 };
--- packages/app/screens/notifications.tsx
+++ packages/app/screens/notifications.tsx
@@ -1,21 +1,28 @@
 import { Suspense } from "react";
 
+import { useHeaderHeight } from "@react-navigation/elements";
+
 import { withColorScheme } from "app/components/memo-with-theme";
 import { Notifications } from "app/components/notifications";
 
 import { View, Spinner } from "design-system";
 
 const NotificationsScreen = withColorScheme(() => {
+  const headerHeight = useHeaderHeight();
+
   return (
-    <Suspense
-      fallback={
-        <View tw="mt-10 items-center justify-center">
-          <Spinner size="small" />
-        </View>
-      }
-    >
-      <Notifications />
-    </Suspense>
+    <>
+      <View tw={`h-[${headerHeight}px] bg-black`} />
+      <Suspense
+        fallback={
+          <View tw="mt-10 items-center justify-center">
+            <Spinner size="small" />
+          </View>
+        }
+      >
+        <Notifications />
+      </Suspense>
+    </>
   );
 });
 
