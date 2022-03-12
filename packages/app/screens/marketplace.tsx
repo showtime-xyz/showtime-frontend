c267cbec8927aa411be12caa3a5acef92720e777
nishan (o^▽^o) <nishanbende@gmail.com>
2022-03-12 07:27:25 +0530

    add header offset in other screens (#882)

--- packages/app/screens/marketplace.tsx
+++ packages/app/screens/marketplace.tsx
@@ -1,18 +1,26 @@
+import { useHeaderHeight } from "@react-navigation/elements";
+
 import { withColorScheme } from "app/components/memo-with-theme";
 
 import { View, Text } from "design-system";
 
 const MarketplaceScreen = withColorScheme(() => {
+  const headerHeight = useHeaderHeight();
+
   return (
-    <View tw="p-4">
-      <Text variant="text-2xl" tw="font-extrabold text-black dark:text-white">
-        Discover
-      </Text>
-      <View tw="h-6" />
-      <Text tw="font-semibold text-gray-600 dark:text-gray-400">
-        🚧 Coming soon
-      </Text>
-    </View>
+    <>
+      <View tw={`h-[${headerHeight}px] bg-black`} />
+
+      <View tw="p-4">
+        <Text variant="text-2xl" tw="font-extrabold text-black dark:text-white">
+          Discover
+        </Text>
+        <View tw="h-6" />
+        <Text tw="font-semibold text-gray-600 dark:text-gray-400">
+          🚧 Coming soon
+        </Text>
+      </View>
+    </>
   );
 });
 
