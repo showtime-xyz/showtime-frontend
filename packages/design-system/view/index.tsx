c9fee64e9b0a2d6f4cd71311ffed308e400f475a
nishan (o^▽^o) <nishanbende@gmail.com>
2022-03-21 16:31:42 +0530

    feat: double tap to like (#913)
    * feat: double tap to like
    
    * feat: show header on double tap
    
    * fix: failing test and add tiny animation

--- packages/design-system/view/index.tsx
+++ packages/design-system/view/index.tsx
@@ -1,14 +1,18 @@
-import { ComponentProps } from "react";
+import React, { ComponentProps } from "react";
 
 import { View as DripsyView } from "dripsy";
 
 import { tw as tailwind } from "design-system/tailwind";
-import type { TW } from "design-system/tailwind/types";
 
-export type ViewProps = { tw?: TW } & ComponentProps<typeof DripsyView>;
+export type ViewProps = { tw?: string } & ComponentProps<typeof DripsyView>;
 
-function View({ tw, sx, ...props }: ViewProps) {
-  return <DripsyView sx={{ ...sx, ...tailwind.style(tw) }} {...props} />;
-}
+const View = React.forwardRef(function View(
+  { tw, sx, ...props }: ViewProps,
+  ref: any
+) {
+  return (
+    <DripsyView sx={{ ...sx, ...tailwind.style(tw) }} {...props} ref={ref} />
+  );
+});
 
 export { View };
