0f70e169c9305694ebcd1a7c10311eea2f5fdb6a
Mo Gorhom <gorhom.dev@gmail.com>
2022-04-10 16:25:06 +0100

    fix: added pathname to useRouter (#957)

--- packages/app/navigation/use-router.ts
+++ packages/app/navigation/use-router.ts
@@ -1,11 +1,25 @@
+import {
+  useNavigation as useNativeRouter,
+  NavigationState,
+} from "@react-navigation/native";
 import { useRouter as SolitoRouter } from "solito/router";
 
+const getPath = (navigationState: NavigationState) => {
+  return (
+    navigationState?.routes?.[navigationState?.index]?.path ??
+    navigationState?.routes?.[navigationState?.index]?.state?.routes[0]?.path ??
+    "/"
+  );
+};
+
 export function useRouter() {
+  const { getState } = useNativeRouter();
   const solitoRouter = SolitoRouter();
   return {
     ...solitoRouter,
     pop: () => {
       solitoRouter.back();
     },
+    pathname: getPath(getState()),
   };
 }
