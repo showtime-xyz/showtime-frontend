5d5e0e4b6efd326a66ef22cb5bcab760108da643
Axel Delafosse <axeldelafosse@gmail.com>
2022-04-13 11:25:24 +0200

    set color scheme + improve header and footer

--- packages/app/components/footer.tsx
+++ packages/app/components/footer.tsx
@@ -8,41 +8,52 @@ import {
   NotificationsTabBarIcon,
   ProfileTabBarIcon,
 } from "app/navigation/tab-bar-icons";
+import { useNavigationElements } from "app/navigation/use-navigation-elements";
 import { useRouter } from "app/navigation/use-router";
 
+import { useBlurredBackgroundColor } from "design-system/hooks";
+import { useIsDarkMode } from "design-system/hooks";
 import { View } from "design-system/view";
 
 const Footer = () => {
   const router = useRouter();
+  const isDark = useIsDarkMode();
+  const color = isDark ? "white" : "black";
   const { width } = useWindowDimensions();
   const { isAuthenticated } = useUser();
+  const blurredBackgroundColor = useBlurredBackgroundColor(95);
+  const { isTabBarHidden } = useNavigationElements();
 
   if (width >= 768) {
     // TODO: "web" desktop footer
     return null;
   }
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated || isTabBarHidden) {
     return null;
   }
 
   return (
     <View
       // @ts-expect-error
-      style={{ position: "fixed", backdropFilter: "blur(20px)" }}
+      style={{
+        position: "fixed",
+        backdropFilter: "blur(20px)",
+        backgroundColor: blurredBackgroundColor,
+      }}
       tw="bottom-0 right-0 left-0 z-50 h-16 flex-row items-center justify-between px-4 py-2"
     >
       <HomeTabBarIcon
-        color="black"
+        color={color}
         focused={router.pathname === "/" || router.pathname === "/home"}
       />
       <TrendingTabBarIcon
-        color="black"
+        color={color}
         focused={router.pathname === "/trending"}
       />
-      <CameraTabBarIcon color="black" focused={router.pathname === "/camera"} />
+      <CameraTabBarIcon color={color} focused={router.pathname === "/camera"} />
       <NotificationsTabBarIcon
-        color="black"
+        color={color}
         focused={router.pathname === "/notifications"}
       />
       <ProfileTabBarIcon />
