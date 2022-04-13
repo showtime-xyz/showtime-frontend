9ca8510b96a2bd9909ff3895d8dfd4e4a6060129
Enes Ozturk <enesozturk.d@gmail.com>
2022-04-13 22:30:22 +0300

    feat: add new columns to table and update styles

--- packages/app/components/nft-activity/table-head.tsx
+++ packages/app/components/nft-activity/table-head.tsx
@@ -5,17 +5,27 @@ import { Text, View } from "design-system";
 const TableHead = () => {
   return (
     <View tw="py-2 px-4 bg-gray-100 dark:bg-gray-900 flex flex-row">
-      <View tw="flex-1">
+      <View tw="flex-1 min-w-[128px]">
         <Text variant="text-xs" tw="font-bold text-gray-700 dark:text-gray-300">
           Event
         </Text>
       </View>
-      <View tw="flex-1">
+      <View tw="flex-1 min-w-[128px]">
+        <Text variant="text-xs" tw="font-bold text-gray-700 dark:text-gray-300">
+          From
+        </Text>
+      </View>
+      <View tw="flex-1 min-w-[128px]">
         <Text variant="text-xs" tw="font-bold text-gray-700 dark:text-gray-300">
           To
         </Text>
       </View>
-      <View tw="flex-1">
+      <View tw="flex-1 min-w-[128px]">
+        <Text variant="text-xs" tw="font-bold text-gray-700 dark:text-gray-300">
+          Quantity
+        </Text>
+      </View>
+      <View tw="flex-1 min-w-[128px]">
         <Text variant="text-xs" tw="font-bold text-gray-700 dark:text-gray-300">
           Date
         </Text>
--- packages/app/components/nft-activity/table-row.tsx
+++ packages/app/components/nft-activity/table-row.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { formatDistanceToNowStrict } from "date-fns";
+
 import { Text, View } from "design-system";
 
 import { getNftActivityEventIcon } from "./nft-activity.helpers";
@@ -9,21 +11,33 @@ import UserItem from "./user-item";
 const TableRow = ({ ...props }: TableRowProps) => {
   return (
     <View tw="flex flex-row items-center p-4">
-      <View tw="flex flex-row items-center flex-1">
+      <View tw="flex flex-row items-center flex-1 min-w-[128px]">
         <View tw="mr-2">{getNftActivityEventIcon(props.event_type)}</View>
         <Text tw=" text-gray-600 dark:text-gray-400 text-xs font-semibold">
           {props.event_type}
         </Text>
       </View>
-      <View tw="flex-1 text-gray-600 dark:text-gray-400 text-xs font-semibold">
+      <View tw="flex-1 text-gray-600 dark:text-gray-400 text-xs font-semibold min-w-[128px]">
+        <UserItem
+          imageUrl={props.from_img_url}
+          title={props.from_username}
+          verified={props.from_verified}
+        />
+      </View>
+      <View tw="flex-1 text-gray-600 dark:text-gray-400 text-xs font-semibold min-w-[128px]">
         <UserItem
           imageUrl={props.to_img_url}
           title={props.to_username}
           verified={props.to_verified}
         />
       </View>
-      <Text tw="flex-1 text-gray-600 dark:text-gray-400 text-xs font-semibold">
-        Row {/** TODO(enes): add relative time */}
+      <Text tw="flex-1 text-gray-600 dark:text-gray-400 text-xs font-semibold min-w-[128px]">
+        {props.quantity}
+      </Text>
+      <Text tw="flex-1 text-gray-600 dark:text-gray-400 text-xs font-semibold min-w-[128px]">
+        {formatDistanceToNowStrict(new Date(props.timestamp), {
+          addSuffix: true,
+        })}
       </Text>
     </View>
   );
