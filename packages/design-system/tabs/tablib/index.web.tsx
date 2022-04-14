944e04e75c66811d728bd016b9a3076468847b4b
nishan (o^▽^o) <nishanbende@gmail.com>
2022-04-14 14:00:40 +0530

    fix: profile tabs (#980)

--- packages/design-system/tabs/tablib/index.web.tsx
+++ packages/design-system/tabs/tablib/index.web.tsx
@@ -3,6 +3,8 @@ import { ScrollView, FlatList, SectionList, Animated } from "react-native";
 
 import * as RadixTabs from "@radix-ui/react-tabs";
 
+import { flattenChildren } from "app/utilities";
+
 import { tw } from "../../tailwind";
 import { View } from "../../view";
 import { TabRootProps } from "./types";
@@ -46,13 +48,14 @@ const Root = ({
       let tabContents = [];
       let headerChild;
       let listChild = {};
-      React.Children.forEach(children, (c) => {
+
+      flattenChildren(children).forEach((c) => {
         if (React.isValidElement(c)) {
           //@ts-ignore
           if (c.type === List) {
             listChild = c;
             //@ts-ignore
-            React.Children.map(c.props.children, (c) => {
+            flattenChildren(c.props.children).forEach((c) => {
               if (React.isValidElement(c) && c && c.type === Trigger) {
                 tabTriggers.push(c);
               }
@@ -63,7 +66,7 @@ const Root = ({
             //@ts-ignore
           } else if (c.type === Pager) {
             //@ts-ignore
-            React.Children.map(c.props.children, (c) => {
+            flattenChildren(c.props.children).forEach((c) => {
               tabContents.push(c);
             });
           }
