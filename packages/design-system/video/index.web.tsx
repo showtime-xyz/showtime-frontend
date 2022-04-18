b096269acfddab9426df79e2510470b9c1a2fd41
Nishan <nishanbende@gmail.com>
2022-04-18 14:40:25 +0530

    web: fix video styles

--- packages/design-system/video/index.web.tsx
+++ packages/design-system/video/index.web.tsx
@@ -1,23 +1,46 @@
 import { ComponentProps } from "react";
+import { StyleSheet } from "react-native";
 
 import { Video as ExpoVideo } from "expo-av";
 
+import { useVideoConfig } from "app/context/video-config-context";
+
+import { Image } from "design-system/image";
+import { tw as tailwind } from "design-system/tailwind";
 import type { TW } from "design-system/tailwind/types";
+import { View } from "design-system/view";
 
 type VideoProps = {
   tw?: TW;
 } & ComponentProps<typeof ExpoVideo>;
 
 function Video({ tw, style, ...props }: VideoProps) {
+  const videoConfig = useVideoConfig();
+
   return (
-    <ExpoVideo
-      isMuted
-      useNativeControls={false}
-      resizeMode="cover"
-      shouldPlay
-      source={props.source}
-      {...props}
-    />
+    <View style={[style, tailwind.style(tw)]}>
+      {videoConfig?.previewOnly ? (
+        <Image style={StyleSheet.absoluteFill} source={props.posterSource} />
+      ) : (
+        <>
+          <Image
+            //@ts-ignore
+            source={props.posterSource}
+            style={StyleSheet.absoluteFill}
+          />
+
+          <ExpoVideo
+            style={StyleSheet.absoluteFill}
+            useNativeControls={videoConfig?.useNativeControls}
+            shouldPlay
+            resizeMode="cover"
+            posterSource={props.posterSource}
+            source={props.source}
+            {...props}
+          />
+        </>
+      )}
+    </View>
   );
 }
 
