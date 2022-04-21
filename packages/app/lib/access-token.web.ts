c1ac4e50a91c5857bc69fc21d411bd60159f3992
Axel Delafosse <axeldelafosse@gmail.com>
2022-04-21 18:59:18 +0200

    implement storage listener for access token

--- packages/app/lib/access-token.web.ts
+++ packages/app/lib/access-token.web.ts
@@ -1,19 +1,38 @@
+import { useEffect, useState } from "react";
+
 const ACCESS_TOKEN_STRING = "access-token";
+let listeners: any = [];
 
 export function setAccessToken(token: string) {
-  localStorage.setItem(ACCESS_TOKEN_STRING, token);
+  if (typeof window !== "undefined") {
+    localStorage.setItem(ACCESS_TOKEN_STRING, token);
+    listeners.forEach((listener: any) => listener(token));
+  }
 }
 
 export function getAccessToken() {
-  return localStorage.getItem(ACCESS_TOKEN_STRING);
+  if (typeof window !== "undefined") {
+    return localStorage.getItem(ACCESS_TOKEN_STRING);
+  }
 }
 
 export function deleteAccessToken() {
-  localStorage.removeItem(ACCESS_TOKEN_STRING);
+  if (typeof window !== "undefined") {
+    localStorage.removeItem(ACCESS_TOKEN_STRING);
+  }
 }
 
 export function useAccessToken() {
-  // TODO: implement a storage listener
+  const [, setCount] = useState(0);
+  useEffect(() => {
+    const callback = () => {
+      setCount((c) => c + 1);
+    };
+    listeners.push(callback);
+    return () => {
+      listeners = listeners.filter((l: any) => l !== callback);
+    };
+  }, []);
 
   return getAccessToken();
 }
