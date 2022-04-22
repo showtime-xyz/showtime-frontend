84c889ebd77b5c478951f62c88bc4ea171fdee40
Axel Delafosse <axeldelafosse@gmail.com>
2022-04-22 13:30:30 +0200

    clean code

--- packages/app/components/transfer.tsx
+++ packages/app/components/transfer.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Platform, Linking } from "react-native";
 
 import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
