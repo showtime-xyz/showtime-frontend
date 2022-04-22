b26c136f3af2e2085abfdad4fd1cc136107df2cd
Axel Delafosse <axeldelafosse@gmail.com>
2022-04-22 10:31:58 +0200

    clean access token code

--- packages/app/lib/axios.ts
+++ packages/app/lib/axios.ts
@@ -1,55 +1,7 @@
-import { Platform } from "react-native";
-
-import { captureException } from "@sentry/nextjs";
 import axios from "axios";
 import type { Method, AxiosRequestHeaders } from "axios";
-import createAuthRefreshInterceptor from "axios-auth-refresh";
 
-import { accessTokenManager } from "app/lib/access-token-manager";
-import { setLogout } from "app/lib/logout";
-import { deleteRefreshToken } from "app/lib/refresh-token";
-
-/**
- * An expired or missing access token sent to our backend services will trigger a
- * 401 response status code that we intercept to trigger a silent access token refresh.
- * On refresh success the request will be resent and on failure we force the logout flow.
- */
-if (Platform.OS === "web") {
-  createAuthRefreshInterceptor(
-    axios,
-    async () => {
-      try {
-        const refreshedAccessToken =
-          await accessTokenManager.refreshAccessToken();
-        const invalidRefreshedAccessToken = !refreshedAccessToken;
-
-        if (invalidRefreshedAccessToken) {
-          throw "The refresh request has failed, likely due to an expired refresh token";
-        }
-
-        return axios;
-      } catch (error) {
-        if (process.env.NODE_ENV === "development") {
-          console.error(error);
-        }
-
-        // accessTokenManager.deleteAccessToken();
-        // deleteRefreshToken();
-        // setLogout(Date.now().toString());
-
-        captureException(error, {
-          tags: {
-            failed_silent_refresh: "axios.ts",
-          },
-        });
-
-        // Prevents the refresh interceptor from retrying authentication
-        return Promise.reject();
-      }
-    },
-    { statusCodes: [401] }
-  );
-}
+import { getAccessToken } from "app/lib/access-token";
 
 export type AxiosOverrides = {
   forceAccessTokenAuthorization: boolean;
@@ -72,7 +24,7 @@ const axiosAPI = async ({
   headers,
   overrides,
 }: AxiosParams) => {
-  const accessToken = accessTokenManager.getAccessToken();
+  const accessToken = getAccessToken();
   const forceAccessTokenAuthorization =
     overrides?.forceAccessTokenAuthorization;
   let authorizationHeader = data?.did
@@ -104,11 +56,10 @@ const axiosAPI = async ({
       : {}),
   };
 
-  // console.log("request to the server ", request);
   try {
     return await axios(request).then((res) => res.data);
   } catch (error) {
-    console.log("failed request ", request);
+    console.log("Failed request:", request);
     console.error(error);
     throw error;
   }
