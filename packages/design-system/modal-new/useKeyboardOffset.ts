1a9d2148cdcdda6738fa35f6a7544fa56f3daf7a
Mo Gorhom <gorhom.dev@gmail.com>
2022-04-24 20:56:30 +0100

    feat: added modal footer component

---
+++ packages/design-system/modal-new/useKeyboardOffset.ts
@@ -0,0 +1,7 @@
+import type { KEYBOARD_STATE } from "@gorhom/bottom-sheet";
+import type { SharedValue } from "react-native-reanimated";
+
+export const useKeyboardOffset = (
+  top: number,
+  keyboardState: SharedValue<KEYBOARD_STATE>
+) => undefined;
