91d57bb390c6d9c2411e0c47739c67d0d3f645aa
nishan (o^▽^o) <nishanbende@gmail.com>
2022-04-25 16:05:53 +0530

    feat: responsive trending screen (#1040)
    * show hide components
    
    * show hide components
    
    * hidden improvements
    
    * hidden improvements
    
    * feat: trending

--- packages/app/screens/trending.tsx
+++ packages/app/screens/trending.tsx
@@ -4,12 +4,25 @@ import { withColorScheme } from "app/components/memo-with-theme";
 import { Trending } from "app/components/trending";
 import { mixpanel } from "app/lib/mixpanel";
 
+import { Hidden } from "design-system/hidden";
+
+import { Trending as TrendingMD } from "./trending.md";
+
 const TrendingScreen = withColorScheme(() => {
   useEffect(() => {
     mixpanel.track("Trending page view");
   }, []);
 
-  return <Trending />;
+  return (
+    <>
+      <Hidden from="md">
+        <Trending />
+      </Hidden>
+      <Hidden till="md">
+        <TrendingMD />
+      </Hidden>
+    </>
+  );
 });
 
 export { TrendingScreen };
