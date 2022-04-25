2f7fbc96fb75dfacf62cfe7243833941a34359f7
nishan (o^▽^o) <nishanbende@gmail.com>
2022-04-25 13:29:03 +0530

    web: minting  (#1034)
    * file picker
    
    * file picker
    
    * merge
    
    * nsfw field
    
    * file state in form
    
    * fix: form file state
    
    * fix: create flow native
    
    * fix: video preview fix
    
    * fix: video preview fix
    
    * do not reset state for now
    
    * fix: linking
    
    * fix: signer and provider hook
    
    * fix: alert

--- packages/app/context/mint-context.ts
+++ packages/app/context/mint-context.ts
@@ -2,14 +2,14 @@ import { createContext, Dispatch } from "react";
 
 import {
   MintNFTType,
-  MintNFTStatus,
   ActionPayload,
+  MintActionType,
 } from "app/hooks/use-mint-nft";
 
 type MintContextType = {
   state: MintNFTType;
   dispatch: Dispatch<{
-    type: MintNFTStatus;
+    type: MintActionType;
     payload?: ActionPayload;
   }>;
 };
