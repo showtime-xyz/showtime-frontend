d6d52cade55894b21ef019dab32a55988e1e2f2a
Nishan <nishanbende@gmail.com>
2022-04-25 20:10:55 +0530

    feat: web burn

--- packages/app/hooks/use-burn-nft.ts
+++ packages/app/hooks/use-burn-nft.ts
@@ -1,17 +1,10 @@
-import { useContext, useEffect, useReducer, useState } from "react";
+import { useReducer } from "react";
 
 import { ethers } from "ethers";
 
 import minterAbi from "app/abi/ShowtimeMT.json";
-import { AppContext } from "app/context/app-context";
-import { useWeb3 } from "app/hooks/use-web3";
+import { useSignerAndProvider } from "app/hooks/use-signer-provider";
 import { track } from "app/lib/analytics";
-import { useWalletConnect } from "app/lib/walletconnect";
-import { getBiconomy } from "app/utilities";
-
-import { useAlert } from "design-system/alert";
-
-import { useUser } from "./use-user";
 
 type BurnNFTType = {
   status: "idle" | "burning" | "burningError" | "burningSuccess";
@@ -50,102 +43,69 @@ export type UseBurnNFT = {
 };
 
 export const useBurnNFT = () => {
-  const Alert = useAlert();
   const [state, dispatch] = useReducer(burnNFTReducer, initialBurnNFTState);
-  const { user } = useUser();
-  const [userAddress, setUserAddress] = useState<string>();
-  const connector = useWalletConnect();
-  const { web3 } = useWeb3();
-
-  const getActiveUserAddress = async () => {
-    const isMagic = !!web3;
-
-    if (isMagic) {
-      const signer = web3.getSigner();
-      const activeMagicAddress = await signer.getAddress();
-      setUserAddress(activeMagicAddress);
-    } else if (connector.connected) {
-      const [connectorAddress] = connector.accounts.filter((address) =>
-        address.startsWith("0x")
-      );
-      if (connectorAddress) {
-        setUserAddress(connectorAddress);
-      }
-    } else {
-      await connector.connect();
-      console.log("Not connected to wallet, sending connect request");
-    }
-  };
-
-  useEffect(() => {
-    getActiveUserAddress();
-  }, [user, web3]);
+  const { getSignerAndProvider } = useSignerAndProvider();
 
   async function burnToken({ ...params }: UseBurnNFT) {
     return new Promise<{ transaction: string }>(async (resolve, reject) => {
-      const { biconomy } = await getBiconomy(connector, web3);
+      const result = await getSignerAndProvider();
+      if (result) {
+        const { provider, signer, signerAddress } = result;
 
-      const contract = new ethers.Contract(
-        //@ts-ignore
-        process.env.NEXT_PUBLIC_MINTING_CONTRACT,
-        minterAbi,
-        biconomy.getSignerByAddress(userAddress)
-      );
+        const contract = new ethers.Contract(
+          //@ts-ignore
+          process.env.NEXT_PUBLIC_MINTING_CONTRACT,
+          minterAbi,
+          signer
+        );
 
-      const { data } = await contract.populateTransaction.burn(
-        userAddress,
-        params.tokenId,
-        params.copies
-      );
-      const provider = biconomy.getEthersProvider();
+        const { data } = await contract.populateTransaction.burn(
+          signerAddress,
+          params.tokenId,
+          params.copies
+        );
 
-      console.log("** burning: opening wallet for signing **");
+        console.log("** burning: opening wallet for signing **");
 
-      const transaction = await provider
-        .send("eth_sendTransaction", [
-          {
-            data,
-            from: userAddress,
-            to: process.env.NEXT_PUBLIC_MINTING_CONTRACT,
-            signatureType: "EIP712_SIGN",
-          },
-        ])
-        .catch((error: any) => {
-          console.error(error);
-          // TODO: Add a proper error message. Find what 4001 means
-          if (error.code === 4001) {
+        const transaction = await provider
+          .send("eth_sendTransaction", [
+            {
+              data,
+              from: signerAddress,
+              to: process.env.NEXT_PUBLIC_MINTING_CONTRACT,
+              signatureType: "EIP712_SIGN",
+            },
+          ])
+          .catch((error: any) => {
+            console.error(error);
+            // TODO: Add a proper error message. Find what 4001 means
+            if (error.code === 4001) {
+              reject("Something went wrong");
+            }
+
             reject("Something went wrong");
-          }
+          });
 
-          reject("Something went wrong");
-        });
+        console.log("transaction hash ", transaction);
 
-      console.log("transaction hash ", transaction);
-
-      provider.once(transaction, () => resolve({ transaction: transaction }));
+        provider.once(transaction, () => resolve({ transaction: transaction }));
+      }
     });
   }
 
   async function burnTokenPipeline(params: UseBurnNFT) {
-    if (userAddress) {
-      try {
-        dispatch({ type: "burning" });
-        console.log("** Begin burning **");
+    try {
+      dispatch({ type: "burning" });
+      console.log("** Begin burning **");
 
-        const response = await burnToken({ ...params });
-        dispatch({ type: "burningSuccess", transaction: response.transaction });
-        track("NFT Burned");
+      const response = await burnToken({ ...params });
+      dispatch({ type: "burningSuccess", transaction: response.transaction });
+      track("NFT Burned");
 
-        console.log("** burning success **");
-      } catch (e) {
-        dispatch({ type: "burningError" });
-        throw e;
-      }
-    } else {
-      // TODO: better error handling. Maybe show login screen
-      Alert.alert(
-        "Sorry! We can't find your user address. Please login with a wallet or email/phone"
-      );
+      console.log("** burning success **");
+    } catch (e) {
+      console.error("error while burning ", e);
+      dispatch({ type: "burningError" });
     }
   }
 
