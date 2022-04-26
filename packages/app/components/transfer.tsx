1f4650634b9fa2925340110e0087c23cc821641f
Nishan <nishanbende@gmail.com>
2022-04-26 10:23:34 +0530

    show profile in transfer

--- packages/app/components/transfer.tsx
+++ packages/app/components/transfer.tsx
@@ -1,29 +1,35 @@
 import { useState } from "react";
-import { Platform, Linking } from "react-native";
+import { Linking, Platform } from "react-native";
 
 import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { formatDistanceToNowStrict } from "date-fns";
-import { useForm, Controller } from "react-hook-form";
-import useSWR from "swr";
-import useUnmountSignal from "use-unmount-signal";
+import { Controller, useForm } from "react-hook-form";
 
 import { useCurrentUserAddress } from "app/hooks/use-current-user-address";
-import { supportedVideoExtensions } from "app/hooks/use-mint-nft";
 import { useTransferNFT } from "app/hooks/use-transfer-nft";
-import { axios } from "app/lib/axios";
 import { yup } from "app/lib/yup";
-import type { NFT } from "app/types";
 import { findAddressInOwnerList } from "app/utilities";
 
-import { View, Text, Fieldset, Button, ScrollView } from "design-system";
+import {
+  Button,
+  Fieldset,
+  Media,
+  ScrollView,
+  Text,
+  VerificationBadge,
+  View,
+} from "design-system";
+import { Avatar } from "design-system/avatar";
 import { Collection } from "design-system/card/rows/collection";
 import { Owner } from "design-system/card/rows/owner";
-import { ArrowRight, PolygonScan, Check } from "design-system/icon";
-import { Image } from "design-system/image";
+import { ArrowRight, Check, PolygonScan } from "design-system/icon";
 import { Spinner } from "design-system/spinner";
 import { tw } from "design-system/tailwind";
-import { Video } from "design-system/video";
+
+import { useUserProfile } from "../hooks/api-hooks";
+import useDebounce from "../hooks/use-debounce";
+import { useNFTDetails } from "../hooks/use-nft-details";
 
 type FormData = {
   quantity: number;
@@ -34,13 +40,7 @@ function Transfer({ nftId }: { nftId?: string }) {
   const { startTransfer, state } = useTransferNFT();
   const { userAddress } = useCurrentUserAddress();
 
-  const [url] = useState(`/v2/nft_detail/${nftId}`);
-  const unmountSignal = useUnmountSignal();
-  const { data, error } = useSWR([url], (url) =>
-    axios({ url, method: "GET", unmountSignal })
-  );
-
-  const nft = data?.data as NFT;
+  const { data: nft, error, loading } = useNFTDetails(Number(nftId));
 
   const ownerListItem = findAddressInOwnerList(
     userAddress,
@@ -81,8 +81,13 @@ function Transfer({ nftId }: { nftId?: string }) {
     defaultValues,
   });
 
+  const [transferAddress, setTransferAddress] = useState("");
+  const debouncedTransferAddress = useDebounce(transferAddress, 200);
+
+  const { data } = useUserProfile({ address: debouncedTransferAddress });
+
   function handleSubmitTransfer({ quantity, receiverAddress }: FormData) {
-    startTransfer({ nft, receiverAddress, quantity });
+    if (nft) startTransfer({ nft, receiverAddress, quantity });
   }
 
   function handleOpenPolygonScan() {
@@ -97,15 +102,17 @@ function Transfer({ nftId }: { nftId?: string }) {
     console.error(error);
   }
 
+  if (loading)
+    return (
+      <View tw="flex-1 items-center justify-center">
+        <Spinner />
+      </View>
+    );
+
   if (!nft) return null;
 
   const TransferNftScrollView =
     Platform.OS === "android" ? BottomSheetScrollView : ScrollView;
-
-  const fileExtension = nft?.token_img_url.split(".").pop();
-  const isVideo =
-    fileExtension && supportedVideoExtensions.includes(fileExtension);
-  const Preview = isVideo ? Video : Image;
 
   if (state.status === "transfering" || state.status === "transferingSuccess") {
     return (
@@ -152,12 +159,7 @@ function Transfer({ nftId }: { nftId?: string }) {
 
         <View tw="p-[16px]">
           <View tw="flex-row items-center pb-4">
-            <Preview
-              source={{
-                uri: nft?.token_img_url,
-              }}
-              tw="w-[80px] h-[80px] rounded-2xl"
-            />
+            <Media item={nft} tw="w-[80px] h-[80px] rounded-2xl" />
             <View tw="flex-1 px-[16px]">
               <Text
                 tw="text-black dark:text-white font-bold pb-4"
@@ -218,12 +220,17 @@ function Transfer({ nftId }: { nftId?: string }) {
             <Controller
               control={control}
               name="receiverAddress"
+              rules={{
+                onChange: (e) => {
+                  setTransferAddress(e.target.value);
+                },
+              }}
               render={({ field: { onChange, onBlur, value } }) => {
                 return (
                   <Fieldset
                     tw="flex-1"
                     label="Receiver"
-                    placeholder="eg: @showtime, showtime.eth, 0x..."
+                    placeholder="eg: showtime.eth, 0x..."
                     helperText="Type username, ENS, or Ethereum address"
                     onBlur={onBlur}
                     errorText={errors.receiverAddress?.message}
@@ -235,7 +242,44 @@ function Transfer({ nftId }: { nftId?: string }) {
               }}
             />
           </View>
+          {data?.data.profile ? (
+            <View tw="mt-4 flex-row items-center bg-white dark:bg-black shadow-lg rounded-lg p-4">
+              <Avatar url={data?.data.profile.img_url} />
+              <View tw="justify-around ml-1">
+                <Text
+                  variant="text-xs"
+                  tw="text-gray-600  font-semibold dark:text-gray-400"
+                >
+                  {data?.data.profile.name}
+                </Text>
+                <View tw="flex-row mt-1">
+                  <Text
+                    tw="dark:text-white text-black font-semibold"
+                    variant="text-13"
+                  >
+                    @{data?.data.profile.username}
+                  </Text>
+                  {data?.data.profile.verified ? (
+                    <View tw="ml-1">
+                      <VerificationBadge size={16} />
+                    </View>
+                  ) : null}
+                </View>
+              </View>
+              <View tw="ml-auto">
+                <Button
+                  variant="tertiary"
+                  onPress={() => {
+                    setTransferAddress("");
+                  }}
+                >
+                  Remove
+                </Button>
+              </View>
+            </View>
+          ) : null}
         </View>
+
         <View tw="absolute px-4 w-full" style={{ bottom: 16 }}>
           <Button
             onPress={handleSubmit(handleSubmitTransfer)}
