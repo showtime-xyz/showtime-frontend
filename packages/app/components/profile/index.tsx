c11d3a68201d261d6396df80189caa05a3b3be3b
alantoa <496807117@qq.com>
2022-04-29 00:28:13 +0800

    fix: tab not showing

--- packages/app/components/profile/index.tsx
+++ packages/app/components/profile/index.tsx
@@ -63,7 +63,7 @@ const Profile = ({ address }: { address?: string }) => {
   );
   return (
     <FilterContext.Provider value={{ filter, dispatch }}>
-      <View tw="web:mb-8 w-full items-center">
+      <View tw="web:mb-8 w-full web:items-center flex-1">
         <Tabs.Root
           onIndexChange={setSelected}
           initialIndex={selected}
