52be8560742307b881e5a0243eaeb463cfc1f735
Axel Delafosse <axeldelafosse@gmail.com>
2022-04-29 12:02:40 +0200

    fix profile loading

--- packages/app/components/profile/profile-top.tsx
+++ packages/app/components/profile/profile-top.tsx
@@ -1,3 +1,8 @@
+import { useContext, useEffect, useMemo, useRef, useState } from "react";
+import { Platform, useWindowDimensions } from "react-native";
+
+import reactStringReplace from "react-string-replace";
+
 import { ProfileDropdown } from "app/components/profile-dropdown";
 import { MAX_COVER_WIDTH } from "app/constants/layout";
 import { useMyInfo, useUserProfile } from "app/hooks/api-hooks";
@@ -5,10 +10,11 @@ import { useBlock } from "app/hooks/use-block";
 import { useCurrentUserId } from "app/hooks/use-current-user-id";
 import {
   BottomTabBarHeightContext,
-  useBottomTabBarHeight
+  useBottomTabBarHeight,
 } from "app/lib/react-navigation/bottom-tabs";
 import { TextLink } from "app/navigation/link";
 import { useRouter } from "app/navigation/use-router";
+
 import { Button, ModalSheet, Skeleton, Text, View } from "design-system";
 import { Avatar } from "design-system/avatar";
 import { Hidden } from "design-system/hidden";
@@ -17,15 +23,9 @@ import { Image } from "design-system/image";
 import { Pressable } from "design-system/pressable-scale";
 import { TW } from "design-system/tailwind/types";
 import { VerificationBadge } from "design-system/verification-badge";
-import { useContext, useEffect, useMemo, useRef, useState } from "react";
-import { Platform, useWindowDimensions } from "react-native";
-import reactStringReplace from "react-string-replace";
+
 import { getProfileImage, getProfileName } from "../../utilities";
 import { FollowersList, FollowingList } from "../following-user-list";
-
-
-
-
 
 type FollowProps = {
   onPressFollowing: () => void;
@@ -65,7 +65,7 @@ export const ProfileTop = ({
   address,
   isBlocked,
 }: {
-  address?: string;
+  address: string | null;
   isBlocked: boolean;
 }) => {
   const router = useRouter();
@@ -111,14 +111,16 @@ export const ProfileTop = ({
     [bio]
   );
   const coverImageHeight = useMemo(() => (width < 768 ? 120 : 180), [width]);
+
   useEffect(() => {
-    Platform.OS === 'web' && setShowBottomSheet(null)
+    Platform.OS === "web" && setShowBottomSheet(null);
   }, [address]);
+
   return (
     <>
       <View pointerEvents="box-none">
         <View
-          tw={`overflow-hidden bg-gray-100 dark:bg-gray-900 xl:rounded-b-[32px] xl:-mx-20`}
+          tw={`overflow-hidden bg-gray-100 dark:bg-gray-900 xl:-mx-20 xl:rounded-b-[32px]`}
           pointerEvents="none"
         >
           <Skeleton
@@ -159,46 +161,47 @@ export const ProfileTop = ({
                 </Skeleton>
               </View>
             </View>
-            <View tw="flex-row items-center" pointerEvents="box-none">
-              {isBlocked ? (
-                <Button
-                  size={width < 768 ? "small" : "regular"}
-                  onPress={() => {
-                    unblock(profileId);
-                  }}
-                >
-                  Unblock
-                </Button>
-              ) : (
-                <>
-                  <Hidden until="md">
-                    <Follow
-                      onPressFollower={() => setShowBottomSheet("followers")}
-                      onPressFollowing={() => setShowBottomSheet("following")}
-                      followersCount={profileData?.data.followers_count}
-                      followingCount={profileData?.data.following_count}
-                      tw="mr-8"
-                    />
-                  </Hidden>
-                  {profileId && userId !== profileId ? (
-                    <>
-                      <ProfileDropdown user={profileData?.data.profile} />
-                      <View tw="w-2" />
-                      <Button
-                        size={width < 768 ? "small" : "regular"}
-                        onPress={() => {
-                          if (isFollowingUser) {
-                            unfollow(profileId);
-                          } else {
-                            follow(profileId);
-                          }
-                        }}
-                      >
-                        {isFollowingUser ? "Following" : "Follow"}
-                      </Button>
-                    </>
-                  ) : (
-                    userId === profileId && (
+
+            {address ? (
+              <View tw="flex-row items-center" pointerEvents="box-none">
+                {isBlocked ? (
+                  <Button
+                    size={width < 768 ? "small" : "regular"}
+                    onPress={() => {
+                      unblock(profileId);
+                    }}
+                  >
+                    Unblock
+                  </Button>
+                ) : (
+                  <>
+                    <Hidden until="md">
+                      <Follow
+                        onPressFollower={() => setShowBottomSheet("followers")}
+                        onPressFollowing={() => setShowBottomSheet("following")}
+                        followersCount={profileData?.data.followers_count}
+                        followingCount={profileData?.data.following_count}
+                        tw="mr-8"
+                      />
+                    </Hidden>
+                    {profileId && userId !== profileId ? (
+                      <>
+                        <ProfileDropdown user={profileData?.data.profile} />
+                        <View tw="w-2" />
+                        <Button
+                          size={width < 768 ? "small" : "regular"}
+                          onPress={() => {
+                            if (isFollowingUser) {
+                              unfollow(profileId);
+                            } else {
+                              follow(profileId);
+                            }
+                          }}
+                        >
+                          {isFollowingUser ? "Following" : "Follow"}
+                        </Button>
+                      </>
+                    ) : userId === profileId ? (
                       <Button
                         size="small"
                         onPress={() => {
@@ -207,11 +210,11 @@ export const ProfileTop = ({
                       >
                         Edit profile
                       </Button>
-                    )
-                  )}
-                </>
-              )}
-            </View>
+                    ) : null}
+                  </>
+                )}
+              </View>
+            ) : null}
           </View>
 
           <View tw="px-2 py-3" pointerEvents="box-none">
--- packages/app/hooks/api-hooks.ts
+++ packages/app/hooks/api-hooks.ts
@@ -117,7 +117,7 @@ export const useTrendingNFTS = ({ days }: { days: number }) => {
 };
 
 export const USER_PROFILE_KEY = "/v4/profile_server/";
-export const useUserProfile = ({ address }: { address?: string }) => {
+export const useUserProfile = ({ address }: { address: string | null }) => {
   const { data, error } = useSWR<{ data: UserProfile }>(
     address ? USER_PROFILE_KEY + address : null,
     fetcher
