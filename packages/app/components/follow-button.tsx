5b7be58b7ae19c13b7718c8b1c335f54db7a8c7e
alantoa <496807117@qq.com>
2022-05-09 16:53:58 +0800

    feat: follow-button components

---
+++ packages/app/components/follow-button.tsx
@@ -0,0 +1,48 @@
+import { useCallback, memo } from "react";
+
+import { useMyInfo } from "app/hooks/api-hooks";
+
+import { Button } from "design-system";
+import { useAlert } from "design-system/alert";
+import type { ButtonProps } from "design-system/button/types";
+
+type ToggleFollowParams = Pick<ButtonProps, "size"> & {
+  isFollowing: boolean | 0 | undefined;
+  name?: string;
+  profileId: number;
+};
+export const FollowButton = memo<ToggleFollowParams>(
+  ({ isFollowing, profileId, name, ...rest }) => {
+    const { unfollow, follow } = useMyInfo();
+    const Alert = useAlert();
+    const toggleFollow = useCallback(() => {
+      if (isFollowing) {
+        Alert.alert(`Unfollow ${Boolean(name) ? `@${name}` : ""}?`, "", [
+          {
+            text: "Cancel",
+            style: "cancel",
+          },
+          {
+            text: "Unfollow",
+            style: "destructive",
+            onPress: () => {
+              unfollow(profileId);
+            },
+          },
+        ]);
+      } else {
+        follow(profileId);
+      }
+    }, [isFollowing, profileId, name]);
+
+    return (
+      <Button
+        variant={isFollowing ? "tertiary" : "primary"}
+        onPress={toggleFollow}
+        {...rest}
+      >
+        {isFollowing ? "Following" : "Follow"}
+      </Button>
+    );
+  }
+);
