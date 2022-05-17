0e13d8e51680002a9157ac098554ac54cca5a78e
Enes Ozturk <enesozturk.d@gmail.com>
2022-05-17 15:49:59 +0300

    feat: add custom main provider component

---
+++ packages/app/providers/index.tsx
@@ -0,0 +1,69 @@
+import { useMemo } from "react";
+import { Platform } from "react-native";
+
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import { GrowthBookProvider } from "@growthbook/growthbook-react";
+import { DripsyProvider } from "dripsy";
+
+import { AppContext } from "app/context/app-context";
+import useColorScheme from "app/hooks/use-color-scheme";
+import { growthbook } from "app/lib/growthbook";
+import { SafeAreaProvider } from "app/lib/safe-area";
+import { NavigationProvider } from "app/navigation";
+import { AuthProvider } from "app/providers/auth-provider";
+import { FeedProvider } from "app/providers/feed-provider";
+import { MintProvider } from "app/providers/mint-provider";
+import { SWRProvider } from "app/providers/swr-provider";
+import { UserProvider } from "app/providers/user-provider";
+import { WalletProvider } from "app/providers/wallet-provider";
+import { Web3Provider } from "app/providers/web3-provider";
+
+import { AlertProvider } from "design-system/alert";
+import { SnackbarProvider } from "design-system/snackbar";
+import { tw } from "design-system/tailwind";
+import { theme } from "design-system/theme";
+import { ToastProvider } from "design-system/toast";
+
+export const AppProvider = ({ children }) => {
+  const { injectedGlobalColorContext } = useColorScheme();
+  const isWeb = Platform.OS === "web";
+
+  const safeAreaProviderStyles = useMemo(
+    () => (isWeb ? tw.style("bg-black") : {}),
+    [isWeb]
+  );
+
+  return (
+    <DripsyProvider theme={theme} ssr={isWeb}>
+      <SafeAreaProvider style={safeAreaProviderStyles}>
+        <ToastProvider>
+          <AlertProvider>
+            <SnackbarProvider>
+              <NavigationProvider>
+                <SWRProvider>
+                  <WalletProvider>
+                    <Web3Provider>
+                      <AppContext.Provider value={injectedGlobalColorContext}>
+                        <AuthProvider>
+                          <UserProvider>
+                            <BottomSheetModalProvider>
+                              <GrowthBookProvider growthbook={growthbook}>
+                                <FeedProvider>
+                                  <MintProvider>{children}</MintProvider>
+                                </FeedProvider>
+                              </GrowthBookProvider>
+                            </BottomSheetModalProvider>
+                          </UserProvider>
+                        </AuthProvider>
+                      </AppContext.Provider>
+                    </Web3Provider>
+                  </WalletProvider>
+                </SWRProvider>
+              </NavigationProvider>
+            </SnackbarProvider>
+          </AlertProvider>
+        </ToastProvider>
+      </SafeAreaProvider>
+    </DripsyProvider>
+  );
+};
