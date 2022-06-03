51fbd6c58b72f788418a2a7016802c2a1f80b366
alantoa <496807117@qq.com>
2022-06-03 16:39:26 +0800

    fix: footer keys warning.

--- packages/app/components/footer/links-footer.web.tsx
+++ packages/app/components/footer/links-footer.web.tsx
@@ -1,3 +1,5 @@
+import { memo } from "react";
+
 import { tw } from "@showtime-xyz/universal.tailwind";
 import { Text } from "@showtime-xyz/universal.text";
 import { View } from "@showtime-xyz/universal.view";
@@ -7,14 +9,13 @@ import { Link } from "app/navigation/link";
 
 import ShowtimeWordmark from "design-system/icon/ShowtimeWordmark";
 
-export const WebFooter = () => {
+export const WebFooter = memo(function WebFooter() {
   const { social, links } = useFooter();
-
   return (
     <View tw="w-full items-center bg-white dark:bg-black">
       <View tw="w-full max-w-screen-2xl flex-col-reverse justify-between p-4 md:flex-row md:p-12 ">
         <View tw="mt-4 md:mt-0">
-          <Link href="/">
+          <Link href="/" key="ShowtimeWordmark">
             <ShowtimeWordmark
               color={tw.style("text-gray-900 dark:text-white").color as string}
               height={24}
@@ -36,40 +37,40 @@ export const WebFooter = () => {
                 }}
                 key={item.title}
               >
-                <Text tw="text-13 mb-4 mt-0 font-semibold text-gray-900 dark:text-white">
+                <Text
+                  key={item.title}
+                  tw="text-13 mb-4 mt-0 font-semibold text-gray-900 dark:text-white"
+                >
                   {item.title}
                 </Text>
               </Link>
             ))}
           </View>
           <View tw="flex flex-col">
-            {social.map((item) => (
-              <>
-                <Link
-                  tw="flex-row items-center"
-                  href={item.link}
-                  hrefAttrs={{
-                    target: "_blank",
-                    rel: "noreferrer",
-                  }}
-                  key={item.title}
-                >
-                  <View tw="mr-2 text-base">
-                    {item.icon({
-                      color: tw.style("text-gray-900 dark:text-white")
-                        .color as string,
-                    })}
-                  </View>
-                  <Text tw="text-13 font-semibold text-gray-900 dark:text-white">
-                    {item.title}
-                  </Text>
-                </Link>
-                <View tw="h-6" />
-              </>
+            {social.map((item, i) => (
+              <Link
+                tw="mb-6 flex-row items-center"
+                href={item.link}
+                hrefAttrs={{
+                  target: "_blank",
+                  rel: "noreferrer",
+                }}
+                key={item.title}
+              >
+                <View tw="mr-2 text-base">
+                  {item.icon({
+                    color: tw.style("text-gray-900 dark:text-white")
+                      .color as string,
+                  })}
+                </View>
+                <Text tw="text-13 font-semibold text-gray-900 dark:text-white">
+                  {item.title}
+                </Text>
+              </Link>
             ))}
           </View>
         </View>
       </View>
     </View>
   );
-};
+});
