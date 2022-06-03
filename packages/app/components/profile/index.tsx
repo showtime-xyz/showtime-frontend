7c609501878d7d995289fe462f3ac4ef1158d9f5
alantoa <496807117@qq.com>
2022-06-03 21:40:18 +0800

    refactor: profile screen.

--- packages/app/components/profile/index.tsx
+++ packages/app/components/profile/index.tsx
@@ -1,35 +1,36 @@
-import { Suspense, useCallback, useReducer } from "react";
-import { Platform } from "react-native";
+import { useCallback, useReducer, useMemo, Suspense, useRef } from "react";
+import { Platform, useWindowDimensions } from "react-native";
+
+import { SceneRendererProps } from "react-native-tab-view";
 
 import { tw } from "@showtime-xyz/universal.tailwind";
 import { View } from "@showtime-xyz/universal.view";
 
-import { ErrorBoundary } from "app/components/error-boundary";
 import {
   defaultFilters,
   useProfileNftTabs,
   useUserProfile,
 } from "app/hooks/api-hooks";
 import { useBlock } from "app/hooks/use-block";
-import { TAB_LIST_HEIGHT } from "app/lib/constants";
-import { Haptics } from "app/lib/haptics";
 import { useHeaderHeight } from "app/lib/react-navigation/elements";
-import { createParam } from "app/navigation/use-param";
 
-import { Skeleton, Spinner } from "design-system";
-import { useColorScheme } from "design-system/hooks";
-import { SelectedTabIndicator, TabItem, Tabs } from "design-system/tabs";
+import { Spinner } from "design-system";
+import { useIsDarkMode } from "design-system/hooks";
+import { HeaderTabView, useTabState } from "design-system/tab-view/index";
+import { HeaderTabViewRef } from "design-system/tab-view/src/index.web";
+import { Route } from "design-system/tab-view/src/types";
 
+import useContentWidth from "../../hooks/use-content-width";
+import { ErrorBoundary } from "../error-boundary";
 import { FilterContext } from "./fillter-context";
 import { ProfileListFilter } from "./profile-tab-filter";
-import { ProfileTabList } from "./profile-tab-list";
+import { ProfileTabList, ProfileTabListRef } from "./profile-tab-list";
 import { ProfileTop } from "./profile-top";
 
-type Query = {
-  tab: number;
-};
-
-const { useParam } = createParam<Query>();
+const HEADER_LIGHT_SHADOW =
+  "0px 2px 4px rgba(0, 0, 0, 0.05), 0px 4px 8px rgba(0, 0, 0, 0.05)";
+const HEADER_DARK_SHADOW =
+  "0px 0px 2px rgba(255, 255, 255, 0.5), 0px 8px 16px rgba(255, 255, 255, 0.1)";
 
 const ProfileScreen = ({ username }: { username: string | null }) => {
   return <Profile address={username} />;
@@ -37,18 +38,22 @@ const ProfileScreen = ({ username }: { username: string | null }) => {
 
 const Profile = ({ address }: { address: string | null }) => {
   const { data: profileData } = useUserProfile({ address });
-  const { data, loading: tabsLoading } = useProfileNftTabs({
+  const { width } = useWindowDimensions();
+  const isDark = useIsDarkMode();
+  const contentWidth = useContentWidth();
+  const {
+    data,
+    loading: tabsLoading,
+    error,
+  } = useProfileNftTabs({
     profileId: profileData?.data?.profile.profile_id,
   });
-  const [selected, setSelected] = useParam("tab", {
-    parse: (v) => Number(v ?? 0),
-    initial: 0,
-  });
+
+  const { index, setIndex, setIsRefreshing, isRefreshing } = useTabState([]);
 
   const { getIsBlocked } = useBlock();
   const isBlocked = getIsBlocked(profileData?.data?.profile.profile_id);
 
-  const colorScheme = useColorScheme();
   const headerHeight = useHeaderHeight();
   const [filter, dispatch] = useReducer(
     (state: any, action: any) => {
@@ -74,106 +79,132 @@ const Profile = ({ address }: { address: string | null }) => {
     },
     [dispatch]
   );
+  const tabRefs = useRef<ProfileTabListRef[]>([]);
+  const headerTabViewRef = useRef<HeaderTabViewRef>(null);
+  const onStartRefresh = useCallback(() => {
+    setIsRefreshing(true);
+    tabRefs.current[index]?.refresh();
+    setIsRefreshing(false);
+  }, [index, setIsRefreshing]);
+  const renderScene = useCallback(
+    ({
+      route: { index },
+    }: SceneRendererProps & {
+      route: Route;
+    }) => {
+      const list = data?.data.lists[index];
+      if (!list) return null;
+      return (
+        <ErrorBoundary>
+          <Suspense
+            fallback={
+              <View tw="items-center justify-center pt-20">
+                <Spinner size="small" />
+              </View>
+            }
+          >
+            <ProfileTabList
+              username={profileData?.data.profile.username}
+              profileId={profileData?.data.profile.profile_id}
+              isBlocked={isBlocked}
+              list={list}
+              index={index}
+              ref={(ref) => ref && (tabRefs.current[index] = ref)}
+            />
+          </Suspense>
+        </ErrorBoundary>
+      );
+    },
+    [
+      data?.data.lists,
+      isBlocked,
+      profileData?.data.profile.profile_id,
+      profileData?.data.profile.username,
+    ]
+  );
+  const headerBgLeft = useMemo(() => {
+    return Math.min(-(width - contentWidth) / 2, 0);
+  }, [contentWidth, width]);
 
-  const handleTabOnPress = useCallback(() => {
-    Haptics.impactAsync();
-  }, []);
+  const headerShadow = useMemo(() => {
+    return isDark ? HEADER_DARK_SHADOW : HEADER_LIGHT_SHADOW;
+  }, [isDark]);
 
+  const renderHeader = useCallback(() => {
+    return (
+      <View tw="items-center">
+        {Platform.OS === "web" && (
+          <View
+            tw="absolute left-0 h-full w-screen bg-white dark:bg-black"
+            style={{
+              left: headerBgLeft,
+              height: `calc(100% + 44px)`,
+              // @ts-ignore
+              boxShadow: headerShadow,
+            }}
+          />
+        )}
+        <View tw="web:max-w-screen-xl w-full">
+          {Platform.OS === "ios" && <View tw={`h-[${headerHeight}px]`} />}
+          <ProfileTop address={address} isBlocked={isBlocked} />
+        </View>
+      </View>
+    );
+  }, [headerBgLeft, headerShadow, headerHeight, address, isBlocked]);
+  const routes = useMemo(
+    () =>
+      data?.data?.lists?.map((item, index) => ({
+        title: item?.name,
+        key: item?.name,
+        index,
+      })) ?? [],
+    [data?.data?.lists]
+  );
   return (
     <FilterContext.Provider value={{ filter, dispatch }}>
-      <View tw="web:items-center w-full flex-1 overflow-hidden">
-        <Tabs.Root
-          onIndexChange={setSelected}
-          index={selected}
-          tabListHeight={TAB_LIST_HEIGHT}
-          lazy
-        >
-          <Tabs.Header>
-            <View tw="z-10 items-center bg-white dark:bg-black">
-              <View tw="web:max-w-screen-xl w-full">
-                {Platform.OS === "ios" && <View tw={`h-[${headerHeight}px]`} />}
-                <ProfileTop address={address} isBlocked={isBlocked} />
-                {Platform.OS === "web" && (
-                  <View tw="-bottom-26 absolute w-full justify-between md:right-10 md:-bottom-10 md:w-auto">
-                    <ProfileListFilter
-                      onCollectionChange={onCollectionChange}
-                      onSortChange={onSortChange}
-                      collectionId={filter.collectionId}
-                      collections={
-                        data?.data?.lists[selected]?.collections || []
-                      }
-                      sortId={filter.sortId}
-                    />
-                  </View>
-                )}
-              </View>
-            </View>
-          </Tabs.Header>
-
-          {data?.data.lists ? (
-            <>
-              <Tabs.List
-                onPressCallback={handleTabOnPress}
-                style={tw.style(
-                  `h-[${TAB_LIST_HEIGHT}px] w-full self-center bg-white dark:bg-black`
-                )}
-              >
-                {data?.data.lists.map((list, index) => (
-                  <Tabs.Trigger key={list.id}>
-                    <TabItem name={list.name} selected={selected === index} />
-                  </Tabs.Trigger>
-                ))}
-                <SelectedTabIndicator />
-              </Tabs.List>
-
-              <Tabs.Pager>
-                {data?.data.lists.map((list) => {
-                  return (
-                    <ErrorBoundary key={list.id}>
-                      <Suspense
-                        fallback={
-                          <View tw="items-center justify-center pt-20">
-                            <Spinner size="small" />
-                          </View>
-                        }
-                        key={list.id}
-                      >
-                        <ProfileTabList
-                          username={profileData?.data.profile.username}
-                          profileId={profileData?.data.profile.profile_id}
-                          isBlocked={isBlocked}
-                          list={list}
-                        />
-                      </Suspense>
-                    </ErrorBoundary>
-                  );
-                })}
-              </Tabs.Pager>
-            </>
-          ) : tabsLoading ? (
-            <Tabs.List
-              style={tw.style(
-                `h-[${TAB_LIST_HEIGHT}px] dark:bg-black bg-white border-b border-b-gray-100 dark:border-b-gray-900 ml-4 mt-4`
-              )}
-            >
-              <Tabs.Trigger>
-                <View tw="w-22">
-                  <Skeleton colorMode={colorScheme} width={74} height={20} />
+      <View style={{ width: contentWidth }} tw="flex-1">
+        <HeaderTabView
+          onStartRefresh={onStartRefresh}
+          isRefreshing={isRefreshing}
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          renderScrollHeader={renderHeader}
+          minHeaderHeight={headerHeight}
+          refreshControlTop={headerHeight}
+          initialLayout={{
+            width: contentWidth,
+          }}
+          style={tw.style("z-1")}
+          insertStickyTabBarElement={
+            Platform.OS === "web" ? (
+              <View
+                tw="absolute left-0 top-0 h-full w-screen bg-white dark:bg-black"
+                style={{
+                  left: headerBgLeft,
+                  // @ts-ignore
+                  boxShadow: headerShadow,
+                }}
+              />
+            ) : null
+          }
+          insertTabBarElement={
+            Platform.OS === "web" ? (
+              <>
+                <View tw="absolute -bottom-11 w-full justify-between md:bottom-1.5 md:right-10 md:w-auto">
+                  <ProfileListFilter
+                    onCollectionChange={onCollectionChange}
+                    onSortChange={onSortChange}
+                    collectionId={filter.collectionId}
+                    collections={data?.data?.lists[index]?.collections || []}
+                    sortId={filter.sortId}
+                  />
                 </View>
-              </Tabs.Trigger>
-              <Tabs.Trigger>
-                <View tw="w-22">
-                  <Skeleton colorMode={colorScheme} width={74} height={20} />
-                </View>
-              </Tabs.Trigger>
-              <Tabs.Trigger>
-                <View tw="w-20">
-                  <Skeleton colorMode={colorScheme} width={70} height={20} />
-                </View>
-              </Tabs.Trigger>
-            </Tabs.List>
-          ) : null}
-        </Tabs.Root>
+              </>
+            ) : null
+          }
+          ref={headerTabViewRef}
+        />
       </View>
     </FilterContext.Provider>
   );
--- packages/app/components/profile/profile-tab-list.tsx
+++ packages/app/components/profile/profile-tab-list.tsx
@@ -1,4 +1,11 @@
-import { useCallback, useContext, useMemo } from "react";
+import {
+  useCallback,
+  useContext,
+  useMemo,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+} from "react";
 import { useWindowDimensions } from "react-native";
 
 import { Text } from "@showtime-xyz/universal.text";
@@ -17,7 +24,7 @@ import { Spinner } from "design-system";
 import { Card } from "design-system/card";
 import { Hidden } from "design-system/hidden";
 import { getLocalFileURI } from "design-system/preview";
-import { Tabs } from "design-system/tabs";
+import { TabRecyclerList } from "design-system/tab-view";
 
 import { FilterContext } from "./fillter-context";
 import { ProfileFooter } from "./footer";
@@ -28,211 +35,217 @@ type TabListProps = {
   profileId?: number;
   isBlocked?: boolean;
   list: List;
+  index: number;
 };
 
 const GAP_BETWEEN_ITEMS = 1;
-
-export const ProfileTabList = ({
-  username,
-  profileId,
-  isBlocked,
-  list,
-}: TabListProps) => {
-  const router = useRouter();
-  const { user } = useUser();
-  const { state: mintingState } = useContext(MintContext);
-  const { width, height } = useWindowDimensions();
-
-  const { filter, dispatch } = useContext(FilterContext);
-
-  const {
-    isLoading,
-    data,
-    fetchMore,
-    isRefreshing,
-    refresh,
-    updateItem,
-    isLoadingMore,
-  } = useProfileNFTs({
-    listId: list.id,
-    profileId,
-    collectionId: filter.collectionId,
-    sortId: filter.sortId,
-    // TODO: remove refresh interval once we have the new indexer.
-    refreshInterval: 2000,
-  });
-
-  const onCollectionChange = useCallback(
-    (value: number | string) => {
-      dispatch({ type: "collection_change", payload: value });
-    },
-    [dispatch]
-  );
-
-  const onSortChange = useCallback(
-    (value: number | string) => {
-      dispatch({ type: "sort_change", payload: value });
-    },
-    [dispatch]
-  );
-
-  const onItemPress = useCallback(
-    (nftId: number) => {
-      const index = data.findIndex((v) => v.nft_id === nftId);
-      router.push(
-        `/list?initialScrollIndex=${index}&listId=${list.id}&profileId=${profileId}&collectionId=${filter.collectionId}&sortId=${filter.sortId}&type=profile`
-      );
-    },
-    [list.id, profileId, filter.collectionId, filter.sortId, router, data]
-  );
-
-  const ListFooterComponent = useCallback(
-    () => <ProfileFooter isLoading={isLoadingMore} />,
-    [isLoadingMore]
-  );
-
-  const ListHeaderComponent = useCallback(
-    () => (
-      <View tw="p-4">
-        <Hidden platform="web">
-          <ProfileListFilter
-            onCollectionChange={onCollectionChange}
-            onSortChange={onSortChange}
-            collectionId={filter.collectionId}
-            collections={list.collections}
-            sortId={filter.sortId}
-          />
-        </Hidden>
-        {isBlocked ? (
-          <View tw="mt-8 items-center justify-center">
-            <Text tw="text-gray-900 dark:text-white">
-              <Text tw="font-bold">@{username}</Text> is blocked
-            </Text>
-          </View>
-        ) : data.length === 0 && !isLoading ? (
-          <View tw="mt-20 items-center justify-center">
-            <Text tw="text-gray-900 dark:text-white">No results found</Text>
-          </View>
-        ) : isLoading ? (
-          <View tw="mt-20 items-center justify-center">
-            <Spinner />
-          </View>
-        ) : null}
-      </View>
-    ),
-    [
-      data,
-      username,
-      isLoading,
-      filter,
-      onCollectionChange,
-      onSortChange,
-      list.collections,
-      isBlocked,
-    ]
-  );
-
-  const newData = useMemo(() => {
-    let newData: any = ["header"];
-    if (isBlocked) return newData;
-    if (
-      mintingState.loading &&
-      mintingState.tokenId !== data?.[0]?.token_id &&
-      profileId === user?.data.profile.profile_id
-    ) {
-      //@ts-ignore
-      newData.push({
-        loading: true,
-        chain_name: "polygon",
-        contract_address: "0x8a13628dd5d600ca1e8bf9dbc685b735f615cb90",
-        token_id: mintingState.tokenId ?? "1",
-        source_url: getLocalFileURI(mintingState.file),
-        mime_type: mintingState.fileType ?? "image/jpeg",
-      });
-    }
-
-    newData = newData.concat(data);
-
-    return newData;
-  }, [
-    data,
-    mintingState.loading,
-    mintingState.tokenId,
-    mintingState.file,
-    mintingState.fileType,
-    isBlocked,
-    profileId,
-    user?.data.profile.profile_id,
-  ]);
-
-  const headerHeight = useMemo(
-    () => (isBlocked ? 80 : width < 768 ? 80 : 32),
-    [width, isBlocked]
-  );
-  const _layoutProvider = useNFTCardsListLayoutProvider({
-    newData,
-    headerHeight,
-  });
-
-  const dataProvider = useMemo(
-    () =>
-      new DataProvider((r1, r2) => {
-        return typeof r1 === "string" && typeof r2 === "string"
-          ? r1 !== r2
-          : r1.nft_id !== r2.nft_id;
-      }).cloneWithRows(newData),
-    [newData]
-  );
-  const contentWidth = useContentWidth();
-
-  const layoutSize = useMemo(
-    () => ({
-      width: contentWidth,
-      height,
-    }),
-    [contentWidth, height]
-  );
-  const _rowRenderer = useCallback(
-    (_type: any, item: any) => {
-      if (_type === "header") {
-        return <ListHeaderComponent />;
-      }
-
-      // currently minting nft
-      if (item.loading) {
-        return <Card nft={item} numColumns={3} />;
-      }
-
-      return (
-        // index - 1 because header takes the initial index!
-        <Card
-          nft={item}
-          numColumns={3}
-          listId={list.id}
-          onPress={() => onItemPress(item.nft_id)}
-          hrefProps={{
-            pathname: `/nft/${item.chain_name}/${item.contract_address}/${item.token_id}`,
-          }}
-        />
-      );
-    },
-    [list.id, ListHeaderComponent, onItemPress]
-  );
-
-  return (
-    <MutateProvider mutate={updateItem}>
-      <Tabs.RecyclerList
-        //@ts-ignore
-        layoutProvider={_layoutProvider}
-        dataProvider={dataProvider}
-        rowRenderer={_rowRenderer}
-        onEndReached={fetchMore}
-        refreshing={isRefreshing}
-        onRefresh={refresh}
-        style={{ flex: 1, margin: -GAP_BETWEEN_ITEMS }}
-        renderFooter={ListFooterComponent}
-        layoutSize={layoutSize}
-      />
-    </MutateProvider>
-  );
+export type ProfileTabListRef = {
+  refresh: () => void;
 };
+export const ProfileTabList = forwardRef<ProfileTabListRef, TabListProps>(
+  function ProfileTabList(
+    { username, profileId, isBlocked, list, index },
+    ref
+  ) {
+    const router = useRouter();
+    const { user } = useUser();
+    const { state: mintingState } = useContext(MintContext);
+    const { width, height } = useWindowDimensions();
+
+    const { filter, dispatch } = useContext(FilterContext);
+
+    const { isLoading, data, fetchMore, refresh, updateItem, isLoadingMore } =
+      useProfileNFTs({
+        listId: list.id,
+        profileId,
+        collectionId: filter.collectionId,
+        sortId: filter.sortId,
+        // TODO: remove refresh interval once we have the new indexer.
+        refreshInterval: 5000,
+      });
+    const tabRef = useRef(null);
+
+    useImperativeHandle(
+      ref,
+      () => ({
+        refresh,
+      }),
+      [refresh]
+    );
+    const onCollectionChange = useCallback(
+      (value: number | string) => {
+        dispatch({ type: "collection_change", payload: value });
+      },
+      [dispatch]
+    );
+
+    const onSortChange = useCallback(
+      (value: number | string) => {
+        dispatch({ type: "sort_change", payload: value });
+      },
+      [dispatch]
+    );
+
+    const onItemPress = useCallback(
+      (nftId: number) => {
+        const index = data.findIndex((v) => v.nft_id === nftId);
+        router.push(
+          `/list?initialScrollIndex=${index}&listId=${list.id}&profileId=${profileId}&collectionId=${filter.collectionId}&sortId=${filter.sortId}&type=profile`
+        );
+      },
+      [list.id, profileId, filter.collectionId, filter.sortId, router, data]
+    );
+
+    const ListFooterComponent = useCallback(
+      () => <ProfileFooter isLoading={isLoadingMore} />,
+      [isLoadingMore]
+    );
+
+    const ListHeaderComponent = useCallback(
+      () => (
+        <View tw="p-4">
+          <Hidden platform="web">
+            <ProfileListFilter
+              onCollectionChange={onCollectionChange}
+              onSortChange={onSortChange}
+              collectionId={filter.collectionId}
+              collections={list.collections}
+              sortId={filter.sortId}
+            />
+          </Hidden>
+          {isBlocked ? (
+            <View tw="mt-8 items-center justify-center">
+              <Text tw="text-gray-900 dark:text-white">
+                <Text tw="font-bold">@{username}</Text> is blocked
+              </Text>
+            </View>
+          ) : data.length === 0 && !isLoading ? (
+            <View tw="mt-20 items-center justify-center">
+              <Text tw="text-gray-900 dark:text-white">No results found</Text>
+            </View>
+          ) : isLoading ? (
+            <View tw="mt-20 items-center justify-center">
+              <Spinner />
+            </View>
+          ) : null}
+        </View>
+      ),
+      [
+        data,
+        username,
+        isLoading,
+        filter,
+        onCollectionChange,
+        onSortChange,
+        list.collections,
+        isBlocked,
+      ]
+    );
+
+    const newData = useMemo(() => {
+      let newData: any = ["header"];
+      if (isBlocked) return newData;
+      if (
+        mintingState.loading &&
+        mintingState.tokenId !== data?.[0]?.token_id &&
+        profileId === user?.data.profile.profile_id
+      ) {
+        //@ts-ignore
+        newData.push({
+          loading: true,
+          chain_name: "polygon",
+          contract_address: "0x8a13628dd5d600ca1e8bf9dbc685b735f615cb90",
+          token_id: mintingState.tokenId ?? "1",
+          source_url: getLocalFileURI(mintingState.file),
+          mime_type: mintingState.fileType ?? "image/jpeg",
+        });
+      }
+
+      newData = newData.concat(data);
+
+      return newData;
+    }, [
+      data,
+      mintingState.loading,
+      mintingState.tokenId,
+      mintingState.file,
+      mintingState.fileType,
+      isBlocked,
+      profileId,
+      user?.data.profile.profile_id,
+    ]);
+
+    const headerHeight = useMemo(
+      () => (isBlocked ? 80 : width < 768 ? 80 : 32),
+      [width, isBlocked]
+    );
+    const _layoutProvider = useNFTCardsListLayoutProvider({
+      newData,
+      headerHeight,
+    });
+
+    const dataProvider = useMemo(
+      () =>
+        new DataProvider((r1, r2) => {
+          return typeof r1 === "string" && typeof r2 === "string"
+            ? r1 !== r2
+            : r1.nft_id !== r2.nft_id;
+        }).cloneWithRows(newData),
+      [newData]
+    );
+    const contentWidth = useContentWidth();
+
+    const layoutSize = useMemo(
+      () => ({
+        width: contentWidth,
+        height,
+      }),
+      [contentWidth, height]
+    );
+    const _rowRenderer = useCallback(
+      (_type: any, item: any) => {
+        if (_type === "header") {
+          return <ListHeaderComponent />;
+        }
+
+        // currently minting nft
+        if (item.loading) {
+          return <Card nft={item} numColumns={3} />;
+        }
+
+        return (
+          // index - 1 because header takes the initial index!
+          <Card
+            nft={item}
+            numColumns={3}
+            listId={list.id}
+            onPress={() => onItemPress(item.nft_id)}
+            hrefProps={{
+              pathname: `/nft/${item.chain_name}/${item.contract_address}/${item.token_id}`,
+            }}
+          />
+        );
+      },
+      [list.id, ListHeaderComponent, onItemPress]
+    );
+
+    return (
+      <MutateProvider mutate={updateItem}>
+        <TabRecyclerList
+          layoutProvider={_layoutProvider}
+          dataProvider={dataProvider}
+          rowRenderer={_rowRenderer}
+          onEndReached={fetchMore}
+          style={{
+            flex: 1,
+            margin: -GAP_BETWEEN_ITEMS,
+          }}
+          renderFooter={ListFooterComponent}
+          layoutSize={layoutSize}
+          index={index}
+          ref={tabRef}
+        />
+      </MutateProvider>
+    );
+  }
+);
