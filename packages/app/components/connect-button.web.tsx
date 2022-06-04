a63305786b22adb84d270e310789ad4f2b8a463b
Enes Ozturk <enesozturk.d@gmail.com>
2022-06-04 19:00:28 +0300

    chore: use universal package

--- packages/app/components/connect-button.web.tsx
+++ packages/app/components/connect-button.web.tsx
@@ -2,7 +2,9 @@ import { useWindowDimensions } from "react-native";
 
 import { ConnectButton as RainbowConnectButton } from "@rainbow-me/rainbowkit";
 
-import { Button, View } from "design-system";
+import { View } from "@showtime-xyz/universal.view";
+
+import { Button } from "design-system/button";
 import { useIsDarkMode } from "design-system/hooks";
 import { Alert } from "design-system/icon";
 
