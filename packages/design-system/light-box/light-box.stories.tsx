99a6bae041aab8e6ff861dbf1e3095c00ba9562a
alantoa <toacncom@gmail.com>
2022-06-20 20:52:28 +0800

    deps: upgrade packages

--- packages/design-system/light-box/light-box.stories.tsx
+++ packages/design-system/light-box/light-box.stories.tsx
@@ -17,8 +17,8 @@ const LightBoxBasic = () => {
   return (
     <View tw="flex-1 items-center justify-center">
       <LightBox
-        width={width / 3}
-        height={width / 3}
+        width={120}
+        height={120}
         imgLayout={{ width, height: width }}
         tapToClose
       >
@@ -26,6 +26,8 @@ const LightBoxBasic = () => {
           source={{
             uri: "https://storage.googleapis.com/showtime-test/cdnv2/ethereum/0x495f947276749ce646f68ac8c248420045cb7b5e/114091279957330982022734478648167430968491445987545800607311341400684004114433",
           }}
+          width={120}
+          height={120}
           style={StyleSheet.absoluteFillObject}
         />
       </LightBox>
