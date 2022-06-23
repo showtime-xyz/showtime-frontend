66e57117a08c55e3bab4b6ac98c363cfe3624b0d
nishan (o^▽^o) <nishanbende@gmail.com>
2022-06-23 11:55:40 +0530

    fix: profile metatags (#1211)

--- apps/next-react-native/src/pages/nft/[chainName]/[contractAddress]/[tokenId]/index.tsx
+++ apps/next-react-native/src/pages/nft/[chainName]/[contractAddress]/[tokenId]/index.tsx
@@ -7,25 +7,27 @@ import { getMediaUrl } from "app/utilities";
 
 export async function getServerSideProps(context) {
   const { chainName, contractAddress, tokenId } = context.params;
-  const res = await axios.get(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/v2/token/${contractAddress}/${tokenId}?chain_identifier=${CHAIN_IDENTIFIERS[chainName]}`
-  );
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/v2/token/${contractAddress}/${tokenId}?chain_identifier=${CHAIN_IDENTIFIERS[chainName]}`
+    );
 
-  const nft = res.data?.data?.item as NFT;
-  const imageUrl = getMediaUrl({ nft, stillPreview: false });
-  if (nft) {
-    return {
-      props: {
-        meta: {
-          title: nft.token_name,
-          description: nft.token_description,
-          image: imageUrl,
+    const nft = res.data?.data?.item as NFT;
+    const imageUrl = getMediaUrl({ nft, stillPreview: false });
+    if (nft) {
+      return {
+        props: {
+          meta: {
+            title: nft.token_name,
+            description: nft.token_description,
+            image: imageUrl,
+          },
         },
-      },
-    };
-  }
+      };
+    }
+  } catch (e) {}
 
-  return null;
+  return {};
 }
 
 export default NftScreen;
