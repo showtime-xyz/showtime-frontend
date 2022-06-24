6be751a284ab0f1eb8dca8ee0e774ef670c924e0
Enes Ozturk <enesozturk.d@gmail.com>
2022-06-24 09:56:47 +0300

    fix: activity user item image styles

--- packages/app/components/nft-activity/user-item.tsx
+++ packages/app/components/nft-activity/user-item.tsx
@@ -5,13 +5,29 @@ import { View } from "@showtime-xyz/universal.view";
 
 import type { UserItemProps } from "./nft-activity.types";
 
+const getProfileImageUrl = (imgUrl: string) => {
+  if (imgUrl && imgUrl.includes("https://lh3.googleusercontent.com")) {
+    imgUrl = imgUrl.split("=")[0] + "=s112";
+  }
+  return imgUrl;
+};
+
 const UserItem = ({ imageUrl, title, verified }: UserItemProps) => {
   return (
     <View tw="flex flex-row items-center">
-      <View tw="mr-2 h-6 w-6 rounded-xl bg-gray-200">
-        {imageUrl && (
-          <Image tw={"h-full w-full rounded-full"} source={{ uri: imageUrl }} />
-        )}
+      <View
+        tw={`mr-2 h-6 w-6 overflow-hidden rounded-full ${
+          imageUrl ? "" : "bg-white"
+        }`}
+      >
+        {imageUrl ? (
+          <Image
+            width={24}
+            height={24}
+            tw="h-full w-full"
+            source={{ uri: getProfileImageUrl(imageUrl) }}
+          />
+        ) : null}
       </View>
       <Text tw={"mr-1 text-xs text-black dark:text-white"}>{`@${title}`}</Text>
       {verified ? <VerificationBadge size={12} /> : null}
