8bf4b9ec16bde81a474a91996def77692791e082
alantoa <toacncom@gmail.com>
2022-06-28 16:32:14 +0800

    refactor: Spinner uses moti instead

--- packages/design-system/spinner/index.web.tsx
+++ packages/design-system/spinner/index.web.tsx
@@ -1,45 +1,28 @@
-import { useEffect, useCallback, useMemo } from "react";
-import { Animated, Easing } from "react-native";
+import { Easing } from "react-native";
+
+import { View as MotiView } from "moti";
 
 import { getSpinnerSize, SpinnerView, SpinnerProps } from "./spinner-view";
 
 export const Spinner = ({ size, duration = 750, ...rest }: SpinnerProps) => {
-  const transition = useMemo(() => new Animated.Value(0), []);
-  const startAnimation = useCallback(() => {
-    Animated.timing(transition, {
-      toValue: 1,
-      duration,
-      useNativeDriver: true,
-      easing: Easing.linear,
-    }).start(() => {
-      transition.setValue(0);
-      startAnimation();
-    });
-  }, [duration, transition]);
-
-  useEffect(() => {
-    startAnimation();
-  }, [startAnimation]);
-
-  const animatedStyle = useMemo(
-    () => ({
-      height: getSpinnerSize(size),
-      width: getSpinnerSize(size),
-      transform: [
-        {
-          rotate: transition.interpolate({
-            inputRange: [0, 1],
-            outputRange: ["0deg", "360deg"],
-          }),
-        },
-      ],
-    }),
-    [size, transition]
-  );
-
   return (
-    <Animated.View style={animatedStyle} accessibilityRole="progressbar">
+    <MotiView
+      from={{ rotate: "0deg" }}
+      animate={{ rotate: "360deg" }}
+      transition={{
+        type: "timing",
+        loop: true,
+        repeatReverse: false,
+        easing: Easing.linear,
+        duration,
+      }}
+      style={{
+        height: getSpinnerSize(size),
+        width: getSpinnerSize(size),
+      }}
+      accessibilityRole="progressbar"
+    >
       <SpinnerView size={size} {...rest} />
-    </Animated.View>
+    </MotiView>
   );
 };
