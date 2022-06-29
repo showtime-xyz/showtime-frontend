35ce14a7da20973ee037a18f9fb22712543643c0
Nishan <nishanbende@gmail.com>
2022-06-29 19:48:41 +0530

    wip

--- packages/design-system/typography/index.tsx
+++ packages/design-system/typography/index.tsx
@@ -33,13 +33,25 @@ const capsize = (options: Parameters<typeof precomputeValues>[0]) => {
   } as const;
 };
 
-// Sourced from https://seek-oss.github.io/capsize
-const fontMetrics = {
+// Sourced from @capsizecss/metrics
+const fontMetricsInter = {
+  familyName: "Inter",
   capHeight: 2048,
   ascent: 2728,
   descent: -680,
   lineGap: 0,
   unitsPerEm: 2816,
+  xHeight: 1536,
+};
+
+const fontMetricsSpaceGrotesk = {
+  familyName: "Space Grotesk",
+  capHeight: 700,
+  ascent: 984,
+  descent: -292,
+  lineGap: 0,
+  unitsPerEm: 1000,
+  xHeight: 486,
 };
 
 const createTextSize = ({
@@ -47,10 +59,12 @@ const createTextSize = ({
   lineHeight: leading,
   letterSpacing,
   marginCorrection,
+  fontMetrics,
 }: {
   fontSize: number;
   lineHeight: number;
   letterSpacing: number;
+  fontMetrics: any;
   marginCorrection: {
     ios: number;
     android: number;
@@ -94,6 +108,7 @@ export const textSizes = {
       android: -0.1,
       ios: -0.3,
     },
+    fontMetrics: fontMetricsInter,
   }),
   "text-13": createTextSize({
     fontSize: 13,
@@ -103,6 +118,7 @@ export const textSizes = {
       android: -0.1,
       ios: -0.3,
     },
+    fontMetrics: fontMetricsInter,
   }),
   "text-sm": createTextSize({
     fontSize: 14,
@@ -112,6 +128,7 @@ export const textSizes = {
       android: -0.1,
       ios: -0.3,
     },
+    fontMetrics: fontMetricsInter,
   }),
   "text-base": createTextSize({
     fontSize: 16,
@@ -121,6 +138,7 @@ export const textSizes = {
       android: -0.1,
       ios: -0.5,
     },
+    fontMetrics: fontMetricsInter,
   }),
   "text-lg": createTextSize({
     fontSize: 18,
@@ -130,6 +148,7 @@ export const textSizes = {
       android: 0.2,
       ios: 0,
     },
+    fontMetrics: fontMetricsSpaceGrotesk,
   }),
   "text-xl": createTextSize({
     fontSize: 20,
@@ -139,6 +158,7 @@ export const textSizes = {
       android: 0,
       ios: -0.5,
     },
+    fontMetrics: fontMetricsInter,
   }),
   "text-2xl": createTextSize({
     fontSize: 24,
@@ -148,6 +168,7 @@ export const textSizes = {
       android: -0.3,
       ios: -0.3,
     },
+    fontMetrics: fontMetricsSpaceGrotesk,
   }),
   "text-3xl": createTextSize({
     fontSize: 30,
@@ -157,6 +178,7 @@ export const textSizes = {
       android: -0.3,
       ios: -0.3,
     },
+    fontMetrics: fontMetricsInter,
   }),
   "text-4xl": createTextSize({
     fontSize: 36,
@@ -166,5 +188,6 @@ export const textSizes = {
       android: -0.3,
       ios: -0.3,
     },
+    fontMetrics: fontMetricsInter,
   }),
-} as const;
+};
