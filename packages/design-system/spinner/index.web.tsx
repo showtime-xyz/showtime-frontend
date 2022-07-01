740915dc6d312d9a3876eb9af597cc25b21d3ee6
alantoa <toacncom@gmail.com>
2022-07-01 20:58:02 +0800

    test Spinner chromatic

--- packages/design-system/spinner/index.web.tsx
+++ packages/design-system/spinner/index.web.tsx
@@ -4,17 +4,20 @@ import { View as MotiView } from "moti";
 
 import { getSpinnerSize, SpinnerView, SpinnerProps } from "./spinner-view";
 
+const from = { rotate: "0deg" };
+const animate = { rotate: "360deg" };
 export const Spinner = ({ size, duration = 750, ...rest }: SpinnerProps) => {
   return (
     <MotiView
-      from={{ rotate: "0deg" }}
-      animate={{ rotate: "360deg" }}
+      from={from}
+      animate={animate}
       transition={{
         type: "timing",
         loop: true,
         repeatReverse: false,
         easing: Easing.linear,
         duration,
+        delay: 300,
       }}
       style={{
         height: getSpinnerSize(size),
