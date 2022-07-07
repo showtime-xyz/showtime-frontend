31350f83f02cf8b9c5b802bc31c553ecf7cc906b
Nishan <nishanbende@gmail.com>
2022-07-07 16:06:09 +0530

    fix: sentry on web

--- apps/next-react-native/sentry.server.config.js
+++ apps/next-react-native/sentry.server.config.js
@@ -1,17 +1,17 @@
 // This file configures the initialization of Sentry on the server.
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+import { ExtraErrorData, CaptureConsole } from "@sentry/integrations";
 import * as Sentry from "@sentry/nextjs";
 
-const SENTRY_DSN = process.env.NEXT_PUBLIC_SENTRY_DSN;
-
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  environment: process.env.NEXT_PUBLIC_STAGE,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
+  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
+  environment: process.env.SENTRY_SERVER_ENVIRONMENT,
+  tracesSampleRate: 0.2,
+  integrations: [
+    new ExtraErrorData(),
+    new CaptureConsole({
+      levels: ["warn", "error"],
+    }),
+  ],
 });
