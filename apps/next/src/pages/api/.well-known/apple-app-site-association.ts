bcf6cbce43e1344bb9b4eba011573612165fda3c
Nishan <nishanbende@gmail.com>
2022-07-26 19:43:18 +0530

    wip

--- apps/next/src/pages/api/.well-known/apple-app-site-association.ts
+++ apps/next/src/pages/api/.well-known/apple-app-site-association.ts
@@ -14,7 +14,6 @@ const association = {
           // this is often not desired
           // see the Apple docs to configure this with granularity
           "/nft/*",
-          "/",
         ],
       },
     ],
