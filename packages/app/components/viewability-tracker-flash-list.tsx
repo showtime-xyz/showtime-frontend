e84de6b7cc23ae1dc2c6d0368c9bfc966acd64af
Nishan <nishanbende@gmail.com>
2022-08-02 12:09:35 +0530

    wip

--- packages/app/components/viewability-tracker-flash-list.tsx
+++ packages/app/components/viewability-tracker-flash-list.tsx
@@ -1,9 +1,12 @@
-import { createContext, forwardRef, useCallback, useMemo } from "react";
+import { forwardRef, useCallback } from "react";
 
 import { FlashList, FlashListProps } from "@shopify/flash-list";
-import Animated, { useSharedValue } from "react-native-reanimated";
+import { useSharedValue } from "react-native-reanimated";
 
-import { VideoConfigContext } from "app/context/video-config-context";
+import {
+  ItemKeyContext,
+  ViewabilityItemsContext,
+} from "./viewability-tracker-flatlist";
 
 type ViewabilityItemsContextType = any[];
 
@@ -11,12 +14,6 @@ const viewabilityConfig = {
   minimumViewTime: 100,
   itemVisiblePercentThreshold: 50,
 };
-
-export const ViewabilityItemsContext = createContext<
-  Animated.SharedValue<ViewabilityItemsContextType>
->({
-  value: [],
-});
 
 export const ViewabilityTrackerFlashList = forwardRef(
   (props: FlashListProps<any>, ref: any) => {
@@ -51,30 +48,17 @@ export const ViewabilityTrackerFlashList = forwardRef(
       [_renderItem, keyExtractor]
     );
 
-    const videoConfig = useMemo(
-      () => ({
-        isMuted: true,
-        useNativeControls: false,
-        previewOnly: true,
-      }),
-      []
-    );
-
     return (
-      <VideoConfigContext.Provider value={videoConfig}>
-        <ViewabilityItemsContext.Provider value={visibleItems}>
-          <FlashList
-            {...props}
-            ref={ref}
-            viewabilityConfig={viewabilityConfig}
-            onViewableItemsChanged={onViewableItemsChanged}
-            renderItem={renderItem}
-          />
-        </ViewabilityItemsContext.Provider>
-      </VideoConfigContext.Provider>
+      <ViewabilityItemsContext.Provider value={visibleItems}>
+        <FlashList
+          {...props}
+          ref={ref}
+          viewabilityConfig={viewabilityConfig}
+          onViewableItemsChanged={onViewableItemsChanged}
+          renderItem={renderItem}
+        />
+      </ViewabilityItemsContext.Provider>
     );
   }
 );
 ViewabilityTrackerFlashList.displayName = "ViewabilityTrackerFlashList";
-
-export const ItemKeyContext = createContext<any | undefined>(undefined);
