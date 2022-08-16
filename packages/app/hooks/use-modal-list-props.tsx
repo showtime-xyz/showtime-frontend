3a3ed5475dee3a51cd0a6fd7ab7970d436858d13
alantoa <toacncom@gmail.com>
2022-08-16 17:21:27 +0800

    fix: modal list not visible

--- packages/app/hooks/use-modal-list-props.tsx
+++ packages/app/hooks/use-modal-list-props.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { Platform } from "react-native";
 
 import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
@@ -5,14 +6,29 @@ import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import { useRouter } from "@showtime-xyz/universal.router";
 import { tw } from "@showtime-xyz/universal.tailwind";
 
+const ModalListScreenQueryParamList = [
+  "followersModal",
+  "followingModal",
+  "commentsModal",
+  "claimersModal",
+];
+
 export const useModalListProps = () => {
   const router = useRouter();
+  // Todo: hacks code, will remove it once the react-virtuoso‘s list auto height in modal issue fix.
+  const isHasModalParam = useMemo(() => {
+    const index = ModalListScreenQueryParamList.findIndex((key) =>
+      Object.prototype.hasOwnProperty.call(router.query, key)
+    );
+    return index > -1;
+  }, [router.query]);
+
   return {
     // Todo: react-virtuoso‘s list height does not adapt when in Modal, so set min-height to fix it first. and dig reason later.
-    style:
-      Platform.OS === "web" && (router.query as any)?.commentsModal
-        ? tw.style("min-h-50vh")
-        : null,
+    style: Platform.select({
+      web: isHasModalParam ? tw.style("min-h-50vh") : null,
+      default: null,
+    }),
     useWindowScroll: false,
     ...Platform.select({
       android: { renderScrollComponent: BottomSheetScrollView as any },
