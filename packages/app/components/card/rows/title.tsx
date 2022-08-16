129e1f8c72b0cc9292f03e336d96cdbeab7f5059
alantoa <toacncom@gmail.com>
2022-08-16 21:46:27 +0800

    notes: remove todo & add note

--- packages/app/components/card/rows/title.tsx
+++ packages/app/components/card/rows/title.tsx
@@ -2,9 +2,9 @@ import { useState, useLayoutEffect, useRef } from "react";
 import { Platform } from "react-native";
 
 import { Text } from "@showtime-xyz/universal.text";
+// Todo: now Tooltip component seems to no working on dist floder,it need to check it.
+import { Tooltip } from "@showtime-xyz/universal.tooltip";
 import { View } from "@showtime-xyz/universal.view";
-
-import { Tooltip } from "design-system/tooltip";
 
 type Props = {
   title?: string;
@@ -16,11 +16,6 @@ export function Title({ title, cardMaxWidth, disableTooltip = false }: Props) {
   const [isUseTooltip, setIsOverflow] = useState(
     Platform.OS === "web" && !disableTooltip
   );
-  /**
-   * Todo: If the content width is greater than the container width, use Tooltip components,
-   * but now RecyclerList is used, I can't get valid data, so wait until we replace the list and then implement.
-   *
-   * */
   const textRef = useRef<HTMLElement | null>(null);
   useLayoutEffect(() => {
     if (textRef.current && Platform.OS === "web") {
