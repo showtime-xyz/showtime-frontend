132ff232b8100d48c8a890ff2b78cbbfca4b1c55
Nishan <nishanbende@gmail.com>
2022-08-17 08:57:50 +0530

    fix: mute button location

--- packages/app/components/feed-item/index.tsx
+++ packages/app/components/feed-item/index.tsx
@@ -24,6 +24,7 @@ import { View } from "@showtime-xyz/universal.view";
 
 import { FeedItemTapGesture } from "app/components/feed/feed-item-tap-gesture";
 import { Media } from "app/components/media";
+import { MuteButtonOffsetProvider } from "app/components/mute-button/mute-button";
 import { LikeContextProvider } from "app/context/like-context";
 import { useCreatorCollectionDetail } from "app/hooks/use-creator-collection-detail";
 import { Blurhash } from "app/lib/blurhash";
@@ -112,6 +113,11 @@ export const FeedItem = memo<FeedItemProps>(function FeedItem({
     return <FeedItemMD nft={nft} itemHeight={itemHeight} />;
   }
 
+  const mediaEndY =
+    (itemHeight - bottomPadding - mediaHeight) / 2 + mediaHeight;
+  const detailStartY = itemHeight - bottomMargin - detailHeight;
+  const muteButtonOffsetY = mediaEndY - detailStartY;
+
   return (
     <LikeContextProvider nft={nft} key={nft.nft_id}>
       <View tw="w-full" style={{ height: itemHeight }}>
@@ -148,18 +154,24 @@ export const FeedItem = memo<FeedItemProps>(function FeedItem({
             style={{
               height: Platform.select({
                 web: itemHeight - detailHeight,
-                default: itemHeight - bottomPadding - 50,
+                default: itemHeight - bottomPadding,
               }),
             }}
           >
-            <Media
-              item={nft}
-              numColumns={1}
-              tw={`h-[${mediaHeight}px] w-[${windowWidth}px]`}
-              resizeMode="contain"
-              onPinchStart={hideHeader}
-              onPinchEnd={showHeader}
-            />
+            <MuteButtonOffsetProvider
+              bottomOffset={
+                !isNaN(muteButtonOffsetY) ? muteButtonOffsetY + 10 : 0
+              }
+            >
+              <Media
+                item={nft}
+                numColumns={1}
+                tw={`h-[${mediaHeight}px] w-[${windowWidth}px]`}
+                resizeMode="contain"
+                onPinchStart={hideHeader}
+                onPinchEnd={showHeader}
+              />
+            </MuteButtonOffsetProvider>
           </View>
         </FeedItemTapGesture>
 
@@ -174,7 +186,6 @@ export const FeedItem = memo<FeedItemProps>(function FeedItem({
               layout: { height },
             },
           }) => {
-            if (Platform.OS !== "web") return;
             setDetailHeight(height);
           }}
         >
