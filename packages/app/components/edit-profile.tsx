ba40d1c90292388af78c81b89e4cc254ef7a4813
Nishan <nishanbende@gmail.com>
2022-08-20 19:28:31 +0530

    fix: edit profile

--- packages/app/components/edit-profile.tsx
+++ packages/app/components/edit-profile.tsx
@@ -1,5 +1,10 @@
 import { useEffect, useMemo, useState } from "react";
-import { Platform, Pressable, useWindowDimensions } from "react-native";
+import {
+  Dimensions,
+  Platform,
+  Pressable,
+  useWindowDimensions,
+} from "react-native";
 
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -31,6 +36,8 @@ import { useFilePicker } from "design-system/file-picker";
 import { SelectedTabIndicator, TabItem, Tabs } from "design-system/tabs";
 
 import { MediaCropper } from "./media-cropper";
+
+const windowWidth = Dimensions.get("window").width;
 
 const editProfileValidationSchema = yup.object({
   username: yup
@@ -247,7 +254,7 @@ export const EditProfile = () => {
   return (
     <>
       <BottomSheetModalProvider>
-        <View tw={`w-full flex-1 pb-${insets.bottom}px`}>
+        <View tw={`w-full flex-1`}>
           <Tabs.Root
             onIndexChange={setSelected}
             tabListHeight={TAB_LIST_HEIGHT}
@@ -256,10 +263,12 @@ export const EditProfile = () => {
             lazy
           >
             <Tabs.List
-              style={[
-                tw.style(`ios:w-screen android:w-screen`),
-                { height: TAB_LIST_HEIGHT },
-              ]}
+              style={{
+                height: TAB_LIST_HEIGHT,
+                width: Platform.select({
+                  native: windowWidth,
+                }),
+              }}
             >
               {tabs.map((name, index) => (
                 <Tabs.Trigger key={name}>
