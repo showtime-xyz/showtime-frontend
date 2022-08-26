a81a4e29baf0a5ac3829a6859abb6af34e1bcfcd
Nishan <nishanbende@gmail.com>
2022-08-26 10:32:40 +0530

    wip

--- packages/app/components/viewability-tracker-flash-list.tsx
+++
@@ -1,6 +0,0 @@
-import { FlashList } from "@shopify/flash-list";
-
-import { withViewabilityInfiniteScrollList } from "app/hoc/with-viewability-infinite-scroll-list";
-
-export const ViewabilityTrackerFlashList =
-  withViewabilityInfiniteScrollList(FlashList);
