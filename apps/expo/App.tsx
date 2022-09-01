f2638031f543099605a31af4aa68b09455047e41
alantoa <toacncom@gmail.com>
2022-09-01 23:06:18 +0800

    fix: crash when bad network

--- apps/expo/App.tsx
+++ apps/expo/App.tsx
@@ -4,6 +4,7 @@ import { LogBox } from "react-native";
 import LogRocket from "@logrocket/react-native";
 import rudderClient from "@rudderstack/rudder-sdk-react-native";
 import { Audio } from "expo-av";
+import * as Network from "expo-network";
 import * as Notifications from "expo-notifications";
 import { StatusBar } from "expo-status-bar";
 import { enableScreens } from "react-native-screens";
@@ -50,6 +51,7 @@ function App() {
         process.env.NEXT_PUBLIC_RUDDERSTACK_WRITE_KEY,
         rudderConfig
       );
+      const network = await Network.getNetworkStateAsync();
     };
 
     initAnalytics();
