b3f3a51cac0e5c52b6621fd6483871a032e8fc03
alantoa <toacncom@gmail.com>
2022-09-02 23:41:34 +0800

    refactor: use tw css spin animation

--- packages/design-system/spinner/spinner.web.tsx
+++ packages/design-system/spinner/spinner.web.tsx
@@ -1,32 +1,18 @@
-import { Easing } from "react-native";
-
-import { View as MotiView } from "moti";
+import { View } from "@showtime-xyz/universal.view";
 
 import { getSpinnerSize, SpinnerView, SpinnerProps } from "./spinner-view";
 
-const from = { rotate: "0deg" };
-const animate = { rotate: "360deg" };
-export const Spinner = ({ size, duration = 750, ...rest }: SpinnerProps) => {
+export const Spinner = ({ size, ...rest }: SpinnerProps) => {
   return (
-    <MotiView
-      // @ts-ignore - chromatic ignore DOM element https://www.chromatic.com/docs/ignoring-elements#ignore-dom-elements
-      dataSet={{ chromatic: "ignore" }}
-      from={from}
-      animate={animate}
-      transition={{
-        type: "timing",
-        loop: true,
-        repeatReverse: false,
-        easing: Easing.linear,
-        duration,
-      }}
+    <View
       style={{
         height: getSpinnerSize(size),
         width: getSpinnerSize(size),
       }}
+      tw="animate-spin duration-75"
       accessibilityRole="progressbar"
     >
       <SpinnerView size={size} {...rest} />
-    </MotiView>
+    </View>
   );
 };
