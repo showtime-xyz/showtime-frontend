41d0e0a8c10a7060a76c9a3e5507a07f29e466bb
alantoa <toacncom@gmail.com>
2022-09-08 00:33:08 +0800

    fix: type

--- packages/design-system/tailwind/types.ts
+++ packages/design-system/tailwind/types.ts
@@ -1,3 +1,5 @@
-type TW = string | string[] | TW[];
+// type TW = string | string[] | TW[];
+// TODO: switch the any to TW[] when replaced all tw.style() function.
+type TW = string | string[] | any;
 
 export { TW };
