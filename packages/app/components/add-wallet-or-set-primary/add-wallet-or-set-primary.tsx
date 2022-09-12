ef4af5ac014febe18964251bf4ff93be44f840ec
Nishan <nishanbende@gmail.com>
2022-09-12 19:27:12 +0530

    wip

--- packages/app/components/add-wallet-or-set-primary/add-wallet-or-set-primary.tsx
+++ packages/app/components/add-wallet-or-set-primary/add-wallet-or-set-primary.tsx
@@ -1,23 +1,58 @@
 import * as React from "react";
-import { Text } from "react-native";
+import { useMemo, useState } from "react";
 
+import { Button } from "@showtime-xyz/universal.button";
+import { Select } from "@showtime-xyz/universal.select";
+import { View } from "@showtime-xyz/universal.view";
+
+import { useSetPrimaryWallet } from "app/hooks/api/use-set-primary-wallet";
+import { useAddWallet } from "app/hooks/use-add-wallet";
 import { useUser } from "app/hooks/use-user";
+import { formatAddressShort } from "app/utilities";
 
 export const AddWalletOrSetPrimary = () => {
   const { user } = useUser();
+  const { setPrimaryWallet } = useSetPrimaryWallet();
+  const { addWallet } = useAddWallet();
+  const [selectedWallet, setSelectedWallet] = useState<any>(null);
 
   const hasWallet =
     user?.data.profile.wallet_addresses_excluding_email_v2 &&
     user.data.profile.wallet_addresses_excluding_email_v2.length > 0;
-
   const primary = user?.data.profile.primary_wallet;
 
-  if (!primary) {
-    return <Text>Set primary wallet</Text>;
-  }
+  const wallets = useMemo(() => {
+    return user?.data.profile.wallet_addresses_excluding_email_v2.map(
+      (wallet) => {
+        return {
+          label: formatAddressShort(wallet.address),
+          value: wallet.address,
+        };
+      }
+    );
+  }, [user]);
 
   if (!hasWallet) {
-    return <Text>add wallet</Text>;
+    return (
+      <View>
+        <Button onPress={addWallet}>Add a wallet</Button>
+      </View>
+    );
+  }
+
+  if (!primary) {
+    return (
+      <View>
+        <Select
+          options={wallets}
+          onChange={setSelectedWallet}
+          value={selectedWallet}
+        />
+        <Button onPress={() => setPrimaryWallet(selectedWallet)}>
+          Set primary wallet
+        </Button>
+      </View>
+    );
   }
 
   return <></>;
