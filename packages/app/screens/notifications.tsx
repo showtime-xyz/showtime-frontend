96ffc7f4168fdef29e0905fec5fce01c7e335ee0
alantoa <toacncom@gmail.com>
2022-09-16 14:38:00 +0800

    feat: notification add blur header effect

--- packages/app/screens/notifications.tsx
+++ packages/app/screens/notifications.tsx
@@ -1,5 +1,5 @@
 import { Suspense } from "react";
-import { Platform, useWindowDimensions } from "react-native";
+import { useWindowDimensions } from "react-native";
 
 import { Spinner } from "@showtime-xyz/universal.spinner";
 import { View } from "@showtime-xyz/universal.view";
@@ -8,18 +8,15 @@ import { ErrorBoundary } from "app/components/error-boundary";
 import { withColorScheme } from "app/components/memo-with-theme";
 import { Notifications } from "app/components/notifications";
 import { useTrackPageViewed } from "app/lib/analytics";
-import { useHeaderHeight } from "app/lib/react-navigation/elements";
 
 import { breakpoints } from "design-system/theme";
 
 const NotificationsScreen = withColorScheme(() => {
   useTrackPageViewed({ name: "Notifications" });
-  const headerHeight = useHeaderHeight();
   const { width } = useWindowDimensions();
   const isMdWidth = width >= breakpoints["md"];
   return (
     <View tw="w-full max-w-screen-2xl flex-1 md:px-6">
-      {Platform.OS === "ios" && <View style={{ height: headerHeight }} />}
       <ErrorBoundary>
         <Suspense
           fallback={
