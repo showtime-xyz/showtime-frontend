514e511df1aefe2b637a20c46e6bef8838f78f85
Nishan <nishanbende@gmail.com>
2022-10-11 14:35:42 +0530

    nits

--- packages/app/lib/react-native-spotify-remote/src/PlaybackRestrictions.ts
+++
@@ -1,9 +0,0 @@
-export default interface PlaybackRestrictions {
-    canSkipNext: boolean;
-    canSkipPrevious: boolean;
-    canRepeatTrack: boolean;
-    canRepeatContext: boolean;
-    canToggleShuffle: boolean;
-    canSeek: boolean;
-}
-
