6f88f82da7cb4bb7cfa3cd96bda209c796afacb5
alantoa <toacncom@gmail.com>
2022-10-19 16:33:30 +0800

    fix: setting header on mobile web

--- packages/app/components/settings/setting-header.tsx
+++ packages/app/components/settings/setting-header.tsx
@@ -23,7 +23,7 @@ export const SettingHeaderSection = ({ title = "" }) => {
     </View>
   );
 };
-export type SettingsHeaderProps = {
+type SettingsHeaderProps = {
   index: number;
   setIndex: (index: number) => void;
   routers: Route[];
@@ -39,16 +39,29 @@ export const SettingsHeader = ({
     <>
       {Platform.OS !== "android" && <View style={{ height: headerHeight }} />}
       <View tw="dark:shadow-dark shadow-light items-center bg-white dark:bg-black md:mb-4">
-        <View tw="w-full max-w-screen-2xl flex-row justify-between self-center px-4 py-4 md:py-0">
-          <Text tw="font-space-bold self-center text-2xl font-extrabold text-gray-900 dark:text-white">
-            Settings
-          </Text>
-          {!isWeb ? (
-            <Text tw="font-space-bold text-2xl font-extrabold text-gray-100 dark:text-gray-900">
-              v{Constants?.manifest?.version ?? packageJson?.version}
+        <View tw="w-full max-w-screen-2xl">
+          <View tw="w-full flex-row justify-between self-center px-4 py-4 md:py-0">
+            <Text tw="font-space-bold self-center text-2xl font-extrabold text-gray-900 dark:text-white">
+              Settings
             </Text>
-          ) : (
-            <Hidden until="md">
+            {!isWeb ? (
+              <Text tw="font-space-bold text-2xl font-extrabold text-gray-100 dark:text-gray-900">
+                v{Constants?.manifest?.version ?? packageJson?.version}
+              </Text>
+            ) : (
+              <Hidden until="md">
+                <TabBarSingle
+                  onPress={(i) => {
+                    setIndex(i);
+                  }}
+                  routes={routers}
+                  index={index}
+                />
+              </Hidden>
+            )}
+          </View>
+          {isWeb && (
+            <Hidden from="md">
               <TabBarSingle
                 onPress={(i) => {
                   setIndex(i);
