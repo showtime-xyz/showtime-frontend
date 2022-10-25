bcac1919342e98c51b087851617c1effbed3c02d
alantoa <toacncom@gmail.com>
2022-10-26 04:40:30 +0800

    deps: upgrade packages

--- packages/design-system/data-pill/index.tsx
+++ packages/design-system/data-pill/index.tsx
@@ -5,7 +5,7 @@ import type { TW } from "@showtime-xyz/universal.tailwind";
 import { Text } from "@showtime-xyz/universal.text";
 import { View } from "@showtime-xyz/universal.view";
 
-type DataPillType = "primary" | "secondary" | "text";
+type Type = "primary" | "secondary" | "text";
 type DataPillProps = {
   type?: "primary" | "secondary" | "text";
   label?: string;
@@ -13,13 +13,13 @@ type DataPillProps = {
   textTw?: string;
 };
 // TODO: tw `dark:text-xxx-xxx` prop not working on native, not sure why. so use an array to hack make it works first and dig it later.
-const textTwColorMap = new Map<DataPillType, TW>([
+const textTwColorMap = new Map<Type, TW>([
   ["primary", ["text-gray-500", ""]],
   ["secondary", ["text-white", "text-gray-900"]],
   ["text", ["text-gray-900", "text-gray-100"]],
 ]);
 
-const bgColorMap = new Map<DataPillType, TW>([
+const bgColorMap = new Map<Type, TW>([
   ["primary", "bg-gray-100 dark:bg-gray-900"],
   ["secondary", "bg-gray-900 dark:bg-gray-100"],
   ["text", "bg-gray-100 dark:bg-gray-900"],
