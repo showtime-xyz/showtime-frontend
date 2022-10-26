f14c0074903189115adadf4c4ce105a9e172b6c9
Nishan <nishanbende@gmail.com>
2022-10-26 11:31:03 +0530

    fix: alchemy provider endpoint

--- packages/app/providers/wallet-provider.web.tsx
+++ packages/app/providers/wallet-provider.web.tsx
@@ -1,11 +1,12 @@
 import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, provider, webSocketProvider } = configureChains(
   [chain.mainnet, chain.polygon, chain.polygonMumbai],
   [
-    //@ts-ignore
+    alchemyProvider({ apiKey: process.env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
     publicProvider(),
   ]
 );
