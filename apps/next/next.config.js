7ab6603b1011c96965c0a577d815506fec994827
Axel Delafosse <axeldelafosse@gmail.com>
2022-11-08 19:38:44 +0100

    remove expo next adapter

--- apps/next/next.config.js
+++ apps/next/next.config.js
@@ -1,11 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-
 const isDev = process.env.NODE_ENV === "development";
 
-const { withExpo } = require("@expo/next-adapter");
-const withFonts = require("next-fonts");
 const withImages = require("next-images");
 const withPlugins = require("next-compose-plugins");
 const { withSentryConfig } = require("@sentry/nextjs");
@@ -18,72 +15,7 @@ const withPWA = require("next-pwa")({
   disable: isDev,
   runtimeCaching: cache,
 });
-const withTM = require("next-transpile-modules")([
-  "app",
-  "@gorhom/bottom-sheet",
-  "@gorhom/portal",
-  "moti",
-  "expo-next-react-navigation",
-  "zeego",
-  "sentry-expo",
-  "solito",
-  "three",
-  "@showtime-xyz/universal.typography",
-  "@showtime-xyz/universal.tailwind",
-  "@showtime-xyz/universal.view",
-  "@showtime-xyz/universal.text",
-  "@showtime-xyz/universal.accordion",
-  "@showtime-xyz/universal.activity-indicator",
-  "@showtime-xyz/universal.alert",
-  "@showtime-xyz/universal.avatar",
-  "@showtime-xyz/universal.bottom-sheet",
-  "@showtime-xyz/universal.button",
-  "@showtime-xyz/universal.checkbox",
-  "@showtime-xyz/universal.chip",
-  "@showtime-xyz/universal.clamp-text",
-  "@showtime-xyz/universal.collapsible-tab-view",
-  "@showtime-xyz/universal.color-scheme",
-  "@showtime-xyz/universal.country-code-picker",
-  "@showtime-xyz/universal.color-scheme",
-  "@showtime-xyz/universal.data-pill",
-  "@showtime-xyz/universal.divider",
-  "@showtime-xyz/universal.dropdown-menu",
-  "@showtime-xyz/universal.fieldset",
-  "@showtime-xyz/universal.haptics",
-  "@showtime-xyz/universal.hooks",
-  "@showtime-xyz/universal.icon",
-  "@showtime-xyz/universal.infinite-scroll-list",
-  "@showtime-xyz/universal.image",
-  "@showtime-xyz/universal.input",
-  "@showtime-xyz/universal.label",
-  "@showtime-xyz/universal.light-box",
-  "@showtime-xyz/universal.modal",
-  "@showtime-xyz/universal.modal-screen",
-  "@showtime-xyz/universal.modal-sheet",
-  "@showtime-xyz/universal.pan-to-close",
-  "@showtime-xyz/universal.pinch-to-zoom",
-  "@showtime-xyz/universal.pressable",
-  "@showtime-xyz/universal.pressable-hover",
-  "@showtime-xyz/universal.pressable-scale",
-  "@showtime-xyz/universal.router",
-  "@showtime-xyz/universal.safe-area",
-  "@showtime-xyz/universal.scroll-view",
-  "@showtime-xyz/universal.segmented-control",
-  "@showtime-xyz/universal.select",
-  "@showtime-xyz/universal.skeleton",
-  "@showtime-xyz/universal.snackbar",
-  "@showtime-xyz/universal.spinner",
-  "@showtime-xyz/universal.switch",
-  "@showtime-xyz/universal.text-input",
-  "@showtime-xyz/universal.tab-view",
-  "@showtime-xyz/universal.toast",
-  "@showtime-xyz/universal.tooltip",
-  "@showtime-xyz/universal.utils",
-  "@showtime-xyz/universal.verification-badge",
-  "nativewind",
-  "@shopify/flash-list",
-  "recyclerlistview",
-]);
+// const { DefinePlugin } = require("webpack");
 
 const nextConfig = {
   swcMinify: false,
@@ -100,6 +32,150 @@ const nextConfig = {
     fontLoaders: [
       { loader: "@next/font/google", options: { subsets: ["latin"] } },
     ],
+    transpilePackages: [
+      "app",
+      "@gorhom/bottom-sheet",
+      "@gorhom/portal",
+      "moti",
+      "expo-next-react-navigation",
+      "zeego",
+      "sentry-expo",
+      "solito",
+      "three",
+      "@showtime-xyz/universal.typography",
+      "@showtime-xyz/universal.tailwind",
+      "@showtime-xyz/universal.view",
+      "@showtime-xyz/universal.text",
+      "@showtime-xyz/universal.accordion",
+      "@showtime-xyz/universal.activity-indicator",
+      "@showtime-xyz/universal.alert",
+      "@showtime-xyz/universal.avatar",
+      "@showtime-xyz/universal.bottom-sheet",
+      "@showtime-xyz/universal.button",
+      "@showtime-xyz/universal.checkbox",
+      "@showtime-xyz/universal.chip",
+      "@showtime-xyz/universal.clamp-text",
+      "@showtime-xyz/universal.collapsible-tab-view",
+      "@showtime-xyz/universal.color-scheme",
+      "@showtime-xyz/universal.country-code-picker",
+      "@showtime-xyz/universal.color-scheme",
+      "@showtime-xyz/universal.data-pill",
+      "@showtime-xyz/universal.divider",
+      "@showtime-xyz/universal.dropdown-menu",
+      "@showtime-xyz/universal.fieldset",
+      "@showtime-xyz/universal.haptics",
+      "@showtime-xyz/universal.hooks",
+      "@showtime-xyz/universal.icon",
+      "@showtime-xyz/universal.infinite-scroll-list",
+      "@showtime-xyz/universal.image",
+      "@showtime-xyz/universal.input",
+      "@showtime-xyz/universal.label",
+      "@showtime-xyz/universal.light-box",
+      "@showtime-xyz/universal.modal",
+      "@showtime-xyz/universal.modal-screen",
+      "@showtime-xyz/universal.modal-sheet",
+      "@showtime-xyz/universal.pan-to-close",
+      "@showtime-xyz/universal.pinch-to-zoom",
+      "@showtime-xyz/universal.pressable",
+      "@showtime-xyz/universal.pressable-hover",
+      "@showtime-xyz/universal.pressable-scale",
+      "@showtime-xyz/universal.router",
+      "@showtime-xyz/universal.safe-area",
+      "@showtime-xyz/universal.scroll-view",
+      "@showtime-xyz/universal.segmented-control",
+      "@showtime-xyz/universal.select",
+      "@showtime-xyz/universal.skeleton",
+      "@showtime-xyz/universal.snackbar",
+      "@showtime-xyz/universal.spinner",
+      "@showtime-xyz/universal.switch",
+      "@showtime-xyz/universal.text-input",
+      "@showtime-xyz/universal.tab-view",
+      "@showtime-xyz/universal.toast",
+      "@showtime-xyz/universal.tooltip",
+      "@showtime-xyz/universal.utils",
+      "@showtime-xyz/universal.verification-badge",
+      "nativewind",
+      "@shopify/flash-list",
+      "recyclerlistview",
+      "react-native-web",
+      "expo",
+      // "react-native",
+      // "expo",
+      // "expo-app-loading",
+      // "expo-application",
+      "expo-av",
+      // "expo-blur",
+      // "expo-build-properties",
+      // "expo-camera",
+      // "expo-community-flipper",
+      // "expo-constants",
+      // "expo-dev-client",
+      // "expo-device",
+      // "expo-font",
+      // "expo-gl",
+      // "expo-haptics",
+      // "expo-image-picker",
+      // "expo-linear-gradient",
+      // "expo-linking",
+      // "expo-localization",
+      // "expo-location",
+      // "expo-media-library",
+      // "expo-modules-core",
+      // "expo-navigation-bar",
+      // "expo-next-react-navigation",
+      // "expo-notifications",
+      // "expo-splash-screen",
+      // "expo-status-bar",
+      // "expo-system-ui",
+      // "expo-updates",
+      // "expo-web-browser",
+      // "@rudderstack/rudder-sdk-react-native",
+      // "@react-native-community/blur",
+      // "react-native-reanimated",
+      // "react-native-gesture-handler",
+      // "react-native-safe-area-context",
+      // "react-native-screens",
+    ],
+  },
+  webpack: (config, options) => {
+    // Mix in aliases
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Alias direct react-native imports to react-native-web
+      "react-native$": "react-native-web",
+      // Alias internal react-native modules to react-native-web
+      "react-native/Libraries/EventEmitter/RCTDeviceEventEmitter$":
+        "react-native-web/dist/vendor/react-native/NativeEventEmitter/RCTDeviceEventEmitter",
+      "react-native/Libraries/vendor/emitter/EventEmitter$":
+        "react-native-web/dist/vendor/react-native/emitter/EventEmitter",
+      "react-native/Libraries/EventEmitter/NativeEventEmitter$":
+        "react-native-web/dist/vendor/react-native/NativeEventEmitter",
+    };
+
+    config.resolve.extensions = [
+      ".web.js",
+      ".web.jsx",
+      ".web.ts",
+      ".web.tsx",
+      ...(config.resolve?.extensions ?? []),
+    ];
+
+    if (!config.plugins) {
+      config.plugins = [];
+    }
+
+    // Expose __DEV__ from Metro.
+    // config.plugins.push(
+    //   new DefinePlugin({
+    //     __DEV__: JSON.stringify(process.env.NODE_ENV !== "production"),
+    //   })
+    // );
+
+    return config;
   },
   typescript: {
     ignoreDevErrors: true,
@@ -177,13 +253,10 @@ const nextConfig = {
 
 module.exports = withPlugins(
   [
-    withTM,
-    withFonts,
     withImages,
     withBundleAnalyzer,
     !isDev ? withSentryConfig : null,
     withPWA,
-    [withExpo, { projectRoot: __dirname + "/../.." }],
   ].filter(Boolean),
   nextConfig
 );
