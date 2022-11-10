853ca7a9ad5200bee2870e94be75096ebc7814d4
nishan (o^▽^o) <nishanbende@gmail.com>
2022-11-10 20:27:43 +0530

    fix: wallet login and email issue (#1691)

--- packages/app/components/login/index.tsx
+++ packages/app/components/login/index.tsx
@@ -2,6 +2,7 @@ import { useMemo, useState } from "react";
 import { Platform, StyleSheet } from "react-native";
 
 import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
+import { PortalProvider } from "@gorhom/portal";
 
 import { ScrollView } from "@showtime-xyz/universal.scroll-view";
 import { Text } from "@showtime-xyz/universal.text";
@@ -70,68 +71,70 @@ export function Login({ onLogin }: LoginProps) {
 
   //#endregion
   return (
-    <ContainerView style={styles.container}>
-      {isConnectingToWallet ? (
-        <View tw="py-40">
-          <Text tw="text-center dark:text-gray-400">
-            {walletName
-              ? `Pushed a request to ${walletName}... Please check your wallet.`
-              : `Pushed a request to your wallet...`}
-          </Text>
-        </View>
-      ) : (
-        <View>
-          <View
-            style={[
-              styles.tabListItemContainer,
-              { display: showEmailLogin ? "flex" : "none" },
-            ]}
-          >
-            <LoginInputField
-              key="login-email-field"
-              validationSchema={emailValidationSchema}
-              label="Email address"
-              placeholder="Enter your email address"
-              keyboardType="email-address"
-              textContentType="emailAddress"
-              signInButtonLabel="Send Email"
-              onSubmit={handleSubmitEmail}
-            />
-            <LoginButton
-              onPress={() => setShowEmailLogin(false)}
-              type="social"
-            />
+    <PortalProvider>
+      <ContainerView style={styles.container}>
+        {isConnectingToWallet ? (
+          <View tw="py-40">
+            <Text tw="text-center dark:text-gray-400">
+              {walletName
+                ? `Pushed a request to ${walletName}... Please check your wallet.`
+                : `Pushed a request to your wallet...`}
+            </Text>
           </View>
-          <View style={{ display: showEmailLogin ? "none" : "flex" }}>
-            <LoginHeader />
-            <View style={styles.tabListItemContainer}>
-              <View tw="mb-[16px]">
-                <PhoneNumberPicker
-                  handleSubmitPhoneNumber={handleSubmitPhoneNumber}
-                />
-              </View>
-              <View tw="mx-[-16px] mb-[16px] flex-row items-center">
-                <View tw="h-px flex-1 bg-gray-100 dark:bg-gray-800" />
-                <Text tw="mx-2 text-center text-sm font-bold text-gray-600 dark:text-gray-400">
-                  OR
-                </Text>
-                <View tw="h-px flex-1 bg-gray-100 dark:bg-gray-800" />
-              </View>
-              {/* <LoginWithApple />
+        ) : (
+          <View>
+            <View
+              style={[
+                styles.tabListItemContainer,
+                { display: showEmailLogin ? "flex" : "none" },
+              ]}
+            >
+              <LoginInputField
+                key="login-email-field"
+                validationSchema={emailValidationSchema}
+                label="Email address"
+                placeholder="Enter your email address"
+                keyboardType="email-address"
+                textContentType="emailAddress"
+                signInButtonLabel="Send Email"
+                onSubmit={handleSubmitEmail}
+              />
+              <LoginButton
+                onPress={() => setShowEmailLogin(false)}
+                type="social"
+              />
+            </View>
+            <View style={{ display: showEmailLogin ? "none" : "flex" }}>
+              <LoginHeader />
+              <View style={styles.tabListItemContainer}>
+                <View tw="mb-[16px]">
+                  <PhoneNumberPicker
+                    handleSubmitPhoneNumber={handleSubmitPhoneNumber}
+                  />
+                </View>
+                <View tw="mx-[-16px] mb-[16px] flex-row items-center">
+                  <View tw="h-px flex-1 bg-gray-100 dark:bg-gray-800" />
+                  <Text tw="mx-2 text-center text-sm font-bold text-gray-600 dark:text-gray-400">
+                    OR
+                  </Text>
+                  <View tw="h-px flex-1 bg-gray-100 dark:bg-gray-800" />
+                </View>
+                {/* <LoginWithApple />
               <LoginWithGoogle />
               <LoginWithTwitter /> */}
-              <LoginButton
-                onPress={() => setShowEmailLogin(true)}
-                type="email"
-              />
-              <ConnectButton handleSubmitWallet={handleSubmitWallet} />
-              <LoginFooter />
+                <LoginButton
+                  onPress={() => setShowEmailLogin(true)}
+                  type="email"
+                />
+                <ConnectButton handleSubmitWallet={handleSubmitWallet} />
+                <LoginFooter />
+              </View>
             </View>
           </View>
-        </View>
-      )}
-      <LoginOverlays loading={loading && !isConnectingToWallet} />
-    </ContainerView>
+        )}
+        <LoginOverlays loading={loading && !isConnectingToWallet} />
+      </ContainerView>
+    </PortalProvider>
   );
 }
 
