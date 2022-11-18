2638a7cf87fc45b14adb86b1552e88a7e4c451dd
alantoa <toacncom@gmail.com>
2022-11-18 15:31:52 +0800

    fix: login signature button on web

--- packages/app/components/login/index.web.tsx
+++ packages/app/components/login/index.web.tsx
@@ -1,9 +1,8 @@
-import { useMemo } from "react";
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
-import { BottomSheetScrollView } from "@gorhom/bottom-sheet";
 import { PortalProvider } from "@gorhom/portal";
 
+import { Button } from "@showtime-xyz/universal.button";
 import { ScrollView } from "@showtime-xyz/universal.scroll-view";
 import { Text } from "@showtime-xyz/universal.text";
 import { View } from "@showtime-xyz/universal.view";
@@ -11,47 +10,41 @@ import { View } from "@showtime-xyz/universal.view";
 import { LoginComponent } from "./login";
 import { useLogin } from "./use-login";
 
-const ContainerView: any =
-  Platform.OS === "android" ? BottomSheetScrollView : ScrollView;
 interface LoginProps {
   onLogin?: () => void;
 }
 
 export function Login({ onLogin }: LoginProps) {
   //#region hooks
-  const { walletStatus, walletName } = useLogin(onLogin);
-
+  const { walletStatus, walletName, showSignMessage, verifySignature } =
+    useLogin(onLogin);
   //#endregion
 
-  //#region variables
-  const isConnectingToWallet = useMemo(
-    () =>
-      [
-        "CONNECTING_TO_WALLET",
-        "CONNECTED_TO_WALLET",
-        "FETCHING_NONCE",
-        "FETCHED_NONCE",
-        "SIGNING_PERSONAL_MESSAGE",
-      ].includes(walletStatus),
-    [walletStatus]
-  );
-
-  //#endregion
   return (
     <PortalProvider>
-      <ContainerView style={styles.container}>
-        {isConnectingToWallet ? (
+      <ScrollView style={styles.container}>
+        {walletStatus === "FETCHING_SIGNATURE" ? (
           <View tw="py-40">
             <Text tw="text-center dark:text-gray-400">
-              {walletName
+              {walletName !== ""
                 ? `Pushed a request to ${walletName}... Please check your wallet.`
                 : `Pushed a request to your wallet...`}
             </Text>
           </View>
+        ) : showSignMessage ? (
+          <View tw="py-20 px-10">
+            <Text tw="text-center text-lg dark:text-gray-400">
+              We need a signature in order to verify your identity. This won't
+              cost any gas.
+            </Text>
+            <Button tw="mt-8" onPress={verifySignature}>
+              Sign the message
+            </Button>
+          </View>
         ) : (
           <LoginComponent onLogin={onLogin} />
         )}
-      </ContainerView>
+      </ScrollView>
     </PortalProvider>
   );
 }
@@ -60,7 +53,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 16,
-    minHeight: 400,
   },
   tabListItemContainer: {
     paddingHorizontal: 24,
