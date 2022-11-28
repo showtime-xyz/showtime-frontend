6d5a68249ab8d13d6feb1ea88095e4f094b0cac5
Nishan <nishanbende@gmail.com>
2022-11-28 11:07:09 +0530

    account screen ui

--- packages/app/components/settings/setting.web.tsx
+++ packages/app/components/settings/setting.web.tsx
@@ -1,12 +1,10 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-import { useRouter } from "@showtime-xyz/universal.router";
 import { View } from "@showtime-xyz/universal.view";
 
 import { ErrorBoundary } from "app/components/error-boundary";
 import { usePlatformBottomHeight } from "app/hooks/use-platform-bottom-height";
 import { useTabState } from "app/hooks/use-tab-state";
-import { useUser } from "app/hooks/use-user";
 import { WalletAddressesV2 } from "app/types";
 
 import { EditNicknameModal } from "./setting-edit-nickname-moda";
@@ -14,19 +12,10 @@ import { SettingsHeader } from "./setting-header";
 import { SettingTabsScene, SETTINGS_ROUTES } from "./tabs";
 
 const SettingsTabs = () => {
-  const { isAuthenticated } = useUser();
-  const router = useRouter();
   const bottomHeight = usePlatformBottomHeight();
   const [editingWallet, setEditingWallet] = useState<
     WalletAddressesV2 | undefined
   >(undefined);
-
-  useEffect(() => {
-    const isUnauthenticated = !isAuthenticated;
-    if (isUnauthenticated) {
-      router.pop();
-    }
-  }, [isAuthenticated, router]);
 
   const { index, setIndex, routes } = useTabState(SETTINGS_ROUTES);
 
