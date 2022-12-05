94f048a417d5745aa9b8f9ea795a89789542f1e3
Nishan <nishanbende@gmail.com>
2022-12-05 22:07:43 +0530

    wip

--- apps/next/src/pages/_app.tsx
+++ apps/next/src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { useCallback } from "react";
 import "@rainbow-me/rainbowkit/styles.css";
 import { AppProps } from "next/app";
 import Head from "next/head";
+import "react-datepicker/dist/react-datepicker.css";
 
 import { usePlatformResize } from "@showtime-xyz/universal.hooks";
 import { View } from "@showtime-xyz/universal.view";
