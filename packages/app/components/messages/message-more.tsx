1d391f01a80ba615247326288b4ad3db139866ff
Hirbod Mirjavadi <hm@nightstomp.com>
2023-02-05 13:06:40 +0100

    fix: wrong wording

--- packages/app/components/messages/message-more.tsx
+++ packages/app/components/messages/message-more.tsx
@@ -10,11 +10,11 @@ export function MessageMore({ count = 0, onPress }: MessageMoreProps) {
   if (count === 0) {
     return null;
   }
-  const replyText = count > 1 ? "replies" : "reply";
+  const replyText = count > 1 ? "Show more replies" : "Show reply";
   return (
     <View tw="-mt-2 mb-5 ml-1 flex bg-white pl-14 dark:bg-black">
-      <Text tw="font-space-bold text-xs dark:text-white" onPress={onPress}>
-        {`Load more ${replyText} (${count})`}
+      <Text tw="text-xs font-bold dark:text-white" onPress={onPress}>
+        {`${replyText} (${count})`}
       </Text>
     </View>
   );
