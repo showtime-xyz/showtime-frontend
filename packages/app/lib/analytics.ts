31578c01110f05adca7fc0070c19624efc833f6c
Hirbod Mirjavadi <hm@nightstomp.com>
2023-02-09 23:40:54 +0100

    Revert "fix: tracking screens/page correctly"
    This reverts commit 4c9fa3ff5473498de396e82774b07ff66fb823e4.

--- packages/app/lib/analytics.ts
+++ packages/app/lib/analytics.ts
@@ -1,24 +1,18 @@
 import { useEffect } from "react";
-import { Platform } from "react-native";
 
 import { useRudder } from "app/lib/rudderstack";
 
 type PageViewedProps = {
   name: string;
-  properties?: Record<string, any>;
+  type?: string;
 };
 
 // Page Viewed
-export function useTrackPageViewed({ name, properties }: PageViewedProps) {
+export function useTrackPageViewed({ name, type }: PageViewedProps) {
   const { rudder } = useRudder();
 
   useEffect(() => {
-    if (Platform.OS === "web") {
-      // @ts-expect-error rudderstack-native-sdk does not have a page method but its there in our rudderstack-provider.web.tsx
-      rudder?.page(name, { ...properties });
-    } else {
-      rudder?.screen(name, { ...properties });
-    }
+    rudder?.track("Page Viewed", { name, type });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [rudder]);
 }
