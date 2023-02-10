9a8813dd8676da378157781f7a0d50a7bd5aa2f3
Nishan <nishanbende@gmail.com>
2023-02-10 11:00:30 +0530

    wip

--- packages/app/components/checkout/checkout-return.tsx
+++ packages/app/components/checkout/checkout-return.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 
 import { useRouter } from "@showtime-xyz/universal.router";
 import { Spinner } from "@showtime-xyz/universal.spinner";
 import { Text } from "@showtime-xyz/universal.text";
 import { View } from "@showtime-xyz/universal.view";
+
+import { axios } from "app/lib/axios";
+import { Logger } from "app/lib/logger";
+import { delay } from "app/utilities";
 
 import { stripePromise } from "./stripe";
 
@@ -13,41 +17,71 @@ export const CheckoutReturn = () => {
   const [message, setMessage] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [paymentStatus, setPaymentStatus] = useState<
-    "failed" | "success" | "processing" | null
+    "failed" | "success" | "processing" | null | "notSure"
   >(null);
   const router = useRouter();
   const [time, setTime] = useState(REDIRECT_SECONDS);
 
-  useEffect(() => {
-    setIsLoading(true);
-    stripePromise?.then((stripe) => {
-      const clientSecret = new URLSearchParams(window.location.search).get(
-        "payment_intent_client_secret"
-      );
-      if (!stripe) {
-        return;
-      }
-      if (!clientSecret) {
-        return;
-      }
+  const handlePaymentSuccess = useCallback(() => {
+    setMessage("Payment succeeded!");
+    setPaymentStatus("success");
+    setTimeout(() => {
+      router.replace("/drop");
+    }, REDIRECT_SECONDS * 1000);
+    setInterval(() => {
+      setTime((time) => (time > 0 ? time - 1 : 0));
+    }, 1000);
+  }, [router]);
 
-      stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
+  useEffect(() => {
+    async function confirmPaymentStatus() {
+      try {
+        setIsLoading(true);
+        const stripe = await stripePromise;
+
+        const clientSecret = new URLSearchParams(window.location.search).get(
+          "payment_intent_client_secret"
+        );
+        if (!stripe) {
+          return;
+        }
+        if (!clientSecret) {
+          return;
+        }
+
+        const { paymentIntent } = await stripe.retrievePaymentIntent(
+          clientSecret
+        );
+
         setIsLoading(false);
 
         switch (paymentIntent?.status) {
           case "succeeded":
-            setMessage("Payment succeeded!");
-            setPaymentStatus("success");
-            setTimeout(() => {
-              router.replace("/drop");
-            }, REDIRECT_SECONDS * 1000);
-            setInterval(() => {
-              setTime((time) => time - 1);
-            }, 1000);
+            handlePaymentSuccess();
             break;
           case "processing":
-            setMessage("Your payment is processing.");
-            setPaymentStatus("processing");
+            {
+              setMessage("Your payment is processing.");
+              setPaymentStatus("processing");
+              for (let i = 0; i < 20; i++) {
+                const res = await axios({
+                  method: "GET",
+                  url:
+                    "/v1/payments/status?payment_intent_id=" + paymentIntent.id,
+                });
+                if (res.current_status === "succeeded") {
+                  handlePaymentSuccess();
+                  return;
+                } else if (res.current_status === "payment_failed") {
+                  setPaymentStatus("failed");
+                  setMessage(
+                    "Your payment was not successful, please try again."
+                  );
+                  return;
+                }
+                await delay(3000);
+              }
+            }
             break;
           case "requires_payment_method":
             setPaymentStatus("failed");
@@ -58,13 +92,19 @@ export const CheckoutReturn = () => {
             setMessage("Something went wrong.");
             break;
         }
-      });
-    });
-  }, [router]);
+      } catch (error) {
+        Logger.error("Error confirming payment status", error);
+        setPaymentStatus("notSure");
+        setMessage("Something went wrong.");
+      }
+    }
+
+    confirmPaymentStatus();
+  }, [router, handlePaymentSuccess]);
 
   return (
     <View tw="items-center p-4">
-      {isLoading ? <Spinner size="lg" /> : null}
+      {isLoading ? <Spinner size="large" /> : null}
 
       {paymentStatus === "success" ? (
         <>
@@ -87,6 +127,15 @@ export const CheckoutReturn = () => {
         <>
           <Text tw="text-4xl font-bold">Payment Failed</Text>
           <Text tw="p-8 text-center text-base">{message}</Text>
+        </>
+      ) : null}
+
+      {paymentStatus === "notSure" ? (
+        <>
+          <Text tw="text-4xl font-bold">Something went wrong</Text>
+          <Text tw="p-8 text-center text-base">
+            Please check back later to see if your payment went through.
+          </Text>
         </>
       ) : null}
     </View>
