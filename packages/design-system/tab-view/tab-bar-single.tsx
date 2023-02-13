a20694d97959bd6cf7559a10abf857364e96a18a
Hirbod Mirjavadi <hm@nightstomp.com>
2023-02-13 19:49:25 +0100

    fix: make tab-bar-single z-index 1

--- packages/design-system/tab-view/tab-bar-single.tsx
+++ packages/design-system/tab-view/tab-bar-single.tsx
@@ -41,7 +41,10 @@ export const TabBarSingle = memo<IndependentTabBarProps>(
 
     return (
       <View
-        tw={["no-scrollbar flex-row overflow-x-auto overflow-y-hidden", tw]}
+        tw={[
+          "no-scrollbar z-[1] flex-row overflow-x-auto overflow-y-hidden",
+          tw,
+        ]}
       >
         {routes.map((item, index) => (
           <PressableHover
