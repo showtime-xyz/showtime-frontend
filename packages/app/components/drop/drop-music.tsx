d4cef92ca49d0bd9bf84a9c6a2a5ceaf9bf1f5d3
nishan (o^▽^o) <nishanbende@gmail.com>
2023-02-14 12:22:45 +0530

    nit: rename release date to spotify release date (#1862)

--- packages/app/components/drop/drop-music.tsx
+++ packages/app/components/drop/drop-music.tsx
@@ -103,7 +103,9 @@ const dropValidationSchema = yup.object({
   notSafeForWork: yup.boolean().default(defaultValues.notSafeForWork),
   googleMapsUrl: yup.string().url(),
   radius: yup.number().min(0.01).max(10),
-  releaseDate: yup.date().min(new Date(), "Release date must be in the future"),
+  releaseDate: yup
+    .date()
+    .min(new Date(), "Spotify Release date must be in the future"),
 });
 
 const DROP_FORM_DATA_KEY = "drop_form_local_data_music";
@@ -432,7 +434,7 @@ export const DropMusic = () => {
                         }}
                       >
                         <Text tw="font-bold text-gray-900 dark:text-white">
-                          Release Date
+                          Spotify Release Date
                         </Text>
                         <Text tw="pt-4 text-base text-gray-900 dark:text-white">
                           {(dateValue as Date).toDateString()}
@@ -440,7 +442,7 @@ export const DropMusic = () => {
                       </Pressable>
                     ) : (
                       <Text tw="font-bold text-gray-900 dark:text-white">
-                        Release Date
+                        Spotify Release Date
                       </Text>
                     )}
 
