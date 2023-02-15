de03ca1288b5fc38914f5d7c09dca4cf6869f8fb
Nishan <nishanbende@gmail.com>
2023-02-15 14:52:03 +0530

    fix: get db

--- packages/app/lib/file-storage/file-storage.web.ts
+++ packages/app/lib/file-storage/file-storage.web.ts
@@ -4,42 +4,54 @@ export class FileStorage {
   private readonly dbName: string;
   private db?: IDBDatabase;
 
-  constructor(dbName: string, storeName: string) {
+  constructor(dbName: string) {
     this.dbName = dbName;
-    const request = window.indexedDB.open(this.dbName, 1);
-    request.onupgradeneeded = () => {
-      const db = request.result;
-      if (!db.objectStoreNames.contains(this.dbName)) {
-        db.createObjectStore(this.dbName, { keyPath: "id" });
+    this.getOrInitialiseDB();
+  }
+
+  getOrInitialiseDB() {
+    return new Promise<IDBDatabase>((resolve) => {
+      if (this.db) {
+        resolve(this.db);
+      } else {
+        const request = window.indexedDB.open(this.dbName, 1);
+        request.onupgradeneeded = () => {
+          const db = request.result;
+          if (!db.objectStoreNames.contains(this.dbName)) {
+            db.createObjectStore(this.dbName, { keyPath: "id" });
+          }
+        };
+
+        request.onerror = (error) => {
+          Logger.error(error);
+        };
+
+        request.onsuccess = () => {
+          this.db = request.result;
+          resolve(request.result);
+        };
       }
-    };
-
-    request.onerror = (error) => {
-      Logger.error(error);
-    };
-
-    request.onsuccess = () => {
-      this.db = request.result;
-    };
+    });
   }
 
   public async saveFile(file: File, id: string) {
-    return new Promise<any>((resolve) => {
-      if (this.db) {
-        const transaction = this.db.transaction([this.dbName], "readwrite");
-        const objectStore = transaction.objectStore(this.dbName);
-        const object = { id: id, data: file };
-        objectStore.delete(object.id);
-        const addRequest = objectStore.add(object);
-
-        addRequest.onerror = (e) => {
-          Logger.error(e);
-        };
-
-        addRequest.onsuccess = () => {
-          resolve(object.id);
-        };
+    return new Promise<any>(async (resolve) => {
+      if (!this.db) {
+        this.db = await this.getOrInitialiseDB();
       }
+      const transaction = this.db.transaction([this.dbName], "readwrite");
+      const objectStore = transaction.objectStore(this.dbName);
+      const object = { id: id, data: file };
+      objectStore.delete(object.id);
+      const addRequest = objectStore.add(object);
+
+      addRequest.onerror = (e) => {
+        Logger.error(e);
+      };
+
+      addRequest.onsuccess = () => {
+        resolve(object.id);
+      };
     });
   }
 
@@ -55,21 +67,22 @@ export class FileStorage {
   }
 
   public async getFile(id: any) {
-    return new Promise<File>((resolve) => {
-      if (this.db) {
-        const transaction = this.db.transaction([this.dbName], "readonly");
-        const objectStore = transaction.objectStore(this.dbName);
-        const getRequest = objectStore.get(id);
-
-        getRequest.onerror = (e) => {
-          Logger.error(e);
-        };
-
-        getRequest.onsuccess = () => {
-          const file = (getRequest.result as { id: number; data: File })?.data;
-          resolve(file);
-        };
+    return new Promise<File>(async (resolve) => {
+      if (!this.db) {
+        this.db = await this.getOrInitialiseDB();
       }
+      const transaction = this.db.transaction([this.dbName], "readonly");
+      const objectStore = transaction.objectStore(this.dbName);
+      const getRequest = objectStore.get(id);
+
+      getRequest.onerror = (e) => {
+        Logger.error(e);
+      };
+
+      getRequest.onsuccess = () => {
+        const file = (getRequest.result as { id: number; data: File })?.data;
+        resolve(file);
+      };
     });
   }
 }
