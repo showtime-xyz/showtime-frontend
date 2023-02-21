686fcb851dec45a10c00f2e6c0d61f2a8d2cff4b
nishan (o^▽^o) <nishanbende@gmail.com>
2023-02-21 16:13:52 +0530

    fix: nit copy changes (#1883)

--- packages/app/components/drop/drop-free.tsx
+++ packages/app/components/drop/drop-free.tsx
@@ -150,7 +150,12 @@ export const DropFree = () => {
   }
 
   if (state.status === "success") {
-    return <QRCodeModal contractAddress={state.edition?.contract_address} />;
+    return (
+      <QRCodeModal
+        dropCreated
+        contractAddress={state.edition?.contract_address}
+      />
+    );
   }
 
   const primaryWallet = user.user?.data.profile.primary_wallet;
