440976cd4b9df38b59b2b951124626778f9a5e25
Hirbod Mirjavadi <hm@nightstomp.com>
2023-02-22 10:37:31 +0100

    fix: modal frame on big screen for dropSelect

--- packages/app/screens/drop.tsx
+++ packages/app/screens/drop.tsx
@@ -10,7 +10,7 @@ export const DropScreen = withModalScreen(DropModal, {
   title: "Choose your drop type",
   matchingPathname: "/drop",
   matchingQueryParam: "dropModal",
-  tw: "w-full lg:w-[800px]",
+  tw: "w-full lg:w-[800px] web:lg:pb-14",
   disableBackdropPress: true,
   web_height: `max-h-[100vh] md:max-h-[82vh]`,
   snapPoints: ["100%"],
