afa359fd482246bda4588ef430a2bce566b2a8f2
Nishan <nishanbende@gmail.com>
2023-03-06 15:40:22 +0530

    wip

--- apps/next/src/pages/api/instagram-oauth-redirect.ts
+++ apps/next/src/pages/api/instagram-oauth-redirect.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const redirectURI = req.query.redirectURI as string;
+  const redirectURI = req.query.state as string;
   const authCode = req.query.code as string;
   if (redirectURI && authCode) {
     const decodedRedirectURI = decodeURIComponent(redirectURI);
