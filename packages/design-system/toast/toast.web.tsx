40406d0396f07696aa48b3532b912634001215bd
Alan Toa <37520667+alantoa@users.noreply.github.com>
2023-03-06 20:38:27 +0800

    fix: toast duration & memorize toast options (#1917)
    * Update ToastOptions to remove default duration of 3000s
    
    * fix: improve toast

--- packages/design-system/toast/toast.web.tsx
+++ packages/design-system/toast/toast.web.tsx
@@ -1,3 +1,5 @@
+import { useMemo } from "react";
+
 import { Toaster as RHToaster, toast } from "react-hot-toast";
 
 import { useIsDarkMode } from "@showtime-xyz/universal.hooks";
@@ -10,29 +12,29 @@ const defaultStyle = {
 };
 const Toaster = () => {
   const isDark = useIsDarkMode();
-  return (
-    <RHToaster
-      toastOptions={{
-        style: {
-          ...defaultStyle,
-          ...(isDark
-            ? {
-                background: "#000",
-                color: "#fff",
-                boxShadow:
-                  "0px 0px 2px rgba(255, 255, 255, 0.5), 0px 16px 48px rgba(255, 255, 255, 0.2)",
-              }
-            : {
-                color: "#18181B",
-                background: "#fff",
-                boxShadow:
-                  "0px 12px 16px rgba(0, 0, 0, 0.1), 0px 16px 48px rgba(0, 0, 0, 0.1)",
-              }),
-        },
-      }}
-    />
-  );
+  const toastOptions = useMemo(() => {
+    return {
+      style: {
+        ...defaultStyle,
+        ...(isDark
+          ? {
+              background: "#000",
+              color: "#fff",
+              boxShadow:
+                "0px 0px 2px rgba(255, 255, 255, 0.5), 0px 16px 48px rgba(255, 255, 255, 0.2)",
+            }
+          : {
+              color: "#18181B",
+              background: "#fff",
+              boxShadow:
+                "0px 12px 16px rgba(0, 0, 0, 0.1), 0px 16px 48px rgba(0, 0, 0, 0.1)",
+            }),
+      },
+    };
+  }, [isDark]);
+  return <RHToaster toastOptions={toastOptions} />;
 };
+
 toast.custom = (message: any, options?: any): string => {
   return toast(message, {
     icon: options.web,
