fb7c985d348f6212f2a03e5b012f78f0d83cf246
Nishan <nishanbende@gmail.com>
2023-03-06 18:21:09 +0530

    wip

--- apps/next/src/pages/instagram-oauth-redirect.tsx
+++ apps/next/src/pages/instagram-oauth-redirect.tsx
@@ -1,95 +1,19 @@
 // Web only component. This component never gets rendered on native. It handles the redirect from magic.link
 import { useEffect } from "react";
+import { Platform } from "react-native";
 
-import useSWRMutation from "swr/mutation";
-
-import { Button } from "@showtime-xyz/universal.button";
-import { useRouter } from "@showtime-xyz/universal.router";
-import { Spinner } from "@showtime-xyz/universal.spinner";
-import { Text } from "@showtime-xyz/universal.text";
-import { View } from "@showtime-xyz/universal.view";
-
-import { Logger } from "app/lib/logger";
-import { createParam } from "app/navigation/use-param";
-import { MY_INFO_ENDPOINT } from "app/providers/user-provider";
-
-type Query = {
-  code?: string;
-};
-const { useParam } = createParam<Query>();
+import * as WebBrowser from "expo-web-browser";
 
 const MagicOauthRedirect = () => {
-  const router = useRouter();
-  const [code] = useParam("code");
-  const { trigger, isMutating, error } = useSWRMutation(
-    MY_INFO_ENDPOINT,
-    postInstagramAuthCode
-  );
-
   useEffect(() => {
-    if (code) {
-      console.log("instagram auth code ", code);
-      trigger({ code });
-      window.open(
-        "io.showtime.development://instagram-oauth-redirect?code=" + code,
-        "_self"
-      );
-    }
-  }, [code, trigger]);
+    (async () => {
+      if (Platform.OS === "web") {
+        await WebBrowser.maybeCompleteAuthSession();
+      }
+    })();
+  }, []);
 
-  return (
-    <View tw="flex h-screen w-screen items-center justify-center">
-      {code}
-      {isMutating ? (
-        <View tw="flex-row items-center">
-          <Text tw="text-base font-bold text-gray-900 dark:text-gray-100">
-            Please wait...
-          </Text>
-          <View tw="ml-2">
-            <Spinner size="medium" />
-          </View>
-        </View>
-      ) : error ? (
-        <View tw="h-screen w-full items-center justify-center">
-          <Text tw="text-base font-bold text-gray-900 dark:text-gray-100">
-            Sorry. Something went wrong
-          </Text>
-          <Button
-            tw="mt-4"
-            onPress={() => {
-              router.replace("/");
-            }}
-          >
-            Go back
-          </Button>
-        </View>
-      ) : null}
-    </View>
-  );
+  return null;
 };
 
 export default MagicOauthRedirect;
-
-async function postInstagramAuthCode(
-  _url: string,
-  { arg }: { arg: { code: string } }
-) {
-  if (arg) {
-    try {
-      //   const res = await axios({
-      //     method: "POST",
-      //     url: "/v1/instagram-code",
-      //     data: {
-      //       code: arg.code,
-      //     },
-      //   });
-
-      //   return res;
-      console.log("call backend api");
-      return {};
-    } catch (e) {
-      Logger.error("Payment intent fetch failed ", e);
-      throw e;
-    }
-  }
-}
--- packages/app/components/connect-button/connect-with-instagram.tsx
+++ packages/app/components/connect-button/connect-with-instagram.tsx
@@ -5,26 +5,37 @@ import * as WebBrowser from "expo-web-browser";
 import { useAlert } from "@showtime-xyz/universal.alert";
 import { Button } from "@showtime-xyz/universal.button";
 
+export const redirectURI = Platform.select({
+  web: `${
+    __DEV__
+      ? "http://localhost:3000"
+      : `https://${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}`
+  }/instagram-oauth-redirect`,
+  default: `io.showtime${
+    __DEV__ ? ".development" : ""
+  }://instagram-oauth-redirect`,
+});
+
+let redirectAPIHandler = `https://${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}/api/instagram-oauth-redirect`;
+
 export const ConnectWithInstagram = () => {
   const Alert = useAlert();
   const handleConnectWithInstagram = async () => {
-    const redirectUri = "https://dev.showtime.xyz/instagram-oauth-redirect";
-    const url = `https://www.instagram.com/oauth/authorize?client_id=${
+    const url = `https://api.instagram.com/oauth/authorize?client_id=${
       process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID
     }&redirect_uri=${encodeURIComponent(
-      redirectUri
-    )}&response_type=code&scope=user_profile`;
+      redirectAPIHandler
+    )}&response_type=code&scope=user_profile&state=${encodeURIComponent(
+      redirectURI
+    )}`;
 
-    if (Platform.OS === "web") {
-      window.location.href = url;
-    } else {
-      const res = await WebBrowser.openAuthSessionAsync(url, redirectUri);
-      if (res.type === "success") {
-        let urlObj = new URL(res.url);
-        const code = urlObj.searchParams.get("code");
-        if (code) {
-          console.log("code ", code);
-        }
+    const res = await WebBrowser.openAuthSessionAsync(url, redirectURI);
+    if (res.type === "success") {
+      let urlObj = new URL(res.url);
+      const code = urlObj.searchParams.get("code");
+      if (code) {
+        console.log("code ", code);
+        Alert.alert("Success", "Connected with Instagram");
       } else {
         Alert.alert("Error", "Something went wrong");
       }
