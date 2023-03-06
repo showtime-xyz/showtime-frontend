c819801c50a490992bac9c578f1b122046d2762d
Nishan <nishanbende@gmail.com>
2023-03-06 15:29:24 +0530

    wip

---
+++ apps/next/src/pages/api/instagram-oauth-redirect.ts
@@ -0,0 +1,13 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const redirectURI = req.query.redirectURI as string;
+  const authCode = req.query.code as string;
+  if (redirectURI && authCode) {
+    const decodedRedirectURI = decodeURIComponent(redirectURI);
+    res.redirect(302, decodedRedirectURI + "?code=" + authCode);
+    return;
+  }
+
+  res.status(401).send("Unauthorized");
+}
