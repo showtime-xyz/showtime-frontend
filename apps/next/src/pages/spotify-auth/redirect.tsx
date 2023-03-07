fa79046787d18d65785aae1cd85a0f4201ca922e
Nishan <nishanbende@gmail.com>
2023-03-07 19:12:00 +0530

    spinner

--- apps/next/src/pages/spotify-auth/redirect.tsx
+++ apps/next/src/pages/spotify-auth/redirect.tsx
@@ -2,11 +2,22 @@ import { useEffect } from "react";
 
 import * as WebBrowser from "expo-web-browser";
 
+import { Spinner } from "@showtime-xyz/universal.spinner";
+import { View } from "@showtime-xyz/universal.view";
+
 const SpotifyAuthRedirect = () => {
   useEffect(() => {
     WebBrowser.maybeCompleteAuthSession();
   }, []);
-  return null;
+  return (
+    <View tw="flex-1 items-center justify-center">
+      <Spinner size="large" />
+    </View>
+  );
+};
+
+SpotifyAuthRedirect.getLayout = function getLayout(page) {
+  return page.children;
 };
 
 export default SpotifyAuthRedirect;
