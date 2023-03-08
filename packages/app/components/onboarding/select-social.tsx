4bce2d62d8bf25c08132fa6d742c4ee4f7b5d9d9
Nishan <nishanbende@gmail.com>
2023-03-08 21:51:16 +0530

    twitter connect

--- packages/app/components/onboarding/select-social.tsx
+++ packages/app/components/onboarding/select-social.tsx
@@ -6,13 +6,26 @@ import { Button } from "@showtime-xyz/universal.button";
 import { Text } from "@showtime-xyz/universal.text";
 import { View } from "@showtime-xyz/universal.view";
 
+import { useMagicSocialAuth } from "app/lib/social-logins";
+
 import { InstagramColorful, Twitter } from "design-system/icon";
+import { Spinner } from "design-system/spinner";
 
 import { Challenge } from "./hcaptcha";
 import { OnboardingStepContext } from "./onboarding-context";
 
 export const SelectSocial = () => {
   const { user, redirectUri } = useContext(OnboardingStepContext);
+  const { performMagicAuthWithTwitter, loading } = useMagicSocialAuth();
+
+  const handleConnectTwitter = async () => {
+    const res = await performMagicAuthWithTwitter();
+    console.log("twitter tokens! send it to server ", res);
+  };
+
+  const handleConnectInstagram = async () => {
+    console.log("ig tokens from ig! send it to server ");
+  };
 
   return (
     <MotiView
@@ -49,17 +62,31 @@ export const SelectSocial = () => {
           </Text>
         </View>
         <View tw="mt-16 flex flex-grow-0">
-          <Button size="regular">
+          <Button size="regular" onPress={handleConnectTwitter}>
             <View tw="mr-1">
               <Twitter color="#4A99E9" width={20} height={20} />
             </View>
-            Connect Twitter
+            Connect Twitter{" "}
+            {loading === "twitter" ? (
+              <View tw="absolute right-4 scale-75 justify-center">
+                <Spinner size="small" color="darkgrey" />
+              </View>
+            ) : (
+              <></>
+            )}
           </Button>
-          <Button size="regular" tw="mt-2">
+          <Button size="regular" tw="mt-2" onPress={handleConnectInstagram}>
             <View tw="mr-1.5">
               <InstagramColorful width={20} height={20} />
             </View>
-            Connect Instagram
+            Connect Instagram{" "}
+            {loading === "instagram" ? (
+              <View tw="absolute right-4 scale-75 justify-center">
+                <Spinner size="small" color="darkgrey" />
+              </View>
+            ) : (
+              <></>
+            )}
           </Button>
           <Challenge />
         </View>
--- packages/app/lib/social-logins/index.ts
+++ packages/app/lib/social-logins/index.ts
@@ -1,44 +1,86 @@
+import { useState } from "react";
+
 import { useMagic } from "app/lib/magic";
 import { OAUTH_REDIRECT_URI } from "app/utilities";
 
+import { Logger } from "../logger";
+
 export const useMagicSocialAuth = () => {
   const { magic } = useMagic();
+  // Maybe move spotify auth here?
+  const [loading, setLoading] = useState<
+    "twitter" | "google" | "apple" | "instagram" | null
+  >(null);
+  const [error, setError] = useState(null);
 
   const performMagicAuthWithTwitter = async () => {
+    setError(null);
+    setLoading("twitter");
+
     const scope = ["email"];
     const redirectUri = OAUTH_REDIRECT_URI;
-    return magic.oauth.loginWithPopup({
-      provider: "twitter",
-      scope,
-      redirectURI: redirectUri,
-    });
+    return magic.oauth
+      .loginWithPopup({
+        provider: "twitter",
+        scope,
+        redirectURI: redirectUri,
+      })
+      .catch((err) => {
+        Logger.error(err);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(null);
+      });
   };
 
   const performMagicAuthWithGoogle = async () => {
     const scope = ["email"];
     const redirectUri = OAUTH_REDIRECT_URI;
+    setError(null);
+    setLoading("google");
 
-    return magic.oauth.loginWithPopup({
-      provider: "google",
-      scope,
-      redirectURI: redirectUri,
-    });
+    return magic.oauth
+      .loginWithPopup({
+        provider: "google",
+        scope,
+        redirectURI: redirectUri,
+      })
+      .catch((err) => {
+        Logger.error(err);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(null);
+      });
   };
 
   const performMagicAuthWithApple = async () => {
     const scope = ["email"];
     const redirectUri = OAUTH_REDIRECT_URI;
+    setError(null);
+    setLoading("apple");
 
-    return magic.oauth.loginWithPopup({
-      provider: "apple",
-      scope,
-      redirectURI: redirectUri,
-    });
+    return magic.oauth
+      .loginWithPopup({
+        provider: "apple",
+        scope,
+        redirectURI: redirectUri,
+      })
+      .catch((err) => {
+        Logger.error(err);
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(null);
+      });
   };
 
   return {
     performMagicAuthWithApple,
     performMagicAuthWithGoogle,
     performMagicAuthWithTwitter,
+    loading,
+    error,
   };
 };
