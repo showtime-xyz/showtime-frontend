00d00cf00445d70f0c24c67ffd9c49351bf2a769
Hirbod Mirjavadi <hm@nightstomp.com>
2023-03-08 23:53:54 +0100

    fix: pop once called directly

--- packages/app/components/onboarding/onboarding.tsx
+++ packages/app/components/onboarding/onboarding.tsx
@@ -1,8 +1,9 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useLayoutEffect } from "react";
 
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { AnimatePresence } from "moti";
 
+import { useRouter } from "@showtime-xyz/universal.router";
 import { View } from "@showtime-xyz/universal.view";
 
 import { useUser } from "app/hooks/use-user";
@@ -17,9 +18,19 @@ import { SelectUsername } from "./select-username";
 const { useParam } = createParam<PageQuery>();
 
 export const Onboarding = () => {
-  const { user } = useUser();
+  const { user, isIncompletedProfile } = useUser();
+  const router = useRouter();
 
   const [redirectUri] = useParam("redirectUri");
+
+  // redirect to home if user is not incompleted profile
+  // this should prevent user from going back to onboarding
+  // with the back button
+  useLayoutEffect(() => {
+    if (!isIncompletedProfile) {
+      router.pop();
+    }
+  }, [isIncompletedProfile, router]);
 
   // determine initial step
   const initialStep = useMemo(() => {
@@ -42,6 +53,8 @@ export const Onboarding = () => {
     () => ({ step, setStep, user, redirectUri }),
     [step, user, redirectUri]
   );
+
+  if (!isIncompletedProfile) return null;
 
   return (
     <OnboardingStepContext.Provider value={value}>
