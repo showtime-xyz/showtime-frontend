4415178ff269c5f516795e52e4b69bb22923603a
Nishan <nishanbende@gmail.com>
2023-03-13 06:11:58 +0530

    ig working

--- packages/app/hooks/use-add-magic-social-account.ts
+++ packages/app/hooks/use-add-magic-social-account.ts
@@ -30,41 +30,7 @@ export const useAddMagicSocialAccount = () => {
     MY_INFO_ENDPOINT,
     async (key: string, values: { arg: AddSocialType }) => {
       if (values.arg.type === "instagram") {
-        const scope = "user_profile";
-        const redirectURI = Platform.select({
-          web: `${
-            __DEV__
-              ? "http://localhost:3000"
-              : `https://${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}`
-          }/instagram-oauth-redirect`,
-          default: `io.showtime${
-            __DEV__ ? ".development" : ""
-          }://instagram-oauth-redirect`,
-        });
-
-        let redirectAPIHandler = `https://${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}/api/instagram-oauth-redirect`;
-        const url = `https://api.instagram.com/oauth/authorize?client_id=${
-          process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID
-        }&redirect_uri=${encodeURIComponent(
-          redirectAPIHandler
-        )}&response_type=code&scope=${scope}&state=${encodeURIComponent(
-          redirectURI
-        )}`;
-
-        const res = await WebBrowser.openAuthSessionAsync(url, redirectURI);
-        if (res.type === "success") {
-          let urlObj = new URL(res.url);
-          const code = urlObj.searchParams.get("code");
-          await axios({
-            url: `/v1/profile/accounts/token`,
-            method: "POST",
-            data: {
-              provider: "instagram",
-              access_token: code,
-              scope: [scope],
-            },
-          });
-        }
+        return handleInstagramAccountAdd(Alert, mutate, values);
       } else {
         let res;
         try {
@@ -86,7 +52,6 @@ export const useAddMagicSocialAccount = () => {
             provider_scope: res.oauth.scope,
           };
 
-          console.log("ress ", res);
           try {
             await axios({
               url: `/v2/wallet/add-magic-wallet`,
@@ -132,4 +97,87 @@ export const useAddMagicSocialAccount = () => {
   );
 
   return state;
+};
+
+const handleInstagramAccountAdd = async (
+  Alert: any,
+  mutate,
+  values: { arg: AddSocialType }
+) => {
+  if (values.arg.type === "instagram") {
+    const scope = "user_profile";
+    const redirectURI = Platform.select({
+      web: `${
+        __DEV__
+          ? "http://localhost:3000"
+          : `https://${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}`
+      }/instagram-oauth-redirect`,
+      default: `io.showtime${
+        __DEV__ ? ".development" : ""
+      }://instagram-oauth-redirect`,
+    });
+
+    let redirectAPIHandler = `https://${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}/api/instagram-oauth-redirect`;
+    const url = `https://api.instagram.com/oauth/authorize?client_id=${
+      process.env.NEXT_PUBLIC_INSTAGRAM_CLIENT_ID
+    }&redirect_uri=${encodeURIComponent(
+      redirectAPIHandler
+    )}&response_type=code&scope=${scope}&state=${encodeURIComponent(
+      redirectURI
+    )}`;
+
+    const res = await WebBrowser.openAuthSessionAsync(url, redirectURI);
+    if (res.type === "success") {
+      let urlObj = new URL(res.url);
+      const code = urlObj.searchParams.get("code");
+
+      try {
+        const apiRes = await axios({
+          url: `/v1/profile/accounts/token`,
+          method: "POST",
+          data: {
+            provider: "instagram",
+            code,
+            redirect_uri: redirectAPIHandler,
+            scope: [scope],
+          },
+        });
+        toast.success("Social account added");
+        return apiRes;
+      } catch (error: any) {
+        Logger.error("Add social error", error);
+
+        if (error?.response.status === 420) {
+          Alert.alert(
+            `This account is already linked to another Showtime account`,
+            `Would you like to link it to this account? \n\n By doing so, you will lose your access to the previous account`,
+            [
+              { text: "Cancel" },
+              {
+                text: "Confirm",
+                onPress: async () => {
+                  const apiRes = await axios({
+                    url: `/v1/profile/accounts/token`,
+                    method: "POST",
+                    data: {
+                      provider: "instagram",
+                      code,
+                      redirect_uri: redirectAPIHandler,
+                      scope: [scope],
+                      reassign_wallet: true,
+                    },
+                  });
+
+                  mutate(MY_INFO_ENDPOINT);
+                  toast.success("Social account added");
+
+                  return apiRes;
+                },
+              },
+            ]
+          );
+        }
+      }
+    }
+  }
 };
