edd50d72447375880210d120e9761022687440a7
Hirbod <504909+hirbod@users.noreply.github.com>
2023-03-19 15:15:03 +0100

    fix: enforce captcha gate to fix mismatch with backend (#1959)
    * fix: enforce captcha gate to fix mismatch with backend
    
    * fix: comment

--- packages/app/components/onboarding/hcaptcha/sitekey.ts
+++ packages/app/components/onboarding/hcaptcha/sitekey.ts
@@ -1,3 +1,4 @@
-export const siteKey = process.env.E2E
-  ? "10000000-ffff-ffff-ffff-000000000001"
-  : "e75de5d7-0f0c-4061-8647-65362888979e";
+export const siteKey =
+  process.env.E2E || __DEV__
+    ? "10000000-ffff-ffff-ffff-000000000001"
+    : "e75de5d7-0f0c-4061-8647-65362888979e";
