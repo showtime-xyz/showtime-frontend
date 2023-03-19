cc8d98045ad2cc8ef61d9a16c22cec544ae8aa41
Hirbod <504909+hirbod@users.noreply.github.com>
2023-03-19 20:25:28 +0100

    fix: instagram connect/disconnect (#1960)
    * fix: instagram connect/disconnect
    
    * feat: add mutating status / catch

--- packages/app/components/settings/tabs/account.tsx
+++ packages/app/components/settings/tabs/account.tsx
@@ -47,9 +47,7 @@ export const AccountTab = ({ index = 0 }: AccountTabProps) => {
       <View tw="mt-6 px-4 md:px-0">
         <ConnectSpotify />
         <WalletSocialAccounts />
-        {!instagramProviderId ? (
-          <ConnectInstagram providerId={instagramProviderId} />
-        ) : null}
+        <ConnectInstagram providerId={instagramProviderId} />
       </View>
     </SettingScrollComponent>
   );
@@ -90,12 +88,14 @@ const ConnectSpotify = () => {
   );
 };
 
-const ConnectInstagram = ({ providerId }: { providerId: string }) => {
+const ConnectInstagram = ({ providerId }: { providerId?: string }) => {
   const user = useUser();
   const isDark = useIsDarkMode();
 
-  const { trigger: disconnectInstagram } = useDisconnectInstagram();
-  const { trigger: addSocial } = useAddMagicSocialAccount();
+  const { trigger: disconnectInstagram, isMutating: isDisconnecting } =
+    useDisconnectInstagram();
+  const { trigger: addSocial, isMutating: isConnecting } =
+    useAddMagicSocialAccount();
 
   return (
     <View tw="space-between flex-row items-center justify-between py-2 md:py-3.5">
@@ -116,17 +116,22 @@ const ConnectInstagram = ({ providerId }: { providerId: string }) => {
             : "tertiary"
         }
         onPress={() => {
-          if (user.user?.data.profile.social_login_connections.instagram) {
+          if (
+            user.user?.data.profile.social_login_connections.instagram &&
+            providerId
+          ) {
             disconnectInstagram({
               provider: "instagram",
               providerId,
-            });
+            }).catch(() => {});
           } else {
-            addSocial({ type: "instagram" });
+            addSocial({ type: "instagram" }).catch(() => {});
           }
         }}
       >
-        {user.user?.data.profile.social_login_connections.instagram
+        {isDisconnecting || isConnecting
+          ? "Loading..."
+          : user.user?.data.profile.social_login_connections.instagram
           ? "Disconnect"
           : "Connect"}
       </Button>
