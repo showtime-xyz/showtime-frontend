4e5ed19325eac3d37ced196498449bb0838c9462
Nishan <nishanbende@gmail.com>
2023-03-20 20:40:46 +0530

    wip

--- apps/expo/eas-dev-build-ios.sh
+++ apps/expo/eas-dev-build-ios.sh
@@ -7,14 +7,6 @@ BUILD_OUTPUT=$(STAGE=development eas build --profile simulator --platform ios)
 if echo "$BUILD_OUTPUT" | grep -q "✔ Build finished"; then
   # Extract the URL of the iOS app
   DOWNLOAD_URL=$(echo "$BUILD_OUTPUT" | grep -Eo 'https://expo\.dev/artifacts/eas/[A-Za-z0-9]+/[A-Za-z0-9]+\.tar\.gz')
-
-  # Download the iOS app
-  if [ -n "$DOWNLOAD_URL" ]; then
-    echo "Downloading the iOS app from: $DOWNLOAD_URL"
-    curl -O "$DOWNLOAD_URL"
-  else
-    echo "Failed to extract the download URL from the build output."
-  fi
-else
-  echo "The build process failed."
+  echo "APP_DIR_PATH=${DOWNLOAD_URL}" >>$GITHUB_ENV
+  echo "done!"
 fi
