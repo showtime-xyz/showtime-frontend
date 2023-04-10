67a985b249598d59ceabcc6efefcab5c4c114075
Hirbod Mirjavadi <hm@nightstomp.com>
2023-04-10 02:40:26 +0200

    fix: comment ui issues and flickering

--- packages/app/components/messages/message-row.tsx
+++ packages/app/components/messages/message-row.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useCallback, useRef } from "react";
+import { useMemo, useCallback, useRef, memo } from "react";
 import { Platform } from "react-native";
 
 import { formatDistanceToNowStrict, differenceInSeconds } from "date-fns";
@@ -118,8 +118,8 @@ interface MessageRowProps {
    */
   onUserPress?: (username: string) => void;
 }
-const PlatformSwipeable = Platform.OS === "web" ? View : Swipeable;
-export function MessageRow({
+const PlatformSwipeable = Platform.OS === "web" ? View : memo(Swipeable);
+function MessageRowComponent({
   address,
   username = "",
   userAvatar,
@@ -177,7 +177,7 @@ export function MessageRow({
     return onTagPress
       ? linkifyDescription(
           limitLineBreaks(cleanUserTextInput(content)),
-          "font-bold text-xs text-gray-900 dark:text-gray-100"
+          "font-bold text-sm text-gray-900 dark:text-gray-100"
         )
       : limitLineBreaks(cleanUserTextInput(content));
   }, [content, onTagPress]);
@@ -212,7 +212,7 @@ export function MessageRow({
       rightThreshold={80}
       enabled={!!onDeletePress && Platform.OS !== "web"}
     >
-      <View tw="flex flex-row items-start px-4 py-1">
+      <View tw="flex flex-row items-start bg-white px-4 py-1.5 dark:bg-black md:dark:bg-gray-900">
         {hasParent && <View tw="ml-4" collapsable={true} />}
         <View tw="justify-start">
           <Link href={`/@${username || address}`} tw="-mb-1 -mt-1">
@@ -233,11 +233,11 @@ export function MessageRow({
           </Link>
         </View>
         <View tw={["ml-2 flex-1", isLastReply ? "mb-1" : "-mb-0.5"]}>
-          <Text tw="web:pr-12 pr-7 text-xs text-gray-900 dark:text-gray-100">
+          <Text tw="web:pr-12 pr-7 text-sm text-gray-900 dark:text-gray-100">
             <Link href={`/@${username || address}`}>
               <View tw="mr-3 flex-row items-center">
                 <Text
-                  tw="text-xs font-bold text-gray-900 dark:text-white"
+                  tw="text-sm font-bold text-gray-900 dark:text-white"
                   onPress={handleOnPressUser}
                 >
                   {userNameText}
@@ -304,3 +304,6 @@ export function MessageRow({
     </PlatformSwipeable>
   );
 }
+
+export const MessageRow = memo(MessageRowComponent);
+MessageRow.displayName = "MessageRow";
