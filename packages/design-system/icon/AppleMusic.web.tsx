dd5c8d024933fc6cbe909ad1887dc9d55f1d31a9
Nishan <nishanbende@gmail.com>
2023-04-11 11:47:39 +0530

    fix icons

--- packages/design-system/icon/AppleMusic.web.tsx
+++ packages/design-system/icon/AppleMusic.web.tsx
@@ -1,13 +1,11 @@
 import * as React from "react";
 
-import { Image } from "expo-image";
-
 function SVGAppleMusic(props: { height: number; width: number }) {
   const style = {
     height: props.height ?? 24,
     width: props.width ?? 24,
   };
-  return <Image source={require("./AppleMusic.svg")} style={style} />;
+  return <img src={require("./AppleMusic.svg")} style={style} />;
 }
 
 export default SVGAppleMusic;
