442e74c5ec0e584f9ea99e64df6971a8f1e60bb4
Nishan <nishanbende@gmail.com>
2023-04-11 16:39:28 +0530

    wip

--- apps/next/src/pages/apple-music-auth/apple-music-auth.tsx
+++ apps/next/src/pages/apple-music-auth/apple-music-auth.tsx
@@ -19,17 +19,5 @@ export default function AppleMusicAuth() {
     })();
   }, [getAppleMusicToken]);
 
-  return (
-    <div
-      style={{
-        position: "fixed",
-        zIndex: 10000,
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        background: "white",
-      }}
-    />
-  );
+  return null;
 }
