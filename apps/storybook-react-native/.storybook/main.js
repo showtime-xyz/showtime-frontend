52ebf6fffb9173ddcea05c6867ac2c010db305ae
alantoa <toacncom@gmail.com>
2023-04-12 21:00:36 +0800

    fix: storybook react native

--- apps/storybook-react-native/.storybook/main.js
+++ apps/storybook-react-native/.storybook/main.js
@@ -1,6 +1,5 @@
 module.exports = {
   stories: [
-    "../../../packages/app/**/*.stories.@(js|jsx|ts|tsx|mdx)",
     "../../../packages/design-system/**/*.stories.@(js|jsx|ts|tsx|mdx)",
   ],
   addons: [],
