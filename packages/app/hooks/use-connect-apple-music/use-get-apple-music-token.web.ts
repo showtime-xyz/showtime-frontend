211c9f7ac269fe57ea355a8b5a4493e251b878ad
Nishan <nishanbende@gmail.com>
2023-04-13 14:42:10 +0530

    wip

--- packages/app/hooks/use-connect-apple-music/use-get-apple-music-token.web.ts
+++ packages/app/hooks/use-connect-apple-music/use-get-apple-music-token.web.ts
@@ -6,24 +6,23 @@ import { useScript } from "../use-script";
 import { developerToken } from "./utils";
 
 export const useGetAppleMusicToken = () => {
-  const scriptRes = useScript(
-    "https://js-cdn.music.apple.com/musickit/v3/musickit.js"
-  );
-  const state = useSWRMutation(MY_INFO_ENDPOINT, async () => {
-    if (scriptRes === "ready") {
-      //@ts-ignore
-      const music = await window.MusicKit.configure({
-        developerToken,
-        app: {
-          name: "Showtime",
-          build: "1978.4.1",
-        },
-      });
+  useScript("https://js-cdn.music.apple.com/musickit/v3/musickit.js");
 
-      const token = await music.authorize();
-      return token;
-    }
-  });
+  const fetcher = async () => {
+    //@ts-ignore
+    const music = await window.MusicKit.configure({
+      developerToken,
+      app: {
+        name: "Showtime",
+        build: "1978.4.1",
+      },
+    });
+
+    const token = await music.authorize();
+    return token;
+  };
+
+  const state = useSWRMutation(MY_INFO_ENDPOINT, fetcher);
 
   return { getAppleMusicToken: state.trigger, ...state };
 };
