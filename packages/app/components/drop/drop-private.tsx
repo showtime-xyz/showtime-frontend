e47c01b271c5f02fe61457e253f5ebaa45f0b349
alantoa <toacncom@gmail.com>
2023-05-02 20:32:38 +0800

    update max file size copy

--- packages/app/components/drop/drop-private.tsx
+++ packages/app/components/drop/drop-private.tsx
@@ -346,7 +346,7 @@ export const DropPrivate = () => {
 
                             <View tw="mt-2 hidden md:flex">
                               <Text tw="px-4 text-center text-gray-600 dark:text-gray-200">
-                                Tap to upload a JPG, PNG, GIF, WebM or MP4 file.
+                                {`Tap to upload a JPG, PNG, GIF, WebM or MP4 file.\nMax file size: 30MB`}
                               </Text>
                             </View>
                           </View>
