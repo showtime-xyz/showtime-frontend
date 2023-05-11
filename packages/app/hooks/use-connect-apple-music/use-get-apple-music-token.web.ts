74b6e27f027edb9b57ff183af2ce76c449227d3b
Nishan <nishanbende@gmail.com>
2023-05-11 19:06:48 +0200

    fix: apple auth mobile safari

--- packages/app/hooks/use-connect-apple-music/use-get-apple-music-token.web.ts
+++ packages/app/hooks/use-connect-apple-music/use-get-apple-music-token.web.ts
@@ -3,34 +3,19 @@ import useSWRMutation from "swr/mutation";
 import { useAlert } from "@showtime-xyz/universal.alert";
 
 import { Analytics, EVENTS } from "app/lib/analytics";
-import { axios } from "app/lib/axios";
+import { appleMusicAuth } from "app/lib/apple-music-auth/apple-music-auth";
 import { MY_INFO_ENDPOINT } from "app/providers/user-provider";
 
-import { useScript } from "../use-script";
-
 export const useGetAppleMusicToken = () => {
-  useScript("https://js-cdn.music.apple.com/musickit/v3/musickit.js");
   const Alert = useAlert();
 
   const fetcher = async () => {
-    const res = await axios({
-      url: "/v1/apple_music/get-dev-token",
-      method: "GET",
-    });
-
-    const developerToken = res.developer_token;
-    //@ts-ignore
-    const music = await window.MusicKit.configure({
-      developerToken,
-      app: {
-        name: "Showtime",
-        build: "1978.4.1",
-      },
-    });
-
     try {
-      const token = await music.authorize();
-      return token;
+      if (appleMusicAuth.music) {
+        // @ts-ignore
+        const token = await appleMusicAuth.music.authorize();
+        return token;
+      }
     } catch (e) {
       Analytics.track(EVENTS.APPLE_MUSIC_AUTH_FAILED);
       Alert.alert(
