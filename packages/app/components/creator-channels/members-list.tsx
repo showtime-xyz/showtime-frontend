e3c3485093e6271c7a750938534eeaafada59791
Hirbod Mirjavadi <hm@nightstomp.com>
2023-05-22 18:15:00 +0200

    feat: custom user list for cc

--- packages/app/components/creator-channels/members-list.tsx
+++ packages/app/components/creator-channels/members-list.tsx
@@ -1,8 +1,9 @@
 import { BottomSheetModalProvider } from "@showtime-xyz/universal.bottom-sheet";
 
-import { UserList } from "app/components/user-list";
 import { useFollowingList } from "app/hooks/api/use-follow-list";
 import { createParam } from "app/navigation/use-param";
+
+import { CreatorChannelUserList } from "./user-list";
 
 type Query = {
   channelId: string;
@@ -17,7 +18,7 @@ export const CreatorChannelsMembersModal = () => {
   if (!channelId) return null;
   return (
     <BottomSheetModalProvider>
-      <UserList loading={loading} users={data?.list} />
+      <CreatorChannelUserList loading={loading} users={data?.list} />
     </BottomSheetModalProvider>
   );
 };
