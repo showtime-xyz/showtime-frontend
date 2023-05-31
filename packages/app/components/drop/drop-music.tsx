685af288ce09c8656727d2bfeb614fbb5905577a
Hirbod Mirjavadi <hm@nightstomp.com>
2023-05-31 14:32:30 +0200

    fix: dark mode text color

--- packages/app/components/drop/drop-music.tsx
+++ packages/app/components/drop/drop-music.tsx
@@ -923,10 +923,10 @@ export const DropMusic = () => {
                               tw="flex-1"
                               label={
                                 <View tw="mr-5 flex">
-                                  <Text tw="font-semibold">
+                                  <Text tw="font-semibold dark:text-white">
                                     Explicit visual (18+)
                                   </Text>
-                                  <Text tw="max-w-[100%] pt-1 text-xs">
+                                  <Text tw="max-w-[100%] pt-1 text-xs dark:text-white">
                                     Do not check if your song lyrics are
                                     explicit.
                                   </Text>
