b61b06f87d793e28b00882f07ff29b2fc6eeba0f
alantoa <toacncom@gmail.com>
2023-06-02 02:07:55 +0800

    handle channel unauthenticated

--- packages/app/screens/creator-channels.tsx
+++ packages/app/screens/creator-channels.tsx
@@ -1,10 +1,15 @@
 import { CreatorChannels } from "app/components/creator-channels";
 import { withColorScheme } from "app/components/memo-with-theme";
 import { useIntroducingCreatorChannels } from "app/components/onboarding/introducing-creator-channels";
+import { useUser } from "app/hooks/use-user";
 import { useTrackPageViewed } from "app/lib/analytics";
 
 const CreatorChannelsScreen = withColorScheme(() => {
   useTrackPageViewed({ name: "Creator Channels" });
+  useUser({
+    redirectTo: "/login",
+  });
+
   useIntroducingCreatorChannels();
 
   return <CreatorChannels />;
