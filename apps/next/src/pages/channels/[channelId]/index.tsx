d718111438068c57f4a891c20f4b75dd9116739b
alantoa <toacncom@gmail.com>
2023-06-06 00:35:39 +0800

    add twitter card

--- apps/next/src/pages/channels/[channelId]/index.tsx
+++ apps/next/src/pages/channels/[channelId]/index.tsx
@@ -1,29 +1,28 @@
 import axios from "axios";
 
+import { DEFAULT_AVATAR_PIC } from "design-system/avatar/constants";
+
 export { default } from "app/pages/creator-channels";
 
 export async function getServerSideProps(context) {
   const { channelId } = context.params;
-  const username = "alan";
-  const imageUrl =
-    "https://lh3.googleusercontent.com/G3156TjbanMrCJpQwJL-MgesnfwUDR4Njh2W1awyZUfTlXvW9QcIblzP0v5L3zY6z7TkzhiP0UH1i87Iq7b5e0FnlK3AIQKO81KxIQ";
 
   try {
-    // const res = await axios.get(
-    //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/channels/${channelId}`
-    // );
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/v1/channels/${channelId}`
+    );
+    const username = data.owner?.username || data.owner?.name;
+    const imageUrl = data.owner?.img_url || DEFAULT_AVATAR_PIC;
     const image = `${
       __DEV__
         ? "http://localhost:3000"
         : `https://${process.env.NEXT_PUBLIC_WEBSITE_DOMAIN}`
     }/api/channels?username=${username}&image=${imageUrl}`;
-    console.log(image);
-
     return {
       props: {
         meta: {
-          title: `Showtime`,
-          description: "",
+          title: `Showtime - Creator Channel`,
+          description: `Join @${username} channel on Showtime!`,
           image,
         },
       },
