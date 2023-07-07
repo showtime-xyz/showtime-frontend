51c8107f1be93debc886e7c1841be81073349837
Alan Toa <37520667+alantoa@users.noreply.github.com>
2023-07-07 21:39:00 +0800

    fix: claim sold out status (#2281)

--- packages/app/components/claim/claim-button.tsx
+++ packages/app/components/claim/claim-button.tsx
@@ -43,6 +43,7 @@ export enum ClaimStatus {
 export const getClaimStatus = (edition?: CreatorEditionResponse) => {
   if (!edition) return undefined;
   if (
+    edition.creator_airdrop_edition?.edition_size !== 0 &&
     edition.total_claimed_count >= edition.creator_airdrop_edition?.edition_size
   )
     return ClaimStatus.Soldout;
