626e8c0da8c94d51ff44aa7008860d9004f4023d
Nishan <nishanbende@gmail.com>
2023-07-14 22:14:59 +0530

    fix: sentry upgrade breaking build

--- apps/next/sentry.client.config.js
+++ apps/next/sentry.client.config.js
@@ -3,14 +3,13 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 import { ExtraErrorData, CaptureConsole, Offline } from "@sentry/integrations";
 import * as Sentry from "@sentry/nextjs";
-import { Integrations as TracingIntegrations } from "@sentry/tracing";
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.NEXT_PUBLIC_SENTRY_CLIENT_ENVIRONMENT,
   tracesSampleRate: 0.2,
   integrations: [
-    new TracingIntegrations.BrowserTracing({
+    new Sentry.BrowserTracing({
       tracingOrigins: [process.env.NEXT_PUBLIC_BACKEND_URL],
     }),
     new ExtraErrorData(),
