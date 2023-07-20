f87bff91d9282daeba0d5847974cb1ad9e549547
Nishan <nishanbende@gmail.com>
2023-07-21 03:42:42 +0530

    add base and linea chain

--- packages/app/providers/wallet-provider.web.tsx
+++ packages/app/providers/wallet-provider.web.tsx
@@ -8,9 +8,58 @@ import * as allChains from "wagmi/chains";
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
-// @ts-ignore
-const allChainsArray = Object.keys(allChains).map((key) => allChains[key]);
+const baseChain = {
+  id: 8453,
+  name: "Base",
+  network: "mainnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://developer-access-mainnet.base.org"],
+    },
+    public: {
+      http: ["https://developer-access-mainnet.base.org"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Basescan", url: "https://basescan.org" },
+  },
+};
 
+const lineaChain = {
+  id: 59144,
+  name: "Linea",
+  network: "mainnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Ether",
+    symbol: "ETH",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://linea-mainnet.infura.io/v3"],
+    },
+    public: {
+      http: ["https://linea-mainnet.infura.io/v3"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Lineascan", url: "https://lineascan.build/" },
+  },
+};
+
+const allChainsArray = [
+  // @ts-ignore
+  ...Object.keys(allChains).map((key) => allChains[key]),
+  baseChain,
+  lineaChain,
+];
+
+console.log("Efeff ", allChainsArray);
 const { chains, publicClient, webSocketPublicClient } = configureChains(
   allChainsArray,
   [
