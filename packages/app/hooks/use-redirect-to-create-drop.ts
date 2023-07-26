191289d627528049c6afd75c4a60d934bbc9efde
alantoa <toacncom@gmail.com>
2023-07-27 02:53:56 +0800

    remove drop limit

--- packages/app/hooks/use-redirect-to-create-drop.ts
+++ packages/app/hooks/use-redirect-to-create-drop.ts
@@ -3,77 +3,58 @@ import { Platform } from "react-native";
 
 import { MMKV } from "react-native-mmkv";
 
-import { useAlert } from "@showtime-xyz/universal.alert";
 import { useRouter } from "@showtime-xyz/universal.router";
 
 import { useOnboardingPromise } from "app/components/onboarding";
-import { useUser } from "app/hooks/use-user";
 import { useLogInPromise } from "app/lib/login-promise";
 
 const store = new MMKV();
 const STORE_KEY = "showExplanationv2";
 export const useRedirectToCreateDrop = () => {
-  const { isAuthenticated, user } = useUser();
   const { loginPromise } = useLogInPromise();
   const { onboardingPromise } = useOnboardingPromise();
 
   const router = useRouter();
-  const Alert = useAlert();
 
   const redirectToCreateDrop = useCallback(async () => {
-    if (isAuthenticated && user?.data.can_create_drop === false) {
-      const timeRemaining = 24 - new Date().getUTCHours();
-      Alert.alert(
-        "Wow, you love drops!",
-        `Only one drop per day is allowed.\n\nCome back in ${timeRemaining} hours!`
+    // check if user is logged in
+    await loginPromise();
+    // check if user has completed onboarding
+    await onboardingPromise();
+
+    if (store.getBoolean(STORE_KEY)) {
+      router.push(
+        Platform.select({
+          native: "/dropExplanation",
+          web: {
+            pathname: router.pathname,
+            query: { ...router.query, dropExplanationModal: true },
+          } as any,
+        }),
+        Platform.select({
+          native: "/dropExplanation",
+          web: router.asPath === "/" ? "/dropExplanation" : router.asPath,
+        }),
+        { shallow: true }
       );
     } else {
-      // check if user is logged in
-      await loginPromise();
-      // check if user has completed onboarding
-      await onboardingPromise();
-
-      if (store.getBoolean(STORE_KEY)) {
-        router.push(
-          Platform.select({
-            native: "/dropExplanation",
-            web: {
-              pathname: router.pathname,
-              query: { ...router.query, dropExplanationModal: true },
-            } as any,
-          }),
-          Platform.select({
-            native: "/dropExplanation",
-            web: router.asPath === "/" ? "/dropExplanation" : router.asPath,
-          }),
-          { shallow: true }
-        );
-      } else {
-        // redirect to create drop
-        router.push(
-          Platform.select({
-            native: "/drop",
-            web: {
-              pathname: router.pathname,
-              query: { ...router.query, dropModal: true },
-            } as any,
-          }),
-          Platform.select({
-            native: "/drop",
-            web: router.asPath === "/" ? "/drop" : router.asPath,
-          }),
-          { shallow: true }
-        );
-      }
+      // redirect to create drop
+      router.push(
+        Platform.select({
+          native: "/drop",
+          web: {
+            pathname: router.pathname,
+            query: { ...router.query, dropModal: true },
+          } as any,
+        }),
+        Platform.select({
+          native: "/drop",
+          web: router.asPath === "/" ? "/drop" : router.asPath,
+        }),
+        { shallow: true }
+      );
     }
-  }, [
-    Alert,
-    isAuthenticated,
-    loginPromise,
-    onboardingPromise,
-    router,
-    user?.data.can_create_drop,
-  ]);
+  }, [loginPromise, onboardingPromise, router]);
 
   return redirectToCreateDrop;
 };
