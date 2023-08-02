ec4495b490803b6ed1014d2d94bf9897b9193dc3
alantoa <toacncom@gmail.com>
2023-08-03 01:56:33 +0800

    revert modal code

--- packages/app/components/creator-channels/missing-star-drop-modal.tsx
+++ packages/app/components/creator-channels/missing-star-drop-modal.tsx
@@ -1,9 +1,8 @@
-import { Platform, Modal, StyleSheet } from "react-native";
+import { Platform } from "react-native";
 
 import { Button } from "@showtime-xyz/universal.button";
 import { Image } from "@showtime-xyz/universal.image";
-import { ModalHeader } from "@showtime-xyz/universal.modal";
-// import { Modal } from "@showtime-xyz/universal.modal";
+import { Modal } from "@showtime-xyz/universal.modal";
 import { useRouter } from "@showtime-xyz/universal.router";
 import { Text } from "@showtime-xyz/universal.text";
 import { View } from "@showtime-xyz/universal.view";
@@ -19,75 +18,69 @@ export const MissingStarDropModal = ({ isOpen, onClose, close }: Props) => {
   const router = useRouter();
   return (
     <Modal
-      animationType="fade"
-      transparent={true}
+      mobile_snapPoints={[380]}
       visible={isOpen}
-      onRequestClose={close}
+      onClose={onClose}
+      close={close}
     >
-      <View tw="bg-black/30" style={StyleSheet.absoluteFillObject} />
-      <View tw="flex-1 items-center justify-center px-4">
-        <View tw="max-w-[380px] rounded-3xl bg-white dark:bg-gray-900">
-          <ModalHeader title="Star Drops" onClose={close} />
-          <View tw="items-center justify-center px-4 pb-6">
-            <Image
-              source={
-                Platform.OS === "web"
-                  ? "https://media.showtime.xyz/assets/st-logo.png"
-                  : require("app/components/assets/st-logo.png")
-              }
-              width={50}
-              height={50}
-            />
-            <View tw="pb-6 pt-8">
-              <Text tw="text-base text-gray-900 dark:text-white">
-                Star Drops are collectibles your fans pay for that unlock your
-                channel content.
-              </Text>
-              <View tw="h-8" />
-              <Text tw="text-base text-gray-900 dark:text-white">
-                <Text tw="font-bold">Create a Star Drop </Text>
-                to earn money and to allow users to see your channel updates.
-              </Text>
-            </View>
-            <Button
-              variant="primary"
-              size="regular"
-              onPress={() => {
-                close();
-                if (Platform.OS !== "web") {
-                  router.push("/drop/free");
-                } else {
-                  router.replace("/drop/free");
-                }
-              }}
-              tw="w-full"
-              style={[
-                {
-                  backgroundColor: "transparent",
-                },
-              ]}
-            >
-              <ButtonGoldLinearGradient />
-              <View tw="w-full flex-row items-center justify-center">
-                <View>
-                  <Image
-                    source={
-                      Platform.OS === "web"
-                        ? "https://media.showtime.xyz/assets/st-logo.png"
-                        : require("app/components/assets/st-logo.png")
-                    }
-                    width={24}
-                    height={24}
-                  />
-                </View>
-
-                <Text tw={["ml-2 text-base font-semibold text-black"]}>
-                  Collect Star Drop
-                </Text>
-              </View>
-            </Button>
-          </View>
+      <View tw="min-h- items-center px-4 pb-4 md:px-8">
+        <Image
+          source={
+            Platform.OS === "web"
+              ? "https://media.showtime.xyz/assets/st-logo.png"
+              : require("app/components/assets/st-logo.png")
+          }
+          width={50}
+          height={50}
+        />
+        <View tw="py-6">
+          <Text tw="text-base text-gray-900 dark:text-white">
+            Star Drops are collectibles your fans pay for that unlock your
+            channel content.
+          </Text>
+          <View tw="h-8" />
+          <Text tw="text-base text-gray-900 dark:text-white">
+            <Text tw="font-bold">Create a Star Drop </Text>
+            to earn money and to allow users to see your channel updates.
+          </Text>
         </View>
+        <Button
+          variant="primary"
+          size="regular"
+          onPress={() => {
+            close();
+            if (Platform.OS !== "web") {
+              router.push("/drop/free");
+            } else {
+              router.replace("/drop/free");
+            }
+          }}
+          tw="w-full"
+          style={[
+            {
+              backgroundColor: "transparent",
+            },
+          ]}
+        >
+          <ButtonGoldLinearGradient />
+          <View tw="w-full flex-row items-center justify-center">
+            <View>
+              <Image
+                source={
+                  Platform.OS === "web"
+                    ? "https://media.showtime.xyz/assets/st-logo.png"
+                    : require("app/components/assets/st-logo.png")
+                }
+                width={24}
+                height={24}
+              />
+            </View>
+
+            <Text tw={["ml-2 text-base font-semibold text-black"]}>
+              Collect Star Drop
+            </Text>
+          </View>
+        </Button>
       </View>
     </Modal>
   );
