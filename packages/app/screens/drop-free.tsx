a6d5440972d6a036c3426102d131be3ae5f1b54b
Nishan <nishanbende@gmail.com>
2023-08-08 20:04:27 +0530

    increase drop form height

--- packages/app/screens/drop-free.tsx
+++ packages/app/screens/drop-free.tsx
@@ -6,7 +6,7 @@ export const DropFreeScreen = withModalScreen(DropFree, {
   title: "Create a Drop",
   matchingPathname: "/drop/free",
   matchingQueryParam: "dropFree",
-  tw: "w-full lg:w-[800px] !pb-0",
+  tw: "w-full lg:w-[800px] !pb-0 md:max-h-[98vh]",
   disableBackdropPress: true,
   snapPoints: ["100%"],
   headerShown: false,
