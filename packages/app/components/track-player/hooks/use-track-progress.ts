ea9ef383fb10c3db435613e5cda969ab9fffc161
Hirbod Mirjavadi <hm@nightstomp.com>
2023-08-11 19:26:28 +0200

    chore: refactor track player

--- packages/app/components/track-player/hooks/use-track-progress.ts
+++
@@ -1,8 +0,0 @@
-import { useSnapshot } from "valtio";
-
-import { progressState } from "../store";
-
-export const useTrackProgress = (id: number | string) => {
-  const state = useSnapshot(progressState);
-  return state.tracks[id.toString()] || 0;
-};
