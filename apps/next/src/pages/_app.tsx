52d3bce7c5a85df368a5b29b9d20f88446e645a2
Nishan <nishanbende@gmail.com>
2023-08-14 08:06:03 +0530

    test

--- apps/next/src/pages/_app.tsx
+++ apps/next/src/pages/_app.tsx
@@ -65,6 +65,8 @@ Sentry.init({
   environment: process.env.STAGE,
 });
 
+console.log("hello from dev");
+
 function App({ Component, pageProps, router }: AppProps) {
   const meta = pageProps.meta;
 
