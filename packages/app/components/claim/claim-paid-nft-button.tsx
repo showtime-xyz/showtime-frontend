d8346edd2baca1d6b5a0907a16e6e99c15dd0d9a
alantoa <toacncom@gmail.com>
2023-08-15 23:15:12 +0800

    fix price is covered up

--- packages/app/components/claim/claim-paid-nft-button.tsx
+++ packages/app/components/claim/claim-paid-nft-button.tsx
@@ -3,7 +3,6 @@ import { Platform } from "react-native";
 
 import { LinearGradient } from "expo-linear-gradient";
 import { BorderlessButton } from "react-native-gesture-handler";
-import { useSWRConfig } from "swr";
 import * as Tooltip from "universal-tooltip";
 
 import { Button, ButtonProps } from "@showtime-xyz/universal.button";
@@ -22,6 +21,7 @@ import { ButtonGoldLinearGradient } from "app/components/gold-gradient";
 import { CreatorEditionResponse } from "app/hooks/use-creator-collection-detail";
 import { fetcher } from "app/hooks/use-infinite-list-query";
 import { useRedirectDropImageShareScreen } from "app/hooks/use-redirect-to-drop-image-share-screen";
+import { Logger } from "app/lib/logger";
 import { useLogInPromise } from "app/lib/login-promise";
 import { getCurrencyPrice } from "app/utilities";
 
@@ -72,6 +72,7 @@ const GoldButton = memo(function GoldButton({
     }
     await loginPromise();
     if (Platform.OS !== "web") {
+      Logger.error("Purchase only for web.");
       return;
     }
     if (Platform.OS === "web") {
@@ -103,6 +104,7 @@ const GoldButton = memo(function GoldButton({
       <PressableHover
         tw={["h-6 w-24 items-center justify-center rounded-full"]}
         onPress={() => onHandlePayment()}
+        style={style}
         {...rest}
       >
         <ButtonGoldLinearGradient />
@@ -114,7 +116,7 @@ const GoldButton = memo(function GoldButton({
   }
   if (type === "feed") {
     return (
-      <PressableHover onPress={onHandlePayment} {...rest}>
+      <PressableHover onPress={onHandlePayment} style={style} {...rest}>
         <View tw={"h-14 w-14 items-center justify-center rounded-full"}>
           <ButtonGoldLinearGradient
             style={{ transform: [{ rotate: "84deg" }] }}
@@ -148,6 +150,7 @@ const GoldButton = memo(function GoldButton({
       <PressableHover
         tw="h-12 rounded-full bg-black p-4 dark:bg-white"
         onPress={onHandlePayment}
+        style={style}
         {...rest}
       >
         <View tw="w-full flex-row items-center justify-center">
