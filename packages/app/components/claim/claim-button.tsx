a929f7efa1765be368c15026bc392e10e76cd71a
Alan Toa <37520667+alantoa@users.noreply.github.com>
2023-08-17 21:15:54 +0800

    Update packages/app/components/claim/claim-button.tsx
    Co-authored-by: Hirbod <504909+hirbod@users.noreply.github.com>

--- packages/app/components/claim/claim-button.tsx
+++ packages/app/components/claim/claim-button.tsx
@@ -179,7 +179,7 @@ export const ClaimButton = ({
       <Button {...buttonProps} onPress={handleRaffleResultPress}>
         <>
           <Text tw={["font-semibold text-white", LABEL_SIZE_TW[size]]}>
-            {isPaidGated ? "Announce" : "Announce your raffle"}
+            {isPaidGated ? "Announce winner" : "Announce your raffle"}
           </Text>
         </>
       </Button>
