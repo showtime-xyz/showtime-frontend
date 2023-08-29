b32087dccd266ad3980acb9d7853afcdf4f746c5
alantoa <toacncom@gmail.com>
2023-08-29 23:46:52 +0800

    add deeplink url

--- apps/next/src/pages/notifications/index.tsx
+++ apps/next/src/pages/notifications/index.tsx
@@ -1,1 +1,11 @@
 export { default } from "app/pages/notifications";
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      meta: {
+        deeplinkUrl: `/notifications`,
+      },
+    },
+  };
+}
