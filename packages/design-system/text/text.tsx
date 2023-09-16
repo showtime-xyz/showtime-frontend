d3d870412f29a4c443d70053b896241969dfd326
Hirbod Mirjavadi <hm@nightstomp.com>
2023-09-16 14:43:05 +0200

    fix: naming

--- packages/design-system/text/text.tsx
+++ packages/design-system/text/text.tsx
@@ -5,7 +5,7 @@ import { NativeText } from "react-native/Libraries/Text/TextNativeComponent";
 
 import { styled } from "@showtime-xyz/universal.tailwind";
 
-const Text = NativeText as ComponentType<TextProps>;
-const StyledText = styled(Text);
+const TextElement = NativeText as ComponentType<TextProps>;
+const StyledText = styled(TextElement);
 
 export { StyledText as Text };
