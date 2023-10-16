88a39bb4654fab90580babe03dd65f021ea68c14
Nishan <nishanbende@gmail.com>
2023-10-16 18:06:44 +0530

    cleanup

--- packages/app/components/creator-token/buy-creator-token.tsx
+++ packages/app/components/creator-token/buy-creator-token.tsx
@@ -94,7 +94,7 @@ export const BuyCreatorToken = () => {
         >
           {pollBuyToken.status === "pending" || approveToken.isMutating
             ? "Loading..."
-            : "Buy"}
+            : "Approve & Buy"}
         </Button>
       );
     }
@@ -110,7 +110,7 @@ export const BuyCreatorToken = () => {
         <Text tw="text-2xl font-semibold">tokens</Text>
       </View>
       <View tw="flex-row items-center pt-4" style={{ columnGap: 2 }}>
-        <LockBadge width={14} height={14} color={colors.black} />
+        <LockBadge width={14} height={14} color={colors.gray[600]} />
         <Text tw="text-gray-600">Unlocks exclusive channel content</Text>
       </View>
       <View tw="mt-4 rounded-3xl border-[1px] border-gray-300 p-8">
@@ -118,17 +118,21 @@ export const BuyCreatorToken = () => {
           <Avatar size={100} url={profileData?.data?.profile.img_url} />
           <View style={{ rowGap: 16 }}>
             <View tw="flex-row" style={{ columnGap: 8 }}>
-              <View tw="items-start self-start rounded-md bg-black px-2">
-                <Text tw="text-white">USDC</Text>
+              <View tw="items-start self-start rounded-md bg-black p-2">
+                <Text tw="text-xs text-white">USDC</Text>
               </View>
               {/* <View tw="items-start self-start rounded-sm bg-blue-200 px-2">
                 <Text>ETH</Text>
               </View> */}
             </View>
             <View>
-              <Text tw="text-4xl font-semibold">
-                ${priceToBuyNext.data?.displayPrice}
-              </Text>
+              {priceToBuyNext.isValidating ? (
+                <Skeleton width={100} height={32} />
+              ) : (
+                <Text tw="text-4xl font-semibold">
+                  ${priceToBuyNext.data?.displayPrice}
+                </Text>
+              )}
             </View>
             {/* <View>
               <Text tw="font-semibold text-green-500">^ $2.49 (25%) Month</Text>
@@ -139,14 +143,18 @@ export const BuyCreatorToken = () => {
           tw="mt-4 flex-row items-center justify-between"
           style={{ columnGap: 16 }}
         >
-          <Button tw="flex-1" style={{ backgroundColor: colors.cyan[400] }}>
-            <Text style={{ color: colors.black }}>Buy</Text>
+          <Button tw="flex-1" style={{ backgroundColor: "#08F6CC" }}>
+            <Text style={{ color: colors.black }} tw="font-semibold">
+              Buy
+            </Text>
           </Button>
-          <Button tw="flex-1" style={{ backgroundColor: colors.blue[200] }}>
-            <Text style={{ color: colors.black }}>Sell</Text>
+          <Button tw="flex-1" style={{ backgroundColor: colors.gray[200] }}>
+            <Text style={{ color: colors.black }} tw="font-semibold">
+              Sell
+            </Text>
           </Button>
           <View>
-            <InformationCircle color={colors.blue[300]} />
+            <InformationCircle color={colors.gray[400]} />
           </View>
         </View>
       </View>
@@ -163,7 +171,7 @@ export const BuyCreatorToken = () => {
           <Text tw="flex-2 text-gray-700">Quantity to buy:</Text>
           <View tw="w-4" />
           <View tw="flex-1 flex-row rounded-sm border-[1px] border-gray-200">
-            <View tw="flex-1 items-center border-gray-200 p-4 text-center">
+            <View tw="flex-1 border-gray-200 p-4 text-center">
               <Text>{tokenAmount}</Text>
             </View>
             <Pressable
