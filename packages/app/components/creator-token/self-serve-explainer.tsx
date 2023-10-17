d1b9770cdd7fb071ad99731194d6442954a10ce4
alantoa <toacncom@gmail.com>
2023-10-18 01:19:25 +0800

    finish self serve explainer screen

--- packages/app/components/creator-token/self-serve-explainer.tsx
+++ packages/app/components/creator-token/self-serve-explainer.tsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useState, useCallback } from "react";
 import { Platform } from "react-native";
 
-import { Avatar } from "@showtime-xyz/universal.avatar";
+import { useSWRConfig } from "swr";
+
 import { Button } from "@showtime-xyz/universal.button";
 import { useIsDarkMode } from "@showtime-xyz/universal.hooks";
 import { Flip, ShowtimeRounded } from "@showtime-xyz/universal.icon";
@@ -12,16 +13,43 @@ import { colors } from "@showtime-xyz/universal.tailwind";
 import { Text } from "@showtime-xyz/universal.text";
 import { View } from "@showtime-xyz/universal.view";
 
+import { USER_PROFILE_KEY } from "app/hooks/api-hooks";
 import { useCreatorTokenDeployStatus } from "app/hooks/creator-token/use-creator-token-deploy-status";
 import { useCreatorTokenOptIn } from "app/hooks/creator-token/use-creator-token-opt-in";
+import { useMatchMutate } from "app/hooks/use-match-mutate";
 import { useRedirectToCreatorTokensShare } from "app/hooks/use-redirect-to-creator-tokens-share-screen";
 import { useUser } from "app/hooks/use-user";
+import { axios } from "app/lib/axios";
+import { useFilePicker } from "app/lib/file-picker";
+import { Logger } from "app/lib/logger";
+import { MY_INFO_ENDPOINT } from "app/providers/user-provider";
+import { getFileFormData } from "app/utilities";
+
+import { toast } from "design-system/toast";
+
+import { MediaCropper } from "../media-cropper";
+import { Preview, getLocalFileURI } from "../preview";
 
 export const SelfServeExplainer = () => {
+  const { mutate } = useSWRConfig();
+  const matchMutate = useMatchMutate();
   const isDark = useIsDarkMode();
-  const router = useRouter();
   const { user } = useUser();
   const { top } = useSafeAreaInsets();
+  const defaultProfilePic = user?.data.profile?.img_url;
+  const [profilePic, setProfilePic] = useState<string | undefined | File>(
+    defaultProfilePic
+  );
+  const [value, setValue] = useState<File | undefined>();
+
+  const [selectedImg, setSelectedImg] = useState<
+    string | File | undefined | null
+  >(null);
+  const [currentCropField, setCurrentCropField] = useState<
+    null | "coverPicture" | "profilePicture"
+  >(null);
+
+  const pickFile = useFilePicker();
   const { trigger: deployContract, isMutating } = useCreatorTokenOptIn();
   const redirectToCreatorTokensShare = useRedirectToCreatorTokensShare();
   const creatorTokenDeployStatus = useCreatorTokenDeployStatus({
@@ -34,103 +62,168 @@ export const SelfServeExplainer = () => {
       }
     },
   });
-
   const loading = creatorTokenDeployStatus.status === "pending" || isMutating;
-  console.log("creatorTokenDeployStatus", creatorTokenDeployStatus.status);
+  const onSubmit = useCallback(async () => {
+    if (value) {
+      const formData = new FormData();
+      const profilePictureFormData = await getFileFormData(value);
+      if (profilePictureFormData) {
+        formData.append("image", profilePictureFormData);
+
+        try {
+          await axios({
+            url: "/v1/profile/photo",
+            method: "POST",
+            headers: {
+              "Content-Type": `multipart/form-data`,
+            },
+            data: formData,
+          });
+
+          // TODO: optimise to make fewer API calls!
+          mutate(MY_INFO_ENDPOINT);
+          matchMutate(
+            (key) => typeof key === "string" && key.includes(USER_PROFILE_KEY)
+          );
+        } catch (e) {
+          toast.error("Failed to upload profile picture. Please try again");
+          Logger.error("Failed to upload profile picture.", e);
+        }
+      }
+    }
+    await deployContract();
+    creatorTokenDeployStatus.pollDeployStatus();
+  }, [creatorTokenDeployStatus, deployContract, matchMutate, mutate, value]);
+
+  Logger.log("creatorTokenDeployStatus", creatorTokenDeployStatus.status);
 
   return (
-    <View
-      tw="md:max-w-screen-content px-5"
-      style={{
-        paddingTop: Platform.select({ web: 0, default: top + 48 }),
-        paddingHorizontal: 20,
-      }}
-    >
-      <Text tw="text-xl font-bold text-gray-900 dark:text-white">
-        Inviting onchain artists like you first.
-      </Text>
-      <View tw="web:py-0 py-4">
-        <View tw="flex-row items-center py-2">
-          <Text tw="mr-1 text-sm text-gray-900 dark:text-white">
-            Introducing
-          </Text>
-          <ShowtimeRounded color={colors.gray[900]} width={14} height={14} />
-          <Text tw="text-sm font-bold text-gray-900 dark:text-white">
-            {" "}
-            Creator Tokens.
-          </Text>
-        </View>
-      </View>
-
-      <View tw="mt-6 items-center rounded-3xl border border-gray-200 px-4 py-6">
-        <Pressable tw="">
-          <Avatar url={user?.data.profile.img_url} size={112} />
-          <View tw="absolute bottom-0 left-0 right-0 top-0 flex-row items-center justify-center rounded-full bg-black/30">
-            <Flip color="#fff" width={20} height={20} />
-            <Text tw="ml-1 text-base font-bold text-white">Replace</Text>
-          </View>
-        </Pressable>
-        <View tw="rounded-4xl mb-2 mt-4 w-full border border-gray-200 px-10 py-4">
-          <View tw="flex-row items-center justify-between gap-4">
-            <View tw="flex-1 items-center">
-              <Text tw="text-xs text-gray-500">TOKEN</Text>
-              <View tw="h-2" />
-              <Text tw="text-base font-bold text-gray-900 dark:text-white">
-                $1
-              </Text>
-              <Button
-                tw="mt-2.5"
-                style={{ backgroundColor: "#08F6CC", width: "100%" }}
-              >
-                <>
-                  <Text tw="text-base font-bold text-gray-900">Buy</Text>
-                </>
-              </Button>
-            </View>
-            <View tw="flex-1 items-center justify-center">
-              <Text tw="text-xs text-gray-500">COLLECTORS</Text>
-              <View tw="h-2" />
-              <Text tw="text-base font-bold text-gray-900 dark:text-white">
-                0
-              </Text>
-              <Button
-                tw="mt-2.5"
-                style={{ backgroundColor: "#FD749D", width: "100%" }}
-              >
-                <>
-                  <Text tw="text-base font-bold text-gray-900">Sell</Text>
-                </>
-              </Button>
-            </View>
-          </View>
-        </View>
-        <View tw="pb-6 pt-4">
-          <Text tw="text-13 text-gray-900 dark:text-white">
-            Your Creator Token is a collectible, and your profile picture will
-            be its image. You can update this later.
-          </Text>
-        </View>
-        <Button
-          size="regular"
-          tw={`w-full ${loading ? "opacity-50" : ""}`}
-          disabled={loading}
-          onPress={async () => {
-            await deployContract();
-            creatorTokenDeployStatus.pollDeployStatus();
-          }}
-        >
-          <>
-            <ShowtimeRounded
-              color={isDark ? colors.gray[900] : colors.white}
-              width={20}
-              height={20}
-            />
-            <Text tw="ml-1 text-base font-bold text-white dark:text-gray-900">
-              {loading ? "Creating your Token..." : "Create your Token"}
+    <>
+      <View
+        tw="px-5"
+        style={{
+          paddingTop: Platform.select({ web: 0, default: top + 48 }),
+          paddingHorizontal: 20,
+        }}
+      >
+        <Text tw="text-xl font-bold text-gray-900 dark:text-white">
+          Inviting onchain artists like you first.
+        </Text>
+        <View tw="web:py-0 py-4">
+          <View tw="flex-row items-center py-2">
+            <Text tw="mr-1 text-sm text-gray-900 dark:text-white">
+              Introducing
             </Text>
-          </>
-        </Button>
+            <ShowtimeRounded color={colors.gray[900]} width={14} height={14} />
+            <Text tw="text-sm font-bold text-gray-900 dark:text-white">
+              {" "}
+              Creator Tokens.
+            </Text>
+          </View>
+        </View>
+
+        <View tw="mt-6 items-center rounded-3xl border border-gray-200 px-4 py-6">
+          <Pressable
+            onPress={async () => {
+              const file = await pickFile({
+                mediaTypes: "image",
+                option: { allowsEditing: true, aspect: [1, 1] },
+              });
+
+              setSelectedImg(getLocalFileURI(file.file));
+              setCurrentCropField("profilePicture");
+            }}
+            tw="overflow-hidden rounded-full"
+          >
+            <Preview file={profilePic} width={112} height={112} />
+            <View tw="absolute bottom-0 left-0 right-0 top-0 flex-row items-center justify-center rounded-full bg-black/30">
+              <Flip color="#fff" width={20} height={20} />
+              <Text tw="ml-1 text-base font-bold text-white">Replace</Text>
+            </View>
+          </Pressable>
+          <View tw="rounded-4xl mb-2 mt-4 w-full border border-gray-200 px-10 py-4">
+            <View tw="flex-row items-center justify-between gap-4">
+              <View tw="flex-1 items-center">
+                <Text tw="text-xs text-gray-500">TOKEN</Text>
+                <View tw="h-2" />
+                <Text tw="text-base font-bold text-gray-900 dark:text-white">
+                  $1
+                </Text>
+                <Button
+                  tw="mt-2.5"
+                  style={{ backgroundColor: "#08F6CC", width: "100%" }}
+                  disabled
+                >
+                  <>
+                    <Text tw="text-base font-bold text-gray-900">Buy</Text>
+                  </>
+                </Button>
+              </View>
+              <View tw="flex-1 items-center justify-center">
+                <Text tw="text-xs text-gray-500">COLLECTORS</Text>
+                <View tw="h-2" />
+                <Text tw="text-base font-bold text-gray-900 dark:text-white">
+                  0
+                </Text>
+                <Button
+                  tw="mt-2.5"
+                  style={{ backgroundColor: "#FD749D", width: "100%" }}
+                  disabled
+                >
+                  <>
+                    <Text tw="text-base font-bold text-gray-900">Sell</Text>
+                  </>
+                </Button>
+              </View>
+            </View>
+          </View>
+          <View tw="pb-6 pt-4">
+            <Text tw="text-13 text-gray-900 dark:text-white">
+              Your Creator Token is a collectible, and your profile picture will
+              be its image. You can update this later.
+            </Text>
+          </View>
+          <Button
+            size="regular"
+            tw={`w-full ${loading ? "opacity-50" : ""}`}
+            disabled={loading}
+            onPress={onSubmit}
+          >
+            <>
+              <ShowtimeRounded
+                color={isDark ? colors.gray[900] : colors.white}
+                width={20}
+                height={20}
+              />
+              <Text tw="ml-1 text-base font-bold text-white dark:text-gray-900">
+                {loading ? "Creating your Token..." : "Create your Token"}
+              </Text>
+            </>
+          </Button>
+        </View>
       </View>
-    </View>
+      <MediaCropper
+        src={selectedImg}
+        visible={!!selectedImg}
+        onClose={() => setSelectedImg(null)}
+        aspect={currentCropField === "coverPicture" ? 3 / 1 : 1}
+        title={`Crop your ${
+          currentCropField === "coverPicture" ? "cover" : "profile"
+        } picture`}
+        cropViewHeight={380}
+        onApply={async (e) => {
+          if (!currentCropField) return;
+          const timestamp = new Date().valueOf();
+          const imgFile = new File([e], timestamp.toString(), {
+            lastModified: timestamp,
+            type: e.type,
+          });
+
+          setProfilePic(imgFile);
+          setValue(imgFile);
+          setSelectedImg(null);
+        }}
+      />
+    </>
   );
 };
