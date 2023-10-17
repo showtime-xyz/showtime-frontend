02764ba9f88649f56323afe6025bb09f00317fef
alantoa <toacncom@gmail.com>
2023-10-17 21:09:23 +0800

    update self serve expaliner screen

--- packages/app/components/creator-token/self-serve-explainer.tsx
+++ packages/app/components/creator-token/self-serve-explainer.tsx
@@ -5,8 +5,8 @@ import { Avatar } from "@showtime-xyz/universal.avatar";
 import { Button } from "@showtime-xyz/universal.button";
 import { useIsDarkMode } from "@showtime-xyz/universal.hooks";
 import { Flip, ShowtimeRounded } from "@showtime-xyz/universal.icon";
-import { Image } from "@showtime-xyz/universal.image";
 import { Pressable } from "@showtime-xyz/universal.pressable";
+import { useRouter } from "@showtime-xyz/universal.router";
 import { useSafeAreaInsets } from "@showtime-xyz/universal.safe-area";
 import { colors } from "@showtime-xyz/universal.tailwind";
 import { Text } from "@showtime-xyz/universal.text";
@@ -14,13 +14,12 @@ import { View } from "@showtime-xyz/universal.view";
 
 import { useCreatorTokenDeployStatus } from "app/hooks/creator-token/use-creator-token-deploy-status";
 import { useCreatorTokenOptIn } from "app/hooks/creator-token/use-creator-token-opt-in";
-import { useContentWidth } from "app/hooks/use-content-width";
 import { useRedirectToCreatorTokensShare } from "app/hooks/use-redirect-to-creator-tokens-share-screen";
 import { useUser } from "app/hooks/use-user";
 
 export const SelfServeExplainer = () => {
   const isDark = useIsDarkMode();
-  const width = useContentWidth();
+  const router = useRouter();
   const { user } = useUser();
   const { top } = useSafeAreaInsets();
   const { trigger: deployContract, isMutating } = useCreatorTokenOptIn();
@@ -51,16 +50,13 @@ export const SelfServeExplainer = () => {
       </Text>
       <View tw="web:py-0 py-4">
         <View tw="flex-row items-center py-2">
-          <Text tw="text-sm text-gray-900 dark:text-white">
-            Introducing{" "}
-            <View tw="flex-row items-end pt-0.5">
-              <ShowtimeRounded
-                color={colors.gray[900]}
-                width={16}
-                height={16}
-              />
-              <Text tw="font-bold"> Creator Tokens.</Text>
-            </View>
+          <Text tw="mr-1 text-sm text-gray-900 dark:text-white">
+            Introducing
+          </Text>
+          <ShowtimeRounded color={colors.gray[900]} width={14} height={14} />
+          <Text tw="text-sm font-bold text-gray-900 dark:text-white">
+            {" "}
+            Creator Tokens.
           </Text>
         </View>
         <Text tw="leading45 text-sm text-gray-900 dark:text-white">
@@ -69,15 +65,8 @@ export const SelfServeExplainer = () => {
           your Token <Text tw="font-bold">price increases.</Text>
         </Text>
       </View>
-      <Image
-        source={{
-          uri: "https://showtime-media.b-cdn.net/assets/creator-profile-example.png",
-        }}
-        tw="ios:-mx-5 android:-mx-5"
-        width={width}
-        height={width * (203 / 284)}
-      />
-      <View tw="items-center rounded-3xl border border-gray-200 px-4 py-6">
+
+      <View tw="mt-6 items-center rounded-3xl border border-gray-200 px-4 py-6">
         <Pressable tw="">
           <Avatar url={user?.data.profile.img_url} size={112} />
           <View tw="absolute bottom-0 left-0 right-0 top-0 flex-row items-center justify-center rounded-full bg-black/30">
@@ -85,6 +74,40 @@ export const SelfServeExplainer = () => {
             <Text tw="ml-1 text-base font-bold text-white">Replace</Text>
           </View>
         </Pressable>
+        <View tw="rounded-4xl mb-2 mt-4 w-full border border-gray-200 px-10 py-4">
+          <View tw="flex-row items-center justify-between gap-4">
+            <View tw="flex-1 items-center">
+              <Text tw="text-xs text-gray-500">TOKEN</Text>
+              <View tw="h-2" />
+              <Text tw="text-base font-bold text-gray-900 dark:text-white">
+                $1
+              </Text>
+              <Button
+                tw="mt-2.5"
+                style={{ backgroundColor: "#08F6CC", width: "100%" }}
+              >
+                <>
+                  <Text tw="text-base font-bold text-gray-900">Buy</Text>
+                </>
+              </Button>
+            </View>
+            <View tw="flex-1 items-center justify-center">
+              <Text tw="text-xs text-gray-500">COLLECTORS</Text>
+              <View tw="h-2" />
+              <Text tw="text-base font-bold text-gray-900 dark:text-white">
+                0
+              </Text>
+              <Button
+                tw="mt-2.5"
+                style={{ backgroundColor: "#FD749D", width: "100%" }}
+              >
+                <>
+                  <Text tw="text-base font-bold text-gray-900">Sell</Text>
+                </>
+              </Button>
+            </View>
+          </View>
+        </View>
         <View tw="pb-6 pt-4">
           <Text tw="text-13 text-gray-900 dark:text-white">
             Your Creator Token is a collectible, and your profile picture will
