5cfe9c89d593a7349060b1ce5e8e75c9d7a5ad68
Nishan <nishanbende@gmail.com>
2023-10-20 15:10:23 +0530

    fix: dark mode fixes

--- packages/app/screens/creator-token/buy-creator-token.tsx
+++ packages/app/screens/creator-token/buy-creator-token.tsx
@@ -8,4 +8,5 @@ export const CreatorTokenBuyScreen = withModalScreen(BuyCreatorToken, {
   matchingQueryParam: "creatorTokenBuyModal",
   snapPoints: ["100%"],
   tw: "w-full sm:w-[400px] md:w-[400px] lg:w-[400px] xl:w-[400px]",
+  disableBackdropPress: true,
 });
