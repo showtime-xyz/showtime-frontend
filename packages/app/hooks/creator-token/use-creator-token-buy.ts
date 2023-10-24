2b4851c80232cc67b79e01c46e4387e73174fe9b
Nishan <nishanbende@gmail.com>
2023-10-24 09:08:49 +0530

    buy with connect wallet

--- packages/app/hooks/creator-token/use-creator-token-buy.ts
+++ packages/app/hooks/creator-token/use-creator-token-buy.ts
@@ -1,8 +1,6 @@
 import { useSWRConfig } from "swr";
 import useSWRMutation from "swr/mutation";
 
-import { useAlert } from "@showtime-xyz/universal.alert";
-
 import { creatorTokenAbi } from "app/abi/CreatorTokenAbi";
 import { getChannelByIdCacheKey } from "app/components/creator-channels/hooks/use-channel-detail";
 import { getChannelMessageKey } from "app/components/creator-channels/hooks/use-channel-messages";
@@ -14,7 +12,6 @@ import { formatAPIErrorMessage } from "app/utilities";
 import { toast } from "design-system/toast";
 
 import { useUserProfile } from "../api-hooks";
-import { useAuth } from "../auth/use-auth";
 import { useWallet } from "../use-wallet";
 import { getTotalCollectedKey } from "./use-contract-total-collected";
 import { useApproveToken } from "./use-creator-token-approve";
@@ -36,8 +33,6 @@ export const useCreatorTokenBuy = (params: {
     address: profileData?.data?.profile.creator_token?.address,
     tokenAmount,
   });
-  const Alert = useAlert();
-  const { logout } = useAuth();
   const switchChain = useSwitchChain();
   const { mutate } = useSWRConfig();
 
@@ -45,109 +40,99 @@ export const useCreatorTokenBuy = (params: {
     "buyCreatorToken",
     async () => {
       if (wallet.isMagicWallet) {
-        Alert.alert(
-          "Unsupported wallet",
-          "Please login using a web3 wallet to buy the creator token",
-          [
-            {
-              text: "Log out",
-              onPress: () => {
-                logout();
-              },
-            },
-          ]
-        );
-      } else {
-        if (wallet.address && profileData?.data?.profile.creator_token) {
-          await switchChain.trigger();
-          // @ts-ignore
-          const result = await approveToken.trigger({
-            creatorTokenContract:
-              profileData?.data?.profile.creator_token.address,
-            maxPrice: priceToBuyNext.data?.totalPrice,
-          });
-          if (result) {
-            let requestPayload: any;
+        await wallet.disconnect();
+        await wallet.connect();
+      }
 
-            if (tokenAmount === 1) {
-              const { request } = await publicClient.simulateContract({
-                address: profileData?.data?.profile.creator_token.address,
-                account: wallet.address,
-                abi: creatorTokenAbi,
-                functionName: "buy",
-                args: [priceToBuyNext.data?.totalPrice],
-                chain: baseChain,
-              });
-              requestPayload = request;
-            } else {
-              const { request } = await publicClient.simulateContract({
-                address: profileData?.data?.profile.creator_token.address,
-                account: wallet.address,
-                abi: creatorTokenAbi,
-                functionName: "bulkBuy",
-                args: [tokenAmount, priceToBuyNext.data?.totalPrice],
-                chain: baseChain,
-              });
-              console.log("bulk buy ", request);
-              requestPayload = request;
-            }
+      if (wallet.address && profileData?.data?.profile.creator_token) {
+        await switchChain.trigger();
+        // @ts-ignore
+        const result = await approveToken.trigger({
+          creatorTokenContract:
+            profileData?.data?.profile.creator_token.address,
+          maxPrice: priceToBuyNext.data?.totalPrice,
+        });
+        if (result) {
+          let requestPayload: any;
 
-            Logger.log("simulate ", requestPayload);
-            const transactionHash = await wallet.walletClient?.writeContract?.(
-              requestPayload
-            );
-            Logger.log("Buy transaction hash ", requestPayload);
-            if (transactionHash) {
-              const transaction = await publicClient.waitForTransactionReceipt({
-                hash: transactionHash,
-                pollingInterval: 2000,
-              });
-
-              if (transaction.status === "success") {
-                mutate(
-                  getTotalCollectedKey(
-                    profileData?.data?.profile.creator_token.address
-                  )
-                );
-                mutate(
-                  getPriceToBuyNextKey({
-                    address: profileData?.data?.profile.creator_token.address,
-                    tokenAmount: 1,
-                  })
-                );
-
-                await axios({
-                  url: "/v1/creator-token/poll-buy",
-                  method: "POST",
-                  data: {
-                    creator_token_id: profileData.data.profile.creator_token.id,
-                    quantity: tokenAmount,
-                    tx_hash: transactionHash,
-                  },
-                });
-                mutate(
-                  (key: any) => {
-                    const channelId = profileData.data?.profile.channels[0]?.id;
-                    if (
-                      typeof key === "string" &&
-                      typeof channelId === "number" &&
-                      (key.startsWith(getChannelByIdCacheKey(channelId)) ||
-                        key.startsWith(getChannelMessageKey(channelId)))
-                    ) {
-                      return true;
-                    }
-                  },
-                  undefined,
-                  { revalidate: true }
-                );
-                return true;
-              }
-            }
-          } else {
-            toast.error("Failed", {
-              message: "Approve transaction failed",
+          if (tokenAmount === 1) {
+            const { request } = await publicClient.simulateContract({
+              address: profileData?.data?.profile.creator_token.address,
+              account: wallet.address,
+              abi: creatorTokenAbi,
+              functionName: "buy",
+              args: [priceToBuyNext.data?.totalPrice],
+              chain: baseChain,
             });
+            requestPayload = request;
+          } else {
+            const { request } = await publicClient.simulateContract({
+              address: profileData?.data?.profile.creator_token.address,
+              account: wallet.address,
+              abi: creatorTokenAbi,
+              functionName: "bulkBuy",
+              args: [tokenAmount, priceToBuyNext.data?.totalPrice],
+              chain: baseChain,
+            });
+            console.log("bulk buy ", request);
+            requestPayload = request;
           }
+
+          Logger.log("simulate ", requestPayload);
+          const transactionHash = await wallet.walletClient?.writeContract?.(
+            requestPayload
+          );
+          Logger.log("Buy transaction hash ", requestPayload);
+          if (transactionHash) {
+            const transaction = await publicClient.waitForTransactionReceipt({
+              hash: transactionHash,
+              pollingInterval: 2000,
+            });
+
+            if (transaction.status === "success") {
+              mutate(
+                getTotalCollectedKey(
+                  profileData?.data?.profile.creator_token.address
+                )
+              );
+              mutate(
+                getPriceToBuyNextKey({
+                  address: profileData?.data?.profile.creator_token.address,
+                  tokenAmount: 1,
+                })
+              );
+
+              await axios({
+                url: "/v1/creator-token/poll-buy",
+                method: "POST",
+                data: {
+                  creator_token_id: profileData.data.profile.creator_token.id,
+                  quantity: tokenAmount,
+                  tx_hash: transactionHash,
+                },
+              });
+              mutate(
+                (key: any) => {
+                  const channelId = profileData.data?.profile.channels[0]?.id;
+                  if (
+                    typeof key === "string" &&
+                    typeof channelId === "number" &&
+                    (key.startsWith(getChannelByIdCacheKey(channelId)) ||
+                      key.startsWith(getChannelMessageKey(channelId)))
+                  ) {
+                    return true;
+                  }
+                },
+                undefined,
+                { revalidate: true }
+              );
+              return true;
+            }
+          }
+        } else {
+          toast.error("Failed", {
+            message: "Approve transaction failed",
+          });
         }
       }
     },
--- packages/app/hooks/creator-token/use-creator-token-sell.ts
+++ packages/app/hooks/creator-token/use-creator-token-sell.ts
@@ -10,11 +10,10 @@ import { UserContext } from "app/context/user-context";
 import { axios } from "app/lib/axios";
 import { Logger } from "app/lib/logger";
 import { publicClient } from "app/lib/wallet-public-client";
-import { delay, formatAPIErrorMessage } from "app/utilities";
+import { formatAPIErrorMessage } from "app/utilities";
 
 import { toast } from "design-system/toast";
 
-import { useAuth } from "../auth/use-auth";
 import { useWallet } from "../use-wallet";
 import { getTotalCollectedKey } from "./use-contract-total-collected";
 import { getPriceToBuyNextKey } from "./use-creator-token-price-to-buy-next";
@@ -23,11 +22,9 @@ import { baseChain, useSwitchChain } from "./use-switch-chain";
 export const useCreatorTokenSell = () => {
   const wallet = useWallet();
   const Alert = useAlert();
-  const { logout } = useAuth();
   const switchChain = useSwitchChain();
   const { mutate } = useSWRConfig();
   const user = useContext(UserContext);
-
   const state = useSWRMutation(
     "sellToken",
     async (
@@ -43,96 +40,83 @@ export const useCreatorTokenSell = () => {
       }
     ) => {
       if (wallet.isMagicWallet) {
-        Alert.alert(
-          "Unsupported wallet",
-          "Please login using a web3 wallet to buy the creator token",
-          [
-            {
-              text: "Log out",
-              onPress: () => {
-                logout();
-              },
-            },
-          ]
-        );
+        await wallet.disconnect();
+        await wallet.connect();
+      }
+
+      const tokenIdsRes = await axios({
+        url:
+          "/v1/creator-token/sell/token-ids?creator_token_id=" +
+          arg.creatorTokenId,
+        method: "GET",
+      });
+      console.log("Res ", tokenIdsRes);
+      await switchChain.trigger();
+
+      let requestPayload: any;
+      const tokenIds = tokenIdsRes.token_ids.slice(0, arg.quantity);
+
+      if (tokenIds.length === 0) {
+        // Should never happen ideally, but could happen as currently backend is depending on frontend to sync info. Should be fixed once we integrate webhooks
+        toast.error("You have no tokens to sell this time");
+        return;
+      }
+
+      if (
+        tokenIds.length === 1 &&
+        user?.user?.data.profile.creator_token?.address === arg.contractAddress
+      ) {
+        toast.error("You cannot sell your last token");
+        return;
+      }
+
+      if (tokenIds.length === 1) {
+        const { request } = await publicClient.simulateContract({
+          address: arg.contractAddress,
+          account: wallet.address,
+          abi: creatorTokenAbi,
+          functionName: "sell",
+          args: [tokenIds[0]],
+          chain: baseChain,
+        });
+        requestPayload = request;
       } else {
-        const tokenIdsRes = await axios({
-          url:
-            "/v1/creator-token/sell/token-ids?creator_token_id=" +
-            arg.creatorTokenId,
-          method: "GET",
+        const { request } = await publicClient.simulateContract({
+          address: arg.contractAddress,
+          account: wallet.address,
+          abi: creatorTokenAbi,
+          functionName: "bulkSell",
+          args: [tokenIds],
+          chain: baseChain,
         });
-        console.log("Res ", tokenIdsRes);
-        await switchChain.trigger();
+        requestPayload = request;
+      }
 
-        let requestPayload: any;
-        const tokenIds = tokenIdsRes.token_ids.slice(0, arg.quantity);
+      const txHash = await wallet.walletClient?.writeContract?.(requestPayload);
 
-        if (tokenIds.length === 0) {
-          // Should never happen ideally, but could happen as currently backend is depending on frontend to sync info. Should be fixed once we integrate webhooks
-          toast.error("You have no tokens to sell this time");
-          return;
-        }
+      const transaction = await publicClient.waitForTransactionReceipt({
+        hash: txHash as any,
+        pollingInterval: 2000,
+      });
 
-        if (
-          tokenIds.length === 1 &&
-          user?.user?.data.profile.creator_token?.address ===
-            arg.contractAddress
-        ) {
-          toast.error("You cannot sell your last token");
-          return;
-        }
-
-        if (tokenIds.length === 1) {
-          const { request } = await publicClient.simulateContract({
+      if (transaction.status === "success") {
+        mutate(getTotalCollectedKey(arg.contractAddress));
+        mutate(
+          getPriceToBuyNextKey({
             address: arg.contractAddress,
-            account: wallet.address,
-            abi: creatorTokenAbi,
-            functionName: "sell",
-            args: [tokenIds[0]],
-            chain: baseChain,
-          });
-          requestPayload = request;
-        } else {
-          const { request } = await publicClient.simulateContract({
-            address: arg.contractAddress,
-            account: wallet.address,
-            abi: creatorTokenAbi,
-            functionName: "bulkSell",
-            args: [tokenIds],
-            chain: baseChain,
-          });
-          requestPayload = request;
-        }
-
-        const txHash = await wallet.walletClient?.writeContract?.(
-          requestPayload
+            tokenAmount: 1,
+          })
         );
-
-        const transaction = await publicClient.waitForTransactionReceipt({
-          hash: txHash as any,
-          pollingInterval: 2000,
+        await axios({
+          url: "/v1/creator-token/poll-sell",
+          method: "POST",
+          data: {
+            creator_token_id: arg.creatorTokenId,
+            token_ids: tokenIds,
+            tx_hash: txHash,
+          },
         });
-
-        if (transaction.status === "success") {
-          mutate(getTotalCollectedKey(arg.contractAddress));
-          mutate(
-            getPriceToBuyNextKey({
-              address: arg.contractAddress,
-              tokenAmount: 1,
-            })
-          );
-          await axios({
-            url: "/v1/creator-token/poll-sell",
-            method: "POST",
-            data: {
-              creator_token_id: arg.creatorTokenId,
-              token_ids: tokenIds,
-              tx_hash: txHash,
-            },
-          });
-          return true;
-        }
+        return true;
       }
     },
     {
