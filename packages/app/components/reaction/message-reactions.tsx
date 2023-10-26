a8a56766b8100920c511b24ba81c4b065f19b411
Hirbod Mirjavadi <hm@nightstomp.com>
2023-10-26 20:28:59 +0200

    fix: don't crash when channelReactions missing

--- packages/app/components/reaction/message-reactions.tsx
+++ packages/app/components/reaction/message-reactions.tsx
@@ -58,46 +58,48 @@ export const MessageReactions = ({
 
   return (
     <View tw="-ml-0.5 w-full flex-1 flex-row items-center">
-      {channelReactions.map((item, index) => {
-        const userReaction = reactionGroup.find(
-          (r) => r.reaction_id === item.id
-        );
-        if (userReaction) {
-          return (
-            <AnimatedView
-              key={index}
-              tw="mr-1"
-              layout={Layout.springify().damping(500).stiffness(100)}
-              entering={FadeIn}
-              exiting={FadeOut}
-            >
-              <Pressable
-                onPress={() => handleReactionPress(item.id)}
-                tw={[
-                  "min-h-[25px] items-center justify-center px-2",
-                  userReaction.self_reacted
-                    ? "rounded-lg bg-gray-100 dark:bg-gray-900"
-                    : "",
-                ]}
-              >
-                <Text
-                  tw="text-gray-700 dark:text-gray-200"
-                  style={{
-                    fontSize: 12,
-                    lineHeight: Platform.select({
-                      web: 12,
-                      ios: undefined,
-                      android: 14,
-                    }),
-                  }}
+      {channelReactions
+        ? channelReactions.map((item, index) => {
+            const userReaction = reactionGroup.find(
+              (r) => r.reaction_id === item.id
+            );
+            if (userReaction) {
+              return (
+                <AnimatedView
+                  key={index}
+                  tw="mr-1"
+                  layout={Layout.springify().damping(500).stiffness(100)}
+                  entering={FadeIn}
+                  exiting={FadeOut}
                 >
-                  {item.reaction} {formatToUSNumber(userReaction.count)}
-                </Text>
-              </Pressable>
-            </AnimatedView>
-          );
-        }
-      })}
+                  <Pressable
+                    onPress={() => handleReactionPress(item.id)}
+                    tw={[
+                      "min-h-[25px] items-center justify-center px-2",
+                      userReaction.self_reacted
+                        ? "rounded-lg bg-gray-100 dark:bg-gray-900"
+                        : "",
+                    ]}
+                  >
+                    <Text
+                      tw="text-gray-700 dark:text-gray-200"
+                      style={{
+                        fontSize: 12,
+                        lineHeight: Platform.select({
+                          web: 12,
+                          ios: undefined,
+                          android: 14,
+                        }),
+                      }}
+                    >
+                      {item.reaction} {formatToUSNumber(userReaction.count)}
+                    </Text>
+                  </Pressable>
+                </AnimatedView>
+              );
+            }
+          })
+        : null}
     </View>
   );
 };
