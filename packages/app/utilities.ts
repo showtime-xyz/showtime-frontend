c9ba7cb10aebb5efe980499792318f2b7d1ffb93
Hirbod Mirjavadi <hm@nightstomp.com>
2023-10-28 11:29:40 +0200

    hotfix: crash because of window.location.host

--- packages/app/utilities.ts
+++ packages/app/utilities.ts
@@ -801,9 +801,7 @@ export const OAUTH_REDIRECT_URI = Platform.select({
   web: __DEV__
     ? "http://localhost:3000/magic-oauth-redirect"
     : `https://${
-        typeof window !== "undefined"
-          ? window.location.host
-          : process.env.NEXT_PUBLIC_WEBSITE_DOMAIN
+        window?.location?.host ?? process.env.NEXT_PUBLIC_WEBSITE_DOMAIN
       }/magic-oauth-redirect`,
   default: `io.showtime${__DEV__ ? ".development" : ""}://magic-oauth-redirect`,
 });
