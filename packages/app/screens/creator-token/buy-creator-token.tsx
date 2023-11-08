c9271d49b97561505a27b42f6ee9a2ed587d4b73
alantoa <toacncom@gmail.com>
2023-11-08 22:06:37 +0800

    support new url to buy

--- packages/app/screens/creator-token/buy-creator-token.tsx
+++ packages/app/screens/creator-token/buy-creator-token.tsx
@@ -4,7 +4,7 @@ import { BuyCreatorToken } from "app/components/creator-token/buy-creator-token"
 
 export const CreatorTokenBuyScreen = withModalScreen(BuyCreatorToken, {
   title: "",
-  matchingPathname: "/creator-token/buy",
+  matchingPathname: "/creator-token/[username]/buy",
   matchingQueryParam: "creatorTokenBuyModal",
   snapPoints: ["100%"],
   tw: "w-full sm:w-[400px] md:w-[400px] lg:w-[400px] xl:w-[400px]",
