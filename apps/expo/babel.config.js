22b1e777b9318e4bd574198aa35b6ace49d97af1
Nishan <nishanbende@gmail.com>
2023-11-16 23:25:01 +0530

    add flow strip types plugin

--- apps/expo/babel.config.js
+++ apps/expo/babel.config.js
@@ -22,6 +22,7 @@ module.exports = function (api) {
   let plugins = [
     ["inline-dotenv", { path: envPath }],
     "react-native-reanimated/plugin",
+    "@babel/plugin-transform-flow-strip-types",
     ["@babel/plugin-transform-class-properties", { loose: true }],
     ["@babel/plugin-transform-private-methods", { loose: true }],
     [
