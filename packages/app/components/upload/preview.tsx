3012b60414e651b5295a092574ce3edfbf557610
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-27 23:17:41 +0100

    Add videoPath to UploadPreview component

--- packages/app/components/upload/preview.tsx
+++ packages/app/components/upload/preview.tsx
@@ -1,7 +1,13 @@
+import { useSnapshot } from "valtio";
+
 import { Text } from "@showtime-xyz/universal.text";
 import { View } from "@showtime-xyz/universal.view";
 
+import { videoUploadStore } from "./video-upload-store";
+
 const UploadPreview = () => {
+  const { videoPath } = useSnapshot(videoUploadStore);
+  console.log(videoPath);
   return (
     <View tw="flex-1 items-center justify-center">
       <Text tw="text-white">Preview</Text>
