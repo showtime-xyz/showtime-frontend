b009b398d4425a9fa43297012eba8b6357044b3b
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-27 17:36:44 +0100

    Add UploadPreview component

---
+++ packages/app/components/upload/preview.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@showtime-xyz/universal.text";
+import { View } from "@showtime-xyz/universal.view";
+
+const UploadPreview = () => {
+  return (
+    <View tw="flex-1 items-center justify-center">
+      <Text tw="text-white">Preview</Text>
+    </View>
+  );
+};
+
+export default UploadPreview;
