6096c14d2b1dcb7c074fe1f788c6894ff6a0afc1
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-27 17:37:12 +0100

    Add UploadComposerScreen component

---
+++ packages/app/screens/upload-composer/upload-composer.tsx
@@ -0,0 +1,13 @@
+import { withColorScheme } from "app/components/memo-with-theme";
+import UploadComposer from "app/components/upload/composer";
+import { useAuthScreen } from "app/hooks/use-auth-screen";
+import { useTrackPageViewed } from "app/lib/analytics";
+
+const UploadComposerScreen = withColorScheme(() => {
+  useTrackPageViewed({ name: "Upload Video Composer" });
+  useAuthScreen();
+
+  return <UploadComposer />;
+});
+
+export { UploadComposerScreen };
