23bc491fc421ba946fec101ca42f811d17ddcc27
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-27 17:36:39 +0100

    Add UploadComposer component

---
+++ packages/app/components/upload/composer.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@showtime-xyz/universal.text";
+import { View } from "@showtime-xyz/universal.view";
+
+const UploadComposer = () => {
+  return (
+    <View>
+      <Text>Composer</Text>
+    </View>
+  );
+};
+
+export default UploadComposer;
