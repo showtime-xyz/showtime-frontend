7809779dfed3157731267f1e60f3a6b1bed9bdd2
Nishan <nishanbende@gmail.com>
2023-11-27 16:44:29 +0530

    feat: add linear gradient

--- packages/app/components/video-feed.tsx
+++ packages/app/components/video-feed.tsx
@@ -1,6 +1,8 @@
 import { Platform } from "react-native";
 
+import { fromCSS } from "@bacons/css-to-expo-linear-gradient";
 import { ResizeMode } from "expo-av";
+import { LinearGradient } from "expo-linear-gradient";
 
 import { Avatar } from "@showtime-xyz/universal.avatar";
 import { ChannelLocked, Share } from "@showtime-xyz/universal.icon";
@@ -54,42 +56,50 @@ const FeedItem = ({ video }: any) => {
   return (
     <View tw="w-full">
       <FeedVideo video={video} />
-      <View
-        tw="z-1 absolute bottom-0 w-full flex-row items-center justify-between bg-black"
-        style={{
-          paddingBottom: safeAreaInsets.bottom + 16,
-          paddingHorizontal: 16,
-        }}
-      >
-        <View style={{ rowGap: 10, flex: 3 }}>
-          <View tw="flex-row items-center">
-            <Avatar size={32} />
-            <Text tw="ml-2 mr-4 text-white">lilbubble</Text>
-            <Pressable
-              tw="rounded-4xl items-center justify-center px-4"
-              style={{ backgroundColor: "#08F6CC", height: 30 }}
+      <View tw="z-1 absolute bottom-0 w-full">
+        <LinearGradient
+          style={{
+            flex: 1,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            paddingBottom: safeAreaInsets.bottom + 16,
+            paddingHorizontal: 16,
+          }}
+          {...fromCSS(
+            `linear-gradient(to top,rgba(0,0,0,.4) 0,rgba(0,0,0,0) 160px),linear-gradient(to bottom,rgba(0,0,0,.4) 0,rgba(0,0,0,0) 96px);`
+          )}
+        >
+          <View style={{ rowGap: 10, flex: 3 }}>
+            <View tw="flex-row items-center">
+              <Avatar size={32} />
+              <Text tw="ml-2 mr-4 text-white">lilbubble</Text>
+              <Pressable
+                tw="rounded-4xl items-center justify-center px-4"
+                style={{ backgroundColor: "#08F6CC", height: 30 }}
+              >
+                <Text>Buy $21.67</Text>
+              </Pressable>
+            </View>
+            <Text
+              tw="overflow-hidden whitespace-nowrap text-white"
+              numberOfLines={1}
             >
-              <Text>Buy $21.67</Text>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown
+            </Text>
+          </View>
+          <View tw="flex-1 items-end" style={{ rowGap: 12 }}>
+            <Pressable tw="items-center p-1" style={{ rowGap: 4 }}>
+              <ChannelLocked color="white" width={31} height={28} />
+              <Text tw="text-white">139</Text>
+            </Pressable>
+            <Pressable tw="items-center p-1">
+              <Share color="white" width={31} height={28} />
             </Pressable>
           </View>
-          <Text
-            tw="overflow-hidden whitespace-nowrap text-white"
-            numberOfLines={1}
-          >
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown
-          </Text>
-        </View>
-        <View tw="flex-1 items-end" style={{ rowGap: 12 }}>
-          <Pressable tw="items-center p-1" style={{ rowGap: 4 }}>
-            <ChannelLocked color="white" width={31} height={28} />
-            <Text tw="text-white">139</Text>
-          </Pressable>
-          <Pressable tw="items-center p-1">
-            <Share color="white" width={31} height={28} />
-          </Pressable>
-        </View>
+        </LinearGradient>
       </View>
     </View>
   );
@@ -116,8 +126,8 @@ const FeedVideo = ({ video }: any) => {
       style={videoDimensions}
       // @ts-ignore
       videoStyle={videoDimensions}
-      useNativeControls
-      resizeMode={ResizeMode.CONTAIN}
+      useNativeControls={false}
+      resizeMode={ResizeMode.COVER}
     />
   );
 };
