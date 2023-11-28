2c666db10adbacced330f4288b94668f2adf39a5
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-28 22:16:23 +0100

    Add Create icon and text to CreateTabBarIcon component

--- packages/app/components/upload/upload-tab-bar-icon.tsx
+++ packages/app/components/upload/upload-tab-bar-icon.tsx
@@ -1,9 +1,12 @@
-import { Platform } from "react-native";
+import { Platform, useWindowDimensions } from "react-native";
 
 import { useSnapshot } from "valtio";
 
-import { Gallery, Image, Video } from "@showtime-xyz/universal.icon";
+import { useIsDarkMode } from "@showtime-xyz/universal.hooks";
+import { Create, Gallery, Image, Video } from "@showtime-xyz/universal.icon";
 import { useRouter } from "@showtime-xyz/universal.router";
+import { Text } from "@showtime-xyz/universal.text";
+import { View } from "@showtime-xyz/universal.view";
 
 import { MenuItemIcon } from "app/components/dropdown/menu-item-icon";
 import { TabBarIconProps } from "app/navigation/tab-bar-icons";
@@ -15,12 +18,16 @@ import {
   DropdownMenuRoot,
   DropdownMenuTrigger,
 } from "design-system/dropdown-menu";
+import { breakpoints } from "design-system/theme";
 
 import { videoUploadStore } from "./video-upload-store";
 
 export const CreateTabBarIcon = ({ color }: TabBarIconProps) => {
   const { takeVideo, pickVideo, chooseVideo } = useSnapshot(videoUploadStore);
   const router = useRouter();
+  const { width } = useWindowDimensions();
+  const isMdWidth = width >= breakpoints["md"];
+  const isDark = useIsDarkMode();
 
   const redirectToComposerScreen = () => {
     const as = "/upload/composer";
@@ -45,14 +52,27 @@ export const CreateTabBarIcon = ({ color }: TabBarIconProps) => {
   return (
     <DropdownMenuRoot>
       <DropdownMenuTrigger>
-        <Video
-          style={{ zIndex: 1 }}
-          width={24}
-          height={24}
-          color={color ?? "black"}
-        />
+        <View tw="cursor-pointer flex-row items-center justify-center text-center">
+          <Create
+            style={{ zIndex: 1 }}
+            width={34}
+            height={34}
+            color={color ?? "black"}
+            isDark={isDark}
+          />
+          {isMdWidth && (
+            <View tw="ml-2">
+              <Text tw="font-bold">Create</Text>
+            </View>
+          )}
+        </View>
       </DropdownMenuTrigger>
-      <DropdownMenuContent side="bottom" align="start" sideOffset={10} loop>
+      <DropdownMenuContent
+        side="bottom"
+        align={isMdWidth ? "start" : "center"}
+        sideOffset={10}
+        loop
+      >
         <DropdownMenuItem
           key="b_library"
           onSelect={async () => {
