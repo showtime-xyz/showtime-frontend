e08bd3ea3ac538a930e3a69c1cbc59e08835f0f8
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-28 19:55:05 +0100

    Update upload composer component

--- packages/app/components/upload/composer.tsx
+++ packages/app/components/upload/composer.tsx
@@ -1,10 +1,29 @@
-import { Text } from "@showtime-xyz/universal.text";
+import { useSnapshot } from "valtio";
+
+import { useIsDarkMode } from "@showtime-xyz/universal.hooks";
+import { TextInput } from "@showtime-xyz/universal.text-input";
 import { View } from "@showtime-xyz/universal.view";
 
+import VideoThumbnail from "./video-thumbnail";
+import { videoUploadStore } from "./video-upload-store";
+
 const UploadComposer = () => {
+  const { videoPath } = useSnapshot(videoUploadStore);
+  const isDark = useIsDarkMode();
   return (
-    <View>
-      <Text>Composer</Text>
+    <View tw="flex-1 items-center justify-start p-4">
+      <VideoThumbnail videoUri={videoPath?.uri} timeFrame={100} />
+      <View tw="mt-8 w-full">
+        <TextInput
+          tw="h-36 w-full rounded-3xl bg-gray-200 p-4 text-black dark:bg-[#1B1B1B]  dark:text-white"
+          placeholder="Write a caption..."
+          placeholderTextColor={isDark ? "#9CA3AF" : "#9F9F9F"}
+          textAlignVertical="top"
+          textAlign="left"
+          multiline
+          maxLength={280}
+        />
+      </View>
     </View>
   );
 };
