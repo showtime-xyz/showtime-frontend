6f63aebc94c2a21c4d28fa4af6c9bea6ee8573c4
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-28 19:56:07 +0100

    Remove title in UploadComposerScreen

--- packages/app/screens/upload-composer/upload-composer.web.tsx
+++ packages/app/screens/upload-composer/upload-composer.web.tsx
@@ -3,7 +3,7 @@ import { withModalScreen } from "@showtime-xyz/universal.modal-screen";
 import UploadComposer from "app/components/upload/composer";
 
 export const UploadComposerScreen = withModalScreen(UploadComposer, {
-  title: "asdasd",
+  title: "",
   matchingPathname: "/upload/composer",
   matchingQueryParam: "uploadComposerModal",
   disableBackdropPress: true,
