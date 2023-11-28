10878c773b7414b391714298d3505911d0c22d31
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-28 14:32:23 +0100

    Export UploadComposerScreen component

--- packages/app/screens/upload-composer/upload-composer.tsx
+++ packages/app/screens/upload-composer/upload-composer.tsx
@@ -3,11 +3,9 @@ import UploadComposer from "app/components/upload/composer";
 import { useAuthScreen } from "app/hooks/use-auth-screen";
 import { useTrackPageViewed } from "app/lib/analytics";
 
-const UploadComposerScreen = withColorScheme(() => {
+export const UploadComposerScreen = withColorScheme(() => {
   useTrackPageViewed({ name: "Upload Video Composer" });
   useAuthScreen();
 
   return <UploadComposer />;
 });
-
-export { UploadComposerScreen };
