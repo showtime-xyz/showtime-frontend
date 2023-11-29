604bae75f1b16cbcf1adbe85d847ae547e0a1035
Hirbod Mirjavadi <hm@nightstomp.com>
2023-11-29 20:55:32 +0100

    Add video upload store

---
+++ packages/app/components/upload/video-upload-store.ts
@@ -0,0 +1,16 @@
+import * as tus from "tus-js-client";
+import { proxy, ref } from "valtio";
+
+import { chooseVideo, pickVideo, signUpload, takeVideo } from "./action";
+import { VideoUploadStoreState } from "./types";
+
+export const videoUploadStore = proxy<VideoUploadStoreState>({
+  videoPath: null,
+  uploadProgress: 0,
+  isUploading: false,
+  uploadInstance: ref(new tus.Upload(new Blob(), {})),
+  takeVideo,
+  pickVideo,
+  chooseVideo,
+  signUpload,
+});
